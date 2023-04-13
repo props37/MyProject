73386628620f6c2f6250409f6b179cce42bbea6f
Dmitry Popov <popovdd@melonfashion.com>
2023-04-13 09:22:51 +0300

    Suppress gradle version catalog false-positive errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,7 @@
+// TODO remove after upgrading to Gradle 8.1
+// https://github.com/gradle/gradle/issues/22797
+@Suppress("DSL_SCOPE_VIOLATION")
+
 plugins {
     alias(libs.plugins.com.android.application)
     alias(libs.plugins.org.jetbrains.kotlin.android)
