62c18f39cf058831651f9426317e4e1aa4ea3e63
Dmitry Popov <popovdd@melonfashion.com>
2023-04-15 01:54:35 +0300

    Add onboarding screen structure

--- app/src/main/java/ru/zarina/zarina/ui/screens/OnboardingScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/OnboardingScreen.kt
@@ -1,6 +1,10 @@
 package ru.zarina.zarina.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ru.zarina.zarina.ui.common.tooling.DensityPreviews
 import ru.zarina.zarina.ui.common.tooling.FontScalePreviews
@@ -8,7 +12,48 @@ import ru.zarina.zarina.ui.theme.ZarinaTheme
 
 @Composable
 private fun OnboardingScreenContent() {
+    Banner()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .systemBarsPadding(),
+    ) {
+        TopBar()
+        Logo(
+            modifier = Modifier.weight(1f),
+        )
+        CitySelection()
+    }
+}
 
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier,
+) {
+    // TODO display local banner
+    // TODO load current banner from backend
+}
+
+@Composable
+fun TopBar(
+    modifier: Modifier = Modifier,
+) {
+    // TODO close button
+}
+
+@Composable
+fun Logo(
+    modifier: Modifier = Modifier,
+) {
+    // TODO add logo
+}
+
+@Composable
+fun CitySelection(
+    modifier: Modifier = Modifier,
+) {
+    // TODO text
+    // TODO buttons
 }
 
 @Composable
