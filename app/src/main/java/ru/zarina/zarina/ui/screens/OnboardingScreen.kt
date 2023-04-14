94f865abef73771007df65af7dd3d31303603edb
Dmitry Popov <popovdd@melonfashion.com>
2023-04-15 00:22:05 +0300

    Add onboarding screen preview

--- app/src/main/java/ru/zarina/zarina/ui/screens/OnboardingScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/OnboardingScreen.kt
@@ -1,6 +1,10 @@
 package ru.zarina.zarina.ui.screens
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import ru.zarina.zarina.ui.common.tooling.DensityPreviews
+import ru.zarina.zarina.ui.common.tooling.FontScalePreviews
+import ru.zarina.zarina.ui.theme.ZarinaTheme
 
 @Composable
 private fun OnboardingScreenContent() {
@@ -10,4 +14,14 @@ private fun OnboardingScreenContent() {
 @Composable
 fun OnboardingScreen() {
     OnboardingScreenContent()
+}
+
+@Preview
+@FontScalePreviews
+@DensityPreviews
+@Composable
+fun OnboardingScreenContentPreview() {
+    ZarinaTheme {
+        OnboardingScreenContent()
+    }
 }
