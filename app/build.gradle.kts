8eae4385afb0af769021434d144947bb381d6a03
Dmitry Popov <popovdd@melonfashion.com>
2023-04-14 23:13:03 +0300

    Add navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.splashScreen)
+    implementation(libs.androidx.navigation.compose)
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.ui.core)
     implementation(libs.compose.ui.graphics)
