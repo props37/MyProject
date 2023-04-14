9dfde255eec89d1d8deeebd4796b1eaf177e0a87
Dmitry Popov <popovdd@melonfashion.com>
2023-04-14 18:01:11 +0300

    System bars are transparent

--- app/src/main/java/ru/zarina/zarina/MainActivity.kt
+++ app/src/main/java/ru/zarina/zarina/MainActivity.kt
@@ -3,14 +3,20 @@ package ru.zarina.zarina
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import ru.zarina.zarina.ui.ZarinaApp
+import ru.zarina.zarina.ui.common.system.TransparentSystemBars
 import ru.zarina.zarina.ui.theme.ZarinaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             ZarinaTheme {
+                TransparentSystemBars()
                 ZarinaApp()
             }
         }
