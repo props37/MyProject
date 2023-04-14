79505367ce843ca9a36c56579edc738fac81fc52
Dmitry Popov <popovdd@melonfashion.com>
2023-04-14 17:04:34 +0300

    Use generated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,12 +12,16 @@ android {
     namespace = "ru.zarina.zarina"
     compileSdk = 33
 
+    val tagVersionCode = androidGitVersion.code()
+    val tagVersionName = androidGitVersion.name()
+    println("tagVersionCode = $tagVersionCode, tagVersionName = $tagVersionName")
+
     defaultConfig {
         applicationId = "ru.zarina.zarina"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = tagVersionCode
+        versionName = tagVersionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
