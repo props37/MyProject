5804037bb1ae749e7259af78b181987cc8cfa028
Dmitry Popov <popovdd@melonfashion.com>
2023-04-15 00:01:29 +0300

    Add home destination

--- app/src/main/java/ru/zarina/zarina/ui/ZarinaApp.kt
+++ app/src/main/java/ru/zarina/zarina/ui/ZarinaApp.kt
@@ -1,16 +1,9 @@
 package ru.zarina.zarina.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import ru.zarina.zarina.ui.navigation.ZarinaNavigation
 
 @Composable
 fun ZarinaApp() {
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text("Zarina")
-    }
+    ZarinaNavigation()
 }
