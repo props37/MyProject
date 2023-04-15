eb60aa40874f3f9112060e8f18ad8d8be3dcb7ca
Dmitry Popov <popovdd@melonfashion.com>
2023-04-15 15:25:25 +0300

    Refactor navigation
    Use Destinations instead of BaseRoute

--- app/src/main/java/ru/zarina/zarina/ui/navigation/ZarinaNavigation.kt
+++ app/src/main/java/ru/zarina/zarina/ui/navigation/ZarinaNavigation.kt
@@ -4,24 +4,24 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import ru.zarina.zarina.ui.navigation.base.composableDestination
-import ru.zarina.zarina.ui.navigation.base.parameterless.SimpleDestination
-import ru.zarina.zarina.ui.navigation.destinations.BaseRoute
+import ru.zarina.zarina.ui.navigation.destinations.Destinations
 import ru.zarina.zarina.ui.screens.OnboardingScreen
 
 @Composable
 fun ZarinaNavigation() {
     val navController = rememberNavController()
 
-    NavHost(navController, startDestination = BaseRoute.ONBOARDING.name) {
-        composableDestination(SimpleDestination(BaseRoute.HOME)) { }
-        composableDestination(SimpleDestination(BaseRoute.ONBOARDING)) {
+    NavHost(
+        navController = navController,
+        startDestination = Destinations.ONBOARDING.route
+    ) {
+        composableDestination(Destinations.HOME) {}
+        composableDestination(Destinations.ONBOARDING) {
             OnboardingScreen(
                 showHome = {
-                    navController.navigate(BaseRoute.HOME.name) { popUpTo(0) }
+                    navController.navigate(Destinations.HOME.route) { popUpTo(0) }
                 }
             )
         }
     }
 }
-
-
