a77e34fcaace0e5613d480312890c1f871778233
Dmitry Popov <popovdd@melonfashion.com>
2023-04-15 03:25:51 +0300

    Add side effect mechanism

--- app/src/main/java/ru/zarina/zarina/ui/screens/OnboardingViewModel.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/OnboardingViewModel.kt
@@ -2,14 +2,21 @@ package ru.zarina.zarina.ui.screens
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import ru.zarina.zarina.ui.common.base.ISideEffectSource
+import ru.zarina.zarina.ui.common.base.SideEffectQueue
 
 @HiltViewModel
-class OnboardingViewModel : ViewModel() {
+class OnboardingViewModel : ViewModel(),
+    ISideEffectSource<OnboardingViewModel.SideEffect> by SideEffectQueue() {
 
     fun onDetectClick() {}
 
     fun onSelectClick() {}
 
     fun onCloseClick() {}
+
+    sealed interface SideEffect : ISideEffectSource.ISideEffect {
+        object ShowHome : SideEffect
+    }
 
 }
