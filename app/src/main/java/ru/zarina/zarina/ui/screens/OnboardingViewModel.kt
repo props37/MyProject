30b93228a77db167c61f02e22b303ae9f3ff1200
Dmitry Popov <popovdd@melonfashion.com>
2023-04-15 03:20:01 +0300

    Add view model

---
+++ app/src/main/java/ru/zarina/zarina/ui/screens/OnboardingViewModel.kt
@@ -0,0 +1,15 @@
+package ru.zarina.zarina.ui.screens
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class OnboardingViewModel : ViewModel() {
+
+    fun onDetectClick() {}
+
+    fun onSelectClick() {}
+
+    fun onCloseClick() {}
+
+}
