16348cb48bbce2769cbebcc78d799fc8eeb93cfd
Dmitry Popov <popovdd@melonfashion.com>
2023-04-15 23:46:42 +0300

    Implement detect city UseCase

--- app/src/main/java/ru/zarina/zarina/usecase/location/DetectCityUseCase.kt
+++ app/src/main/java/ru/zarina/zarina/usecase/location/DetectCityUseCase.kt
@@ -2,6 +2,7 @@ package ru.zarina.zarina.usecase.location
 
 import kotlinx.coroutines.CoroutineDispatcher
 import ru.zarina.zarina.base.clean.UseCase
+import ru.zarina.zarina.data.geography.GeographyRepository
 import ru.zarina.zarina.data.location.IGeoLocationRepository
 import ru.zarina.zarina.di.Dispatcher
 import ru.zarina.zarina.di.ZarinaDispatcher
@@ -11,10 +12,12 @@ import javax.inject.Inject
 class DetectCityUseCase @Inject constructor(
     @Dispatcher(ZarinaDispatcher.IO) dispatcher: CoroutineDispatcher,
     private val geoLocationRepository: IGeoLocationRepository,
+    private val geographyRepository: GeographyRepository,
 ) : UseCase<Unit, Unit>(dispatcher) {
     override suspend fun execute(params: Unit) {
-        val geoLocation = geoLocationRepository.getCurrentLocation()
+        val geoLocation = checkNotNull(geoLocationRepository.getCurrentLocation())
         // TODO if geoLocation is null, show "location services are not available"
-        Timber.v("Current location: $geoLocation")
+        val city = geographyRepository.getCity(geoLocation)
+        Timber.v("Current location: $geoLocation, detected city: $city")
     }
 }
