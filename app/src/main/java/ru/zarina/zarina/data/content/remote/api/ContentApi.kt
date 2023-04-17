624adccedd6dbc91dab13771777580d0c408dfdf
Dmitry Popov <popovdd@melonfashion.com>
2023-04-17 22:06:09 +0300

    Add splash request implementation

--- app/src/main/java/ru/zarina/zarina/data/content/remote/api/ContentApi.kt
+++ app/src/main/java/ru/zarina/zarina/data/content/remote/api/ContentApi.kt
@@ -1,11 +1,17 @@
 package ru.zarina.zarina.data.content.remote.api
 
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 import ru.zarina.zarina.data.content.remote.api.dto.SplashDto
 import javax.inject.Inject
 
-class ContentApi @Inject constructor() : IContentApi {
+class ContentApi @Inject constructor(
+    private val client: HttpClient,
+) : IContentApi {
     override suspend fun getOnboardingSplash(): SplashDto {
-        TODO("Not yet implemented")
+        val response = client.get("/api/main/splash")
+        return response.body()
     }
 
 }
