da653526c061fca1cb3d9a31bc79ad28430c91d0
Dmitry Popov <popovdd@melonfashion.com>
2023-04-18 16:46:21 +0300

    Add dynamic start destination

--- app/src/main/java/ru/zarina/zarina/ui/ZarinaApp.kt
+++ app/src/main/java/ru/zarina/zarina/ui/ZarinaApp.kt
@@ -1,9 +1,14 @@
 package ru.zarina.zarina.ui
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import ru.zarina.zarina.ui.navigation.ZarinaNavigation
 
 @Composable
 fun ZarinaApp() {
-    ZarinaNavigation()
+    val viewModel = hiltViewModel<AppViewModel>()
+
+    ZarinaNavigation(
+        startDestination = viewModel.startDestination,
+    )
 }
--- app/src/main/java/ru/zarina/zarina/ui/navigation/ZarinaNavigation.kt
+++ app/src/main/java/ru/zarina/zarina/ui/navigation/ZarinaNavigation.kt
@@ -3,17 +3,20 @@ package ru.zarina.zarina.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import ru.zarina.zarina.ui.navigation.base.Destination
 import ru.zarina.zarina.ui.navigation.base.composableDestination
 import ru.zarina.zarina.ui.navigation.destinations.Destinations
 import ru.zarina.zarina.ui.screens.onboarding.OnboardingScreen
 
 @Composable
-fun ZarinaNavigation() {
+fun ZarinaNavigation(
+    startDestination: Destination<*>,
+) {
     val navController = rememberNavController()
 
     NavHost(
         navController = navController,
-        startDestination = Destinations.ONBOARDING.route
+        startDestination = startDestination.routeSchema,
     ) {
         composableDestination(Destinations.HOME) {}
         composableDestination(Destinations.ONBOARDING) {
