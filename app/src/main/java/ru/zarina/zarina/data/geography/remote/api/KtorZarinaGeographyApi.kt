2d51e0259831a524b0569d38c25b5192572ff95d
Dmitry Popov <popovdd@melonfashion.com>
2023-04-18 16:38:28 +0300

    Use tokenized http client for geography

--- app/src/main/java/ru/zarina/zarina/data/geography/remote/api/KtorZarinaGeographyApi.kt
+++ app/src/main/java/ru/zarina/zarina/data/geography/remote/api/KtorZarinaGeographyApi.kt
@@ -9,7 +9,7 @@ import ru.zarina.zarina.di.Authorization
 import javax.inject.Inject
 
 class KtorZarinaGeographyApi @Inject constructor(
-    @Authorization(Authorization.Type.NONE)
+    @Authorization(Authorization.Type.TOKEN)
     private val client: HttpClient,
 ) : IZarinaGeographyApi {
 
