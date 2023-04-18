d5f67675b2b986c0d36fc91c087d0afecac126d5
Dmitry Popov <popovdd@melonfashion.com>
2023-04-18 12:54:16 +0300

    Add datastore library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.splashScreen)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.datastore.preferences)
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.ui.core)
     implementation(libs.compose.ui.graphics)
