a65b6f9f951a686b59c841af78219b4e33b7a425
Dmitry Popov <popovdd@melonfashion.com>
2023-04-18 18:58:14 +0300

    Add content type to set city request

--- app/src/main/java/ru/zarina/zarina/data/user/remote/api/KtorZarinaUserApi.kt
+++ app/src/main/java/ru/zarina/zarina/data/user/remote/api/KtorZarinaUserApi.kt
@@ -3,6 +3,8 @@ package ru.zarina.zarina.data.user.remote.api
 import io.ktor.client.HttpClient
 import io.ktor.client.request.put
 import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import ru.zarina.zarina.data.user.remote.api.dto.SetCityBody
 import ru.zarina.zarina.di.Authorization
 import javax.inject.Inject
@@ -14,6 +16,7 @@ class KtorZarinaUserApi @Inject constructor(
 
     override suspend fun setCity(body: SetCityBody) {
         client.put("/api/location/city") {
+            contentType(ContentType.Application.Json)
             setBody(body)
         }
     }
