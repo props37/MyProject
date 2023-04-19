51800c722c76007f0d5cfdb98b0fd4bf18400021
Dmitry Popov <popovdd@melonfashion.com>
2023-04-19 18:26:36 +0300

    Make snackbar text non-bold

--- app/src/main/java/ru/zarina/zarina/ui/theme/UiKit.kt
+++ app/src/main/java/ru/zarina/zarina/ui/theme/UiKit.kt
@@ -49,7 +49,6 @@ data class UiKitTypography(
     val snackbar: TextStyle = TextStyle(
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        fontWeight = FontWeight.Bold,
         fontFamily = Fonts.Families.Circle
     ),
     val onboardingHeader: TextStyle = TextStyle(
