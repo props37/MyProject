81806a7c95a20bde9f3061647271ca224c6a20f7
Dmitry Popov <popovdd@melonfashion.com>
2023-04-19 18:33:07 +0300

    Add inset padding to reload button

--- app/src/main/java/ru/zarina/zarina/ui/screens/cityselection/CitySelectionScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/cityselection/CitySelectionScreen.kt
@@ -17,14 +17,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.ime
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -62,6 +58,7 @@ import ru.zarina.zarina.ui.common.tooling.preview.providers.ui.CityListItemProvi
 import ru.zarina.zarina.ui.screens.cityselection.CitySelectionViewModel.ErrorState
 import ru.zarina.zarina.ui.theme.UiKitTheme
 import ru.zarina.zarina.ui.theme.ZarinaTheme
+import ru.zarina.zarina.utils.compose.navigationOrIme
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalAnimationApi::class)
 @Composable
@@ -106,12 +103,11 @@ fun CitySelectionScreenContent(
                         onRefreshClick = onRefreshClick,
                         modifier = Modifier
                             .fillMaxSize()
-                            .imePadding()
+                            .padding(WindowInsets.navigationOrIme.asPaddingValues())
                     )
                 else
                     LazyColumn(
-                        contentPadding = WindowInsets.ime.add(WindowInsets.navigationBars)
-                            .asPaddingValues(),
+                        contentPadding = WindowInsets.navigationOrIme.asPaddingValues(),
                         modifier = Modifier.fillMaxSize()
                     ) {
                         cityItems.forEach { item ->
