14bb8adb85e4b9457d2c207fa8f272d878f0cadc
Dmitry Popov <popovdd@melonfashion.com>
2023-04-19 12:09:18 +0300

    Add CityListItem

--- app/src/main/java/ru/zarina/zarina/ui/screens/cityselection/CitySelectionViewModel.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/cityselection/CitySelectionViewModel.kt
@@ -3,9 +3,11 @@ package ru.zarina.zarina.ui.screens.cityselection
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import ru.zarina.zarina.domain.City
 import ru.zarina.zarina.ui.common.base.ISideEffectSource
 import ru.zarina.zarina.ui.common.base.SideEffectQueue
@@ -17,8 +19,8 @@ class CitySelectionViewModel @Inject constructor(
 ) : ViewModel(),
     ISideEffectSource<CitySelectionViewModel.SideEffect> by SideEffectQueue() {
 
-    private val _cities = MutableStateFlow<List<City>>(emptyList())
-    val cities: StateFlow<List<City>> = _cities
+    private val _cities = MutableStateFlow<List<CityListItem>>(emptyList())
+    val cities: StateFlow<List<CityListItem>> = _cities
 
     init {
         fetchCities(null)
@@ -29,9 +31,30 @@ class CitySelectionViewModel @Inject constructor(
         // TODO operation tracking
         viewModelScope.launch {
             interactor.getCities(query)
-                .onSuccess { _cities.value = it }
+                .onSuccess {
+                    withContext(Dispatchers.IO) {
+                        _cities.value = it.toCityListItems()
+                    }
+                }
                 .onFailure { /* TODO display some error */ }
         }
+    }
+
+    private fun List<City>.toCityListItems(): List<CityListItem> = buildList {
+        var previousStartingLetter: Char? = null
+        this@toCityListItems.forEach { city ->
+            if (city.name.isEmpty()) return@forEach
+            if (city.name.first() != previousStartingLetter) {
+                add(CityListItem.Header(previousStartingLetter.toString()))
+                previousStartingLetter = city.name.first()
+            }
+            add(CityListItem.Item(city))
+        }
+    }
+
+    sealed class CityListItem {
+        data class Header(val letter: String) : CityListItem()
+        data class Item(val city: City) : CityListItem()
     }
 
     sealed interface SideEffect : ISideEffectSource.ISideEffect
