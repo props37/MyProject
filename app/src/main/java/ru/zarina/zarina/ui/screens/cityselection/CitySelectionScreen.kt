97f29f5b02139e4048b5b55992cff6f85be72823
Dmitry Popov <popovdd@melonfashion.com>
2023-04-19 15:20:37 +0300

    Improve list insets

--- app/src/main/java/ru/zarina/zarina/ui/screens/cityselection/CitySelectionScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/cityselection/CitySelectionScreen.kt
@@ -8,11 +8,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.add
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.BasicTextField
@@ -53,8 +57,7 @@ fun CitySelectionScreenContent(
         modifier = Modifier
             .fillMaxSize()
             .background(UiKitTheme.colors.screenBackground)
-            .systemBarsPadding()
-            .imePadding()
+            .statusBarsPadding(),
     ) {
         SearchBar(
             query = query,
@@ -62,6 +65,7 @@ fun CitySelectionScreenContent(
             modifier = Modifier.fillMaxWidth()
         )
         LazyColumn(
+            contentPadding = WindowInsets.ime.add(WindowInsets.navigationBars).asPaddingValues(),
             modifier = Modifier.weight(1f),
         ) {
             cityItems.forEach { item ->
