986493ffdaa5cbc537992a3e53683aff12c46a75
Dmitry Popov <popovdd@melonfashion.com>
2023-04-23 00:41:06 +0300

    Add VideoItem

--- app/src/main/java/ru/zarina/zarina/ui/common/components/MediaPager.kt
+++ app/src/main/java/ru/zarina/zarina/ui/common/components/MediaPager.kt
@@ -1,14 +1,27 @@
 package ru.zarina.zarina.ui.common.components
 
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
+import androidx.media3.ui.AspectRatioFrameLayout
+import androidx.media3.ui.PlayerView
 import coil.compose.AsyncImage
 import ru.zarina.zarina.domain.Media
 
@@ -33,6 +46,7 @@ fun MediaPager(
             )
 
             Media.Type.VIDEO -> VideoItem(
+                media = item,
                 modifier = itemModifier,
             )
         }
@@ -54,9 +68,41 @@ private fun ImageItem(
     )
 }
 
+@androidx.annotation.OptIn(UnstableApi::class)
 @Composable
 private fun VideoItem(
+    media: Media,
     modifier: Modifier = Modifier,
 ) {
-    Box(modifier = modifier)
+    val context = LocalContext.current
+    val player = remember(context) {
+        ExoPlayer.Builder(context)
+            .build()
+            .apply {
+                val dataSourceFactory = DefaultDataSource.Factory(context)
+                val source = ProgressiveMediaSource
+                    .Factory(dataSourceFactory)
+                    .createMediaSource(MediaItem.fromUri(media.url.value))
+                setMediaSource(source)
+                playWhenReady = true
+                repeatMode = Player.REPEAT_MODE_ONE
+                prepare()
+            }
+    }
+    DisposableEffect(player) { onDispose { player.release() } }
+    AndroidView(
+        factory = {
+            PlayerView(context).apply {
+                hideController()
+                useController = false
+                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
+                this.player = player
+                layoutParams = FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            }
+        },
+        modifier = modifier
+    )
 }
