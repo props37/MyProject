ecd4711091ef712a8703db9425c7d84d214f91f0
Dmitry Popov <popovdd@melonfashion.com>
2023-04-22 22:58:40 +0300

    Add qualifier

--- app/src/main/java/ru/zarina/zarina/data/product/remote/api/KtorZarinaProductApi.kt
+++ app/src/main/java/ru/zarina/zarina/data/product/remote/api/KtorZarinaProductApi.kt
@@ -4,9 +4,11 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import ru.zarina.zarina.data.product.remote.api.dto.ProductDto
+import ru.zarina.zarina.di.Authorization
 import javax.inject.Inject
 
 class KtorZarinaProductApi @Inject constructor(
+    @Authorization(Authorization.Type.TOKEN)
     private val client: HttpClient,
 ) : IZarinaProductApi {
 
