e58a432356eaedb99b5fcdc726b016c3e44838ee
Dmitry Popov <popovdd@melonfashion.com>
2023-04-23 23:23:21 +0300

    Add loader for VideoPlayer

--- app/src/main/java/ru/zarina/zarina/ui/common/components/VideoPlayer.kt
+++ app/src/main/java/ru/zarina/zarina/ui/common/components/VideoPlayer.kt
@@ -2,12 +2,19 @@ package ru.zarina.zarina.ui.common.components
 
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
@@ -25,6 +32,7 @@ import androidx.media3.exoplayer.source.ProgressiveMediaSource
 import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
 import ru.zarina.zarina.domain.Media
+import ru.zarina.zarina.ui.theme.UiKitTheme
 import timber.log.Timber
 
 @androidx.annotation.OptIn(UnstableApi::class)
@@ -34,6 +42,7 @@ fun VideoPlayer(
     cache: State<Cache?>,
     modifier: Modifier = Modifier,
 ) {
+    val isLoaded = remember { mutableStateOf(false) }
     val context = LocalContext.current
     val player = remember(context, cache) {
         ExoPlayer.Builder(context)
@@ -52,6 +61,11 @@ fun VideoPlayer(
                 setMediaSource(source)
                 playWhenReady = true
                 repeatMode = Player.REPEAT_MODE_ONE
+                addListener(object : Player.Listener {
+                    override fun onPlaybackStateChanged(playbackState: Int) {
+                        if (playbackState == Player.STATE_READY) isLoaded.value = true
+                    }
+                })
                 prepare()
             }
     }
@@ -79,19 +93,32 @@ fun VideoPlayer(
             lifecycleOwner.value.lifecycle.removeObserver(observer)
         }
     }
-    AndroidView(
-        factory = {
-            PlayerView(context).apply {
-                hideController()
-                useController = false
-                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
-                this.player = player
-                layoutParams = FrameLayout.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-            }
-        },
-        modifier = modifier
+    val playerAlpha by animateFloatAsState(
+        targetValue = if (isLoaded.value) 1f else 0f,
+        label = "player alpha"
     )
+    Box(modifier = modifier) {
+        AndroidView(
+            factory = {
+                PlayerView(context).apply {
+                    hideController()
+                    useController = false
+                    resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
+                    this.player = player
+                    layoutParams = FrameLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                }
+            },
+            modifier = Modifier
+                .graphicsLayer { alpha = playerAlpha }
+        )
+        CircularProgressIndicator(
+            color = UiKitTheme.colors.primaryContentColor,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .graphicsLayer { alpha = 1 - playerAlpha }
+        )
+    }
 }
