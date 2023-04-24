27f4c7e75c3b2a6904055e466c5b2c8e90db9327
Dmitry Popov <popovdd@melonfashion.com>
2023-04-24 19:23:31 +0300

    Add ContentPadding to product screen column

--- app/src/main/java/ru/zarina/zarina/ui/screens/product/ProductScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/product/ProductScreen.kt
@@ -7,8 +7,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
@@ -64,6 +67,7 @@ fun ProductScreenContent(
     if (product != null)
         LazyColumn(
             horizontalAlignment = Alignment.CenterHorizontally,
+            contentPadding = WindowInsets.navigationBars.asPaddingValues(),
             modifier = Modifier
                 .fillMaxSize()
                 .background(UiKitTheme.colors.screenBackground)
