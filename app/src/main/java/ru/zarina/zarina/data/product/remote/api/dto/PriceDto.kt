b12808614bbdec03b754646a7ccd555ca8e0571b
Dmitry Popov <popovdd@melonfashion.com>
2023-04-24 11:21:26 +0300

    Add price to Product

---
+++ app/src/main/java/ru/zarina/zarina/data/product/remote/api/dto/PriceDto.kt
@@ -0,0 +1,33 @@
+package ru.zarina.zarina.data.product.remote.api.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import ru.zarina.zarina.domain.Price
+import ru.zarina.zarina.utils.kotlin.isNotNull
+
+@Serializable
+data class PriceDto(
+    @SerialName("common_price")
+    val regularPrice: Int?,
+    @SerialName("has_discount")
+    val isDiscount: Boolean?,
+    @SerialName("discount")
+    val discountPercentage: Int?,
+    @SerialName("discount_price")
+    val discountedPrice: Int?,
+) {
+
+    fun toDomain(): Price? {
+        return if (
+            isNotNull(discountedPrice, "discount_price")
+            && isNotNull(regularPrice, "common_price")
+        )
+            Price(
+                current = discountedPrice,
+                original = regularPrice,
+            )
+        else
+            null
+    }
+
+}
--- app/src/main/java/ru/zarina/zarina/data/product/remote/api/dto/ProductDto.kt
+++ app/src/main/java/ru/zarina/zarina/data/product/remote/api/dto/ProductDto.kt
@@ -1,16 +1,25 @@
 package ru.zarina.zarina.data.product.remote.api.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import ru.zarina.zarina.domain.Product
 import ru.zarina.zarina.utils.kotlin.isNotNull
 
 @Serializable
 data class ProductDto(
+    @SerialName("id")
     val id: String?,
+    @SerialName("media")
     val media: List<MediaDto>?,
+    @SerialName("price")
+    val price: PriceDto,
 ) {
     fun toDomain(): Product? {
-        if (isNotNull(id, "id")) return Product(
+        val price = price.toDomain()
+        if (
+            isNotNull(id, "id")
+            && isNotNull(price, "price")
+        ) return Product(
             id = id,
             media = media?.mapNotNull { it.toDomain() }.orEmpty()
         )
