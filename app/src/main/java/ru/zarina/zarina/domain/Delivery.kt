8c86092b8f2f33ae73231f02397b413ae3f4f46e
Dmitry Popov <popovdd@melonfashion.com>
2023-04-27 16:09:09 +0300

    Add Delivery domain class

---
+++ app/src/main/java/ru/zarina/zarina/domain/Delivery.kt
@@ -0,0 +1,16 @@
+package ru.zarina.zarina.domain
+
+data class Delivery(
+    val cityName: String,
+    val options: List<Option>,
+) {
+
+    data class Option(
+        val type: Type,
+        val name: String,
+        val estimatedTimeDays: Int,
+    ) {
+        enum class Type { EXPRESS, POST, PICKUP }
+    }
+
+}
