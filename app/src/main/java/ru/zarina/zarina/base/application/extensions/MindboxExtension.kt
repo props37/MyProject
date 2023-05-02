256f1740c8f1b496a37e5530fad872cbb21b62fc
Dmitry Popov <popovdd@melonfashion.com>
2023-05-02 11:19:54 +0300

    Add MindboxExtension

---
+++ app/src/main/java/ru/zarina/zarina/base/application/extensions/MindboxExtension.kt
@@ -0,0 +1,30 @@
+package ru.zarina.zarina.base.application.extensions
+
+import android.app.Application
+import cloud.mindbox.mobile_sdk.Mindbox
+import cloud.mindbox.mobile_sdk.MindboxConfiguration
+import ru.zarina.zarina.BuildConfig
+import ru.zarina.zarina.base.application.extensions.base.ApplicationExtension
+import javax.inject.Inject
+
+class MindboxExtension @Inject constructor() : ApplicationExtension {
+
+    override fun install(application: Application) {
+        val configuration = getConfiguration(application)
+        Mindbox.init(application, configuration, emptyList())
+    }
+
+    private fun getConfiguration(application: Application) = MindboxConfiguration.Builder(
+        context = application.applicationContext,
+        domain = DOMAIN,
+        endpointId = BuildConfig.MINDBOX_ENDPOINT,
+    )
+        .shouldCreateCustomer(false)
+        .subscribeCustomerIfCreated(true)
+        .build()
+
+    companion object {
+        private const val DOMAIN = "api.mindbox.ru"
+    }
+
+}
--- app/src/main/java/ru/zarina/zarina/base/application/extensions/base/ExtensionManager.kt
+++ app/src/main/java/ru/zarina/zarina/base/application/extensions/base/ExtensionManager.kt
@@ -1,12 +1,14 @@
 package ru.zarina.zarina.base.application.extensions.base
 
+import ru.zarina.zarina.base.application.extensions.MindboxExtension
 import ru.zarina.zarina.base.application.extensions.TimberExtension
 import javax.inject.Inject
 
 class ExtensionManager @Inject constructor(
     timber: TimberExtension,
+    mindbox: MindboxExtension,
 ) {
 
-    val extensions = listOf(timber)
+    val extensions = listOf(timber, mindbox)
 
 }
