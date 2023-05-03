0412f4e4c8256e23b45378e5833252c29e1e38da
Dmitry Popov <popovdd@melonfashion.com>
2023-05-03 12:03:25 +0300

    Replace regular collections with immutable for city selection screen

--- app/src/main/java/ru/zarina/zarina/ui/common/tooling/preview/providers/ui/CityListItem.kt
+++ app/src/main/java/ru/zarina/zarina/ui/common/tooling/preview/providers/ui/CityListItem.kt
@@ -1,13 +1,14 @@
 package ru.zarina.zarina.ui.common.tooling.preview.providers.ui
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import kotlinx.collections.immutable.persistentListOf
 import ru.zarina.zarina.domain.AddressId
 import ru.zarina.zarina.domain.City
 import ru.zarina.zarina.ui.screens.cityselection.CitySelectionViewModel
 
 class CityListItemProvider : PreviewParameterProvider<List<CitySelectionViewModel.CityListItem>> {
     override val values = sequenceOf(
-        listOf(
+        persistentListOf(
             CitySelectionViewModel.CityListItem.Header("С"),
             CitySelectionViewModel.CityListItem.Item(
                 City(
