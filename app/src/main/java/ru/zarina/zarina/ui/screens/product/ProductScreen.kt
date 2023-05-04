d70c96a5cfad52936f44858fda9f199039a547ed
Dmitry Popov <popovdd@melonfashion.com>
2023-05-04 16:15:45 +0300

    Remove commented code

--- app/src/main/java/ru/zarina/zarina/ui/screens/product/ProductScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/product/ProductScreen.kt
@@ -153,7 +153,6 @@ fun ProductScreenContent(
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .bringIntoViewRequester(completeLookRequester)
-//                                .focusTarget()
                         )
                     if (similarProducts.isNotEmpty())
                         ProductHorizontalSection(
