c4d5258218b28a4fccacd01f12a9e1330cff521a
Dmitry Popov <popovdd@melonfashion.com>
2023-05-05 15:38:48 +0300

    Add product to pickup screen

---
+++ app/src/main/java/ru/zarina/zarina/ui/common/components/HorizontalProductCard.kt
@@ -0,0 +1,134 @@
+package ru.zarina.zarina.ui.common.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import kotlinx.collections.immutable.toImmutableList
+import ru.zarina.zarina.domain.Media
+import ru.zarina.zarina.domain.Product
+import ru.zarina.zarina.ui.common.tooling.preview.providers.domain.ProductProvider
+import ru.zarina.zarina.ui.theme.UiKitTheme
+import ru.zarina.zarina.ui.theme.ZarinaTheme
+
+@Composable
+fun HorizontalProductCard(
+    product: Product,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val inactiveOverlayColor = UiKitTheme.colors.inactiveOverlay
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = onClick,
+            )
+            .drawWithContent {
+                drawContent()
+                if (!product.isAvailable) drawRect(inactiveOverlayColor)
+            }
+    ) {
+        Box(
+            modifier = Modifier
+                .aspectRatio(Media.Defaults.PRODUCT_MEDIA_ASPECT_RATIO)
+                .fillMaxWidth()
+        ) {
+            val image = product.media.firstOrNull { it.type == Media.Type.IMAGE }
+            AsyncImageLoader(
+                url = image?.url?.value,
+                modifier = Modifier.fillMaxSize()
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(8.dp)
+            ) {
+                DiscountBadge(
+                    price = product.price,
+                )
+                if (!product.isAvailable)
+                    OutOfStockBadge()
+            }
+        }
+        Spacer(modifier = Modifier.height(4.dp))
+        Tag(
+            text = product.attributes.firstOrNull().orEmpty(),
+            modifier = Modifier
+        )
+        Text(
+            text = product.name,
+            color = UiKitTheme.colors.primaryContentColor,
+            style = UiKitTheme.typography.productCardName,
+            maxLines = 1,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        val colors = remember(product) { product.colorVariants.map { it.key }.toImmutableList() }
+        val selectedColor = remember(product) {
+            product.colorVariants.entries.firstOrNull { it.value.isCurrent }?.key
+        }
+        SmallColorPicker(
+            colors = colors,
+            selectedColor = selectedColor,
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        ProductPrice(
+            price = product.price,
+            textStyle = UiKitTheme.typography.productCardPrice,
+        )
+    }
+}
+
+@Composable
+private fun Tag(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = text.uppercase(),
+        color = UiKitTheme.colors.primaryContentColor,
+        style = UiKitTheme.typography.productCardTag,
+        modifier = modifier
+    )
+}
+
+@Preview(
+    showBackground = true,
+)
+@Composable
+fun HorizontalProductCardPreview(
+    @PreviewParameter(ProductProvider::class, limit = 1)
+    product: Product,
+) {
+    ZarinaTheme {
+        HorizontalProductCard(
+            product = product,
+            onClick = {},
+        )
+    }
+}
--- app/src/main/java/ru/zarina/zarina/ui/screens/pickup/PickupScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/pickup/PickupScreen.kt
@@ -1,23 +1,36 @@
 package ru.zarina.zarina.ui.screens.pickup
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.Flow
 import ru.zarina.zarina.R
+import ru.zarina.zarina.domain.Product
+import ru.zarina.zarina.ui.common.components.HorizontalProductCard
 import ru.zarina.zarina.ui.common.components.ZarinaScaffold
 import ru.zarina.zarina.ui.common.components.toolbar.CloseButton
 import ru.zarina.zarina.ui.common.components.toolbar.ScreenToolbar
 import ru.zarina.zarina.ui.common.tooling.preview.DensityPreviews
 import ru.zarina.zarina.ui.common.tooling.preview.FontScalePreviews
+import ru.zarina.zarina.ui.common.tooling.preview.providers.domain.ProductProvider
 import ru.zarina.zarina.ui.theme.ZarinaTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PickupScreenContent() {
+fun PickupScreenContent(
+    product: Product?,
+) {
     ZarinaScaffold(
         toolbar = {
             ScreenToolbar(
@@ -26,21 +39,43 @@ fun PickupScreenContent() {
                     CloseButton(onClick = { /*TODO*/ })
                 }
             )
-        }
+        },
     ) {
-
+        if (product != null)
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CityPicker()
+                HorizontalProductCard(
+                    product = product,
+                    onClick = { /*TODO*/ }
+                )
+            }
     }
+}
+
+@Composable
+private fun CityPicker(
+    modifier: Modifier = Modifier,
+) {
+
 }
 
 @Composable
 fun PickupScreen() {
     val viewModel = hiltViewModel<PickupViewModel>()
 
+    val product by viewModel.product.collectAsStateWithLifecycle()
+
     PickupScreenBehavior(
-        sideEffects = viewModel.sideEffects
+        sideEffects = viewModel.sideEffects,
     )
 
-    PickupScreenContent()
+    PickupScreenContent(
+        product = product,
+    )
 }
 
 @Composable
@@ -60,8 +95,13 @@ fun PickupScreenBehavior(
 @FontScalePreviews
 @DensityPreviews
 @Composable
-fun PickupScreenContentPreview() {
+fun PickupScreenContentPreview(
+    @PreviewParameter(ProductProvider::class, limit = 1)
+    product: Product,
+) {
     ZarinaTheme {
-        PickupScreenContent()
+        PickupScreenContent(
+            product = product,
+        )
     }
 }
