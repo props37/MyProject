4dab5b05dea8db0da020026019ca496343246597
Dmitry Popov <popovdd@melonfashion.com>
2023-05-10 15:26:26 +0300

    Implement SelectSizeViewModel api

--- app/src/main/java/ru/zarina/zarina/ui/screens/selectsize/SelectSizeInteractor.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/selectsize/SelectSizeInteractor.kt
@@ -1,5 +1,13 @@
 package ru.zarina.zarina.ui.screens.selectsize
 
+import ru.zarina.zarina.domain.Product
+import ru.zarina.zarina.usecase.catalog.GetProductUseCase
 import javax.inject.Inject
 
-class SelectSizeInteractor @Inject constructor()
+class SelectSizeInteractor @Inject constructor(
+    private val getProductUseCase: GetProductUseCase,
+) {
+
+    suspend fun getProduct(id: Product.Id) = getProductUseCase(GetProductUseCase.Params(id))
+
+}
