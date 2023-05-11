85330fd45d3aee287f95b3d5988bf20037565c95
Dmitry Popov <popovdd@melonfashion.com>
2023-05-11 12:13:37 +0300

    Add GetCityUseCase

---
+++ app/src/main/java/ru/zarina/zarina/usecase/user/GetCity.kt
@@ -0,0 +1,22 @@
+package ru.zarina.zarina.usecase.user
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.first
+import ru.zarina.zarina.base.clean.UseCase
+import ru.zarina.zarina.data.user.UserRepository
+import ru.zarina.zarina.di.Dispatcher
+import ru.zarina.zarina.di.ZarinaDispatcher
+import ru.zarina.zarina.domain.City
+import timber.log.Timber
+import javax.inject.Inject
+
+class GetCityUseCase @Inject constructor(
+    @Dispatcher(ZarinaDispatcher.IO) dispatcher: CoroutineDispatcher,
+    private val userRepository: UserRepository,
+) : UseCase<Unit, City?>(dispatcher) {
+    override suspend fun execute(params: Unit): City? {
+        val city = userRepository.getCity().first()
+        Timber.v("Current user city: $city")
+        return city
+    }
+}
