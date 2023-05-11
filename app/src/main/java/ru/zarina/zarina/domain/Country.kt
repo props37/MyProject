777912cc8b9a6ea1d9cb372515c26184d9f1ccb6
Dmitry Popov <popovdd@melonfashion.com>
2023-05-11 16:33:12 +0300

    Add GetPickupCities usecase

--- app/src/main/java/ru/zarina/zarina/domain/Country.kt
+++ app/src/main/java/ru/zarina/zarina/domain/Country.kt
@@ -4,4 +4,12 @@ data class Country(
     val id: String,
     val name: String,
     val cities: List<City>,
-)
+) {
+
+    val isPickupSupported: Boolean = id == ID_RUSSIA
+
+    companion object {
+        private const val ID_RUSSIA = "949"
+    }
+
+}
