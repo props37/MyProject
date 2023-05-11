635c6e172f66d6e2cc8097a9144e2a1884edaeb6
Dmitry Popov <popovdd@melonfashion.com>
2023-05-11 15:34:17 +0300

    Add shop repository to di

--- app/src/main/java/ru/zarina/zarina/di/DataModule.kt
+++ app/src/main/java/ru/zarina/zarina/di/DataModule.kt
@@ -42,6 +42,12 @@ import ru.zarina.zarina.data.recommendation.remote.IRecommendationRemoteSource
 import ru.zarina.zarina.data.recommendation.remote.MindboxRecommendationRemoteSource
 import ru.zarina.zarina.data.recommendation.remote.api.IMindboxRecommendationApi
 import ru.zarina.zarina.data.recommendation.remote.api.KtorMindboxRecommendationApi
+import ru.zarina.zarina.data.shop.IShopRepository
+import ru.zarina.zarina.data.shop.ShopRepository
+import ru.zarina.zarina.data.shop.remote.IShopRemoteSource
+import ru.zarina.zarina.data.shop.remote.ZarinaShopRemoteSource
+import ru.zarina.zarina.data.shop.remote.api.IZarinaShopApi
+import ru.zarina.zarina.data.shop.remote.api.KtorZarinaShopApi
 import ru.zarina.zarina.data.user.IUserRepository
 import ru.zarina.zarina.data.user.UserRepository
 import ru.zarina.zarina.data.user.local.DataStoreUserLocalSource
@@ -123,5 +129,14 @@ interface DataModule {
 
     @Binds
     fun bindsRecommendationRepository(repository: RecommendationRepository): IRecommendationRepository
+
+    @Binds
+    fun bindsZarinaShopApi(api: KtorZarinaShopApi): IZarinaShopApi
+
+    @Binds
+    fun bindsShopRemoteSource(source: ZarinaShopRemoteSource): IShopRemoteSource
+
+    @Binds
+    fun bindsShopRepository(repository: ShopRepository): IShopRepository
 
 }
