44679f250eef33ca1c397967bb57ab9609b0280f
Dmitry Popov <popovdd@melonfashion.com>
2023-05-12 11:13:01 +0300

    Add filtering to city list

--- app/src/main/java/ru/zarina/zarina/ui/screens/pickup/selectpickupcity/SelectPickupCityViewModel.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/pickup/selectpickupcity/SelectPickupCityViewModel.kt
@@ -3,11 +3,14 @@ package ru.zarina.zarina.ui.screens.pickup.selectpickupcity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toPersistentList
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import ru.zarina.zarina.domain.City
 import ru.zarina.zarina.ui.common.base.ISideEffectSource
@@ -22,12 +25,19 @@ class SelectPickupCityViewModel @Inject constructor(
 ) : ViewModel(),
     ISideEffectSource<SelectPickupCityViewModel.SideEffect> by SideEffectQueue() {
 
-    private val _cities =
-        MutableStateFlow<ImmutableList<SelectCityComponent.CityListItem>>(persistentListOf())
-    val cities: StateFlow<ImmutableList<SelectCityComponent.CityListItem>> = _cities
+    private val _cities = MutableStateFlow<List<City>>(persistentListOf())
 
     private val _query = MutableStateFlow("")
     val query: StateFlow<String> = _query.asStateFlow()
+
+    val cities = combine(_cities, _query) { cities, query ->
+        with(selectCityComponent) {
+            cities
+                .filter { city -> city.name.contains(query, ignoreCase = true) }
+                .toCityListItems(true)
+                .toPersistentList()
+        }
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), persistentListOf())
 
     private val _errorType = MutableStateFlow<SelectCityComponent.ErrorType?>(null)
     val errorType = _errorType.asStateFlow()
@@ -38,7 +48,6 @@ class SelectPickupCityViewModel @Inject constructor(
 
     fun onQueryChange(query: String) {
         _query.value = query
-        // TODO filter list
     }
 
     private fun loadPickupCities() {
@@ -46,9 +55,7 @@ class SelectPickupCityViewModel @Inject constructor(
             // TODO loader
             // TODO errors
             interactor.getPickupCities()
-                .onSuccess {
-                    _cities.value = with(selectCityComponent) { it.toCityListItems(true) }
-                }
+                .onSuccess { _cities.value = it }
         }
     }
 
