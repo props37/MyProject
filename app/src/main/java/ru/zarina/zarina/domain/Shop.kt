3261f11b71db2475feb9f1d5ef05bcc2c8cce9b2
Dmitry Popov <popovdd@melonfashion.com>
2023-05-12 17:50:58 +0300

    Add fields to Shop

--- app/src/main/java/ru/zarina/zarina/domain/Shop.kt
+++ app/src/main/java/ru/zarina/zarina/domain/Shop.kt
@@ -2,4 +2,9 @@ package ru.zarina.zarina.domain
 
 data class Shop(
     val id: String,
+    val name: String,
+    val geoLocation: GeoLocation,
+    val address: String,
+    val phone: String,
+    val schedule: String,
 )
