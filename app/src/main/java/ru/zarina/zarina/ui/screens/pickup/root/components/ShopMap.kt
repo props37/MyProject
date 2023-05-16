87a211a46eee7fa7a557b624d78ea99aa987daf5
Dmitry Popov <popovdd@melonfashion.com>
2023-05-16 16:17:55 +0300

    Add shop markers display

--- app/src/main/java/ru/zarina/zarina/ui/screens/pickup/root/components/ShopMap.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/pickup/root/components/ShopMap.kt
@@ -8,14 +8,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import ru.zarina.zarina.R
 import ru.zarina.zarina.domain.Stock
+import ru.zarina.zarina.ui.common.utils.domain.toLatLng
+import ru.zarina.zarina.utils.maps.getBitmapDescriptor
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
@@ -49,6 +56,16 @@ fun ShopMap(
             properties = properties,
             contentPadding = WindowInsets.navigationBars.asPaddingValues(),
             modifier = Modifier.fillMaxSize()
-        )
+        ) {
+            val context = LocalContext.current
+            val pinBitmapDescriptor =
+                remember(context) { context.getBitmapDescriptor(R.drawable.ic_map_pin_marker) }
+            stocks.forEach { stock ->
+                Marker(
+                    state = MarkerState(position = stock.shop.geoLocation.toLatLng()),
+                    icon = pinBitmapDescriptor,
+                )
+            }
+        }
     }
 }
