48d1f898b966d04b935feeb0c7a1edc856950fb6
Dmitry Popov <popovdd@melonfashion.com>
2023-05-16 15:30:45 +0300

    Improve map display
    * add content padding
    * enable location detection

--- app/src/main/java/ru/zarina/zarina/ui/screens/pickup/root/components/ShopMap.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/pickup/root/components/ShopMap.kt
@@ -1,10 +1,14 @@
 package ru.zarina.zarina.ui.screens.pickup.root.components
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
 import ru.zarina.zarina.domain.Stock
 
 @Composable
@@ -16,6 +20,11 @@ fun ShopMap(
         modifier = modifier.fillMaxSize()
     ) {
         GoogleMap(
+            properties = MapProperties(
+                isBuildingEnabled = true,
+                isMyLocationEnabled = true,
+            ),
+            contentPadding = WindowInsets.navigationBars.asPaddingValues(),
             modifier = Modifier.fillMaxSize()
         )
     }
