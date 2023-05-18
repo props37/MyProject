2ebd6d2117ff2304ead87e1c8b3cdb408a491f88
Dmitry Popov <popovdd@melonfashion.com>
2023-05-18 14:20:42 +0300

    Add pickup details screen base

---
+++ app/src/main/java/ru/zarina/zarina/ui/screens/pickup/details/DetailsScreen.kt
@@ -0,0 +1,49 @@
+package ru.zarina.zarina.ui.screens.pickup.details
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.flow.Flow
+import ru.zarina.zarina.ui.common.tooling.preview.DensityPreviews
+import ru.zarina.zarina.ui.common.tooling.preview.FontScalePreviews
+import ru.zarina.zarina.ui.theme.ZarinaTheme
+
+@Composable
+fun DetailsScreenContent() {
+
+}
+
+@Composable
+fun DetailsScreen() {
+    val viewModel = hiltViewModel<DetailsViewModel>()
+
+    DetailsScreenBehavior(
+        sideEffects = viewModel.sideEffects
+    )
+
+    DetailsScreenContent()
+}
+
+@Composable
+fun DetailsScreenBehavior(
+    sideEffects: Flow<DetailsViewModel.SideEffect>,
+) {
+    LaunchedEffect(sideEffects) {
+        sideEffects.collect { effect ->
+            when (effect) {
+                else -> TODO()
+            }
+        }
+    }
+}
+
+@Preview
+@FontScalePreviews
+@DensityPreviews
+@Composable
+fun DetailsScreenContentPreview() {
+    ZarinaTheme {
+        DetailsScreenContent()
+    }
+}
