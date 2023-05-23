3103e378e1fee4490ed605a256a9450a4d4d5c17
Dmitry Popov <popovdd@melonfashion.com>
2023-05-23 15:29:02 +0300

    Add SubscribeScreen basic layout

--- app/src/main/java/ru/zarina/zarina/ui/screens/subscribe/SubscribeScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/subscribe/SubscribeScreen.kt
@@ -1,28 +1,119 @@
 package ru.zarina.zarina.ui.screens.subscribe
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.Flow
-import ru.zarina.zarina.ui.common.tooling.preview.DensityPreviews
-import ru.zarina.zarina.ui.common.tooling.preview.FontScalePreviews
+import ru.zarina.zarina.R
+import ru.zarina.zarina.ui.common.components.ZarinaScaffold
+import ru.zarina.zarina.ui.common.components.form.Input
+import ru.zarina.zarina.ui.common.components.toolbar.BackButton
+import ru.zarina.zarina.ui.common.components.toolbar.ScreenToolbar
+import ru.zarina.zarina.ui.theme.UiKitTheme
 import ru.zarina.zarina.ui.theme.ZarinaTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SubscribeScreenContent() {
-
+fun SubscribeScreenContent(
+    name: String,
+    onNameChange: (String) -> Unit,
+    email: String,
+    onEmailChange: (String) -> Unit,
+    isSwitchChecked: Boolean,
+    onSwitchCheckedChange: (Boolean) -> Unit,
+    onBackClick: () -> Unit,
+) {
+    ZarinaScaffold(
+        toolbar = {
+            ScreenToolbar(
+                title = stringResource(R.string.subscription_to_product),
+                startIcon = {
+                    BackButton(onClick = onBackClick)
+                }
+            )
+        },
+    ) {
+        Column(
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+                .verticalScroll(rememberScrollState()),
+        ) {
+            Text(
+                text = stringResource(R.string.leave_your_contacts),
+                style = UiKitTheme.typography.circle1618,
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+            Input(
+                value = name,
+                onValueChange = onNameChange,
+                hint = stringResource(R.string.name),
+                modifier = Modifier
+                    .padding(bottom = 8.dp)
+                    .fillMaxWidth(),
+            )
+            Input(
+                value = email,
+                onValueChange = onEmailChange,
+                hint = stringResource(R.string.email),
+                modifier = Modifier
+                    .padding(bottom = 8.dp)
+                    .fillMaxWidth(),
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "", // TODO
+                    modifier = Modifier.weight(1f),
+                )
+            }
+        }
+    }
 }
 
 @Composable
 fun SubscribeScreen() {
     val viewModel = hiltViewModel<SubscribeViewModel>()
 
+    val name by viewModel.name.collectAsStateWithLifecycle()
+    val email by viewModel.email.collectAsStateWithLifecycle()
+    val isSwitchChecked by viewModel.isSwitchChecked.collectAsStateWithLifecycle()
+
     SubscribeScreenBehavior(
         sideEffects = viewModel.sideEffects
     )
 
-    SubscribeScreenContent()
+    SubscribeScreenContent(
+        name = name,
+        onNameChange = viewModel::onNameChange,
+        email = email,
+        onEmailChange = viewModel::onEmailChange,
+        isSwitchChecked = isSwitchChecked,
+        onSwitchCheckedChange = viewModel::onSwitchCheckedChange,
+        onBackClick = viewModel::onBackClick,
+    )
 }
 
 @Composable
@@ -39,11 +130,18 @@ fun SubscribeScreenBehavior(
 }
 
 @Preview
-@FontScalePreviews
-@DensityPreviews
 @Composable
 fun SubscribeScreenContentPreview() {
     ZarinaTheme {
-        SubscribeScreenContent()
+        var isSwitchChecked by remember { mutableStateOf(false) }
+        SubscribeScreenContent(
+            name = "Михаил",
+            onNameChange = {},
+            email = "mikhail@gmail.com",
+            onEmailChange = {},
+            isSwitchChecked = isSwitchChecked,
+            onSwitchCheckedChange = { isSwitchChecked = it },
+            onBackClick = {},
+        )
     }
 }
--- app/src/main/java/ru/zarina/zarina/ui/screens/subscribe/SubscribeViewModel.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/subscribe/SubscribeViewModel.kt
@@ -2,6 +2,8 @@ package ru.zarina.zarina.ui.screens.subscribe
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import ru.zarina.zarina.ui.common.base.ISideEffectSource
 import ru.zarina.zarina.ui.common.base.SideEffectQueue
 import javax.inject.Inject
@@ -11,6 +13,31 @@ class SubscribeViewModel @Inject constructor(
     private val interactor: SubscribeInteractor,
 ) : ViewModel(),
     ISideEffectSource<SubscribeViewModel.SideEffect> by SideEffectQueue() {
+
+    private val _name = MutableStateFlow("")
+    val name = _name.asStateFlow()
+
+    private val _email = MutableStateFlow("")
+    val email = _email.asStateFlow()
+
+    private val _isSwitchChecked = MutableStateFlow(false)
+    val isSwitchChecked = _isSwitchChecked.asStateFlow()
+
+    fun onNameChange(name: String) {
+        _name.value = name
+    }
+
+    fun onEmailChange(email: String) {
+        _email.value = email
+    }
+
+    fun onSwitchCheckedChange(isChecked: Boolean) {
+        _isSwitchChecked.value = isChecked
+    }
+
+    fun onBackClick() {
+        // TODO
+    }
 
     sealed interface SideEffect : ISideEffectSource.ISideEffect
 
