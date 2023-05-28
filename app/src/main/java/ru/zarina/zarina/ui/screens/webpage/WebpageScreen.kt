5bd25046b295a2bf629dcc4c24e9dc293cee4a6e
Dmitry Popov <popovdd@melonfashion.com>
2023-05-28 21:43:36 +0300

    Add WebView

--- app/src/main/java/ru/zarina/zarina/ui/screens/webpage/WebpageScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/webpage/WebpageScreen.kt
@@ -1,28 +1,42 @@
 package ru.zarina.zarina.ui.screens.webpage
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
 import kotlinx.coroutines.flow.Flow
-import ru.zarina.zarina.ui.common.tooling.preview.DensityPreviews
-import ru.zarina.zarina.ui.common.tooling.preview.FontScalePreviews
 import ru.zarina.zarina.ui.theme.ZarinaTheme
 
 @Composable
-fun WebpageScreenContent() {
-
+fun WebpageScreenContent(
+    url: String,
+) {
+    val state = rememberWebViewState(url = url)
+    WebView(
+        state = state,
+        modifier = Modifier.fillMaxSize()
+    )
 }
 
 @Composable
 fun WebpageScreen() {
     val viewModel = hiltViewModel<WebpageViewModel>()
 
+    val url by viewModel.url.collectAsStateWithLifecycle()
+
     WebpageScreenBehavior(
         sideEffects = viewModel.sideEffects
     )
 
-    WebpageScreenContent()
+    WebpageScreenContent(
+        url = url,
+    )
 }
 
 @Composable
@@ -39,11 +53,11 @@ fun WebpageScreenBehavior(
 }
 
 @Preview
-@FontScalePreviews
-@DensityPreviews
 @Composable
 fun WebpageScreenContentPreview() {
     ZarinaTheme {
-        WebpageScreenContent()
+        WebpageScreenContent(
+            url = "https://zarina.ru/help/privacy-policy/"
+        )
     }
 }
--- app/src/main/java/ru/zarina/zarina/ui/screens/webpage/WebpageViewModel.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/webpage/WebpageViewModel.kt
@@ -1,16 +1,21 @@
 package ru.zarina.zarina.ui.screens.webpage
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ru.zarina.zarina.ui.common.base.ISideEffectSource
 import ru.zarina.zarina.ui.common.base.SideEffectQueue
+import ru.zarina.zarina.ui.navigation.destinations.Destinations
 import javax.inject.Inject
 
 @HiltViewModel
 class WebpageViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
     private val interactor: WebpageInteractor,
 ) : ViewModel(),
     ISideEffectSource<WebpageViewModel.SideEffect> by SideEffectQueue() {
+
+    val url = savedStateHandle.getStateFlow(Destinations.Webpage.ARGUMENT_URL, "")
 
     sealed interface SideEffect : ISideEffectSource.ISideEffect
 
