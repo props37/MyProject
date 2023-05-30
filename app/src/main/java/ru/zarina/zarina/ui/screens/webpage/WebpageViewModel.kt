ba0acfa21db2b69f06e2090f44939b9260aa4684
Dmitry Popov <popovdd@melonfashion.com>
2023-05-30 14:51:22 +0300

    Add authorization header

--- app/src/main/java/ru/zarina/zarina/ui/screens/webpage/WebpageViewModel.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/webpage/WebpageViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.collections.immutable.toPersistentMap
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import ru.zarina.zarina.ui.common.base.ISideEffectSource
 import ru.zarina.zarina.ui.common.base.SideEffectQueue
 import ru.zarina.zarina.ui.navigation.destinations.Destinations
@@ -15,13 +17,27 @@ import javax.inject.Inject
 @HiltViewModel
 class WebpageViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    interactor: WebpageInteractor,
+    private val interactor: WebpageInteractor,
 ) : ViewModel(),
     ISideEffectSource<WebpageViewModel.SideEffect> by SideEffectQueue() {
 
-    val headers = MutableStateFlow(interactor.getUserAgentHeaders())
+    private val _headers = MutableStateFlow(interactor.getUserAgentHeaders())
+    val headers = _headers
         .mapState(viewModelScope) { it.toPersistentMap() }
     val url = savedStateHandle.getStateFlow(Destinations.Webpage.ARGUMENT_URL, "")
+
+    init {
+        setupAuthorizationToken()
+    }
+
+    private fun setupAuthorizationToken() {
+        viewModelScope.launch {
+            interactor.getAuthorizationToken()
+                .onSuccess { token ->
+                    _headers.update { it + ("Authorization" to "Bearer ${token.token}") }
+                }
+        }
+    }
 
     sealed interface SideEffect : ISideEffectSource.ISideEffect
 
