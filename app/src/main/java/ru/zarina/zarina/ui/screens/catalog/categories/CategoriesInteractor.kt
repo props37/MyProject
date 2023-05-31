9b71bf30bbe135613e77d0737e69593ac02437c3
Dmitry Popov <popovdd@melonfashion.com>
2023-05-31 14:23:28 +0300

    Load categories

--- app/src/main/java/ru/zarina/zarina/ui/screens/catalog/categories/CategoriesInteractor.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/catalog/categories/CategoriesInteractor.kt
@@ -1,5 +1,11 @@
 package ru.zarina.zarina.ui.screens.catalog.categories
 
+import ru.zarina.zarina.usecase.catalog.GetCategoriesUseCase
+import ru.zarina.zarina.utils.clean.invoke
 import javax.inject.Inject
 
-class CategoriesInteractor @Inject constructor()
+class CategoriesInteractor @Inject constructor(
+    private val getCategoriesUseCase: GetCategoriesUseCase,
+) {
+    suspend fun getCategories() = getCategoriesUseCase()
+}
--- app/src/main/java/ru/zarina/zarina/ui/screens/catalog/categories/CategoriesViewModel.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/catalog/categories/CategoriesViewModel.kt
@@ -1,9 +1,15 @@
 package ru.zarina.zarina.ui.screens.catalog.categories
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import ru.zarina.zarina.domain.Category
 import ru.zarina.zarina.ui.common.base.ISideEffectSource
 import ru.zarina.zarina.ui.common.base.SideEffectQueue
+import ru.zarina.zarina.utils.coroutine.mapState
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,6 +17,16 @@ class CategoriesViewModel @Inject constructor(
     private val interactor: CategoriesInteractor,
 ) : ViewModel(),
     ISideEffectSource<CategoriesViewModel.SideEffect> by SideEffectQueue() {
+
+    private val categoriesResult = MutableStateFlow<Result<List<Category>>?>(null)
+    val categories = categoriesResult
+        .mapState(viewModelScope) { it?.getOrNull().orEmpty().toPersistentList() }
+
+    init {
+        viewModelScope.launch {
+            categoriesResult.value = interactor.getCategories()
+        }
+    }
 
     sealed interface SideEffect : ISideEffectSource.ISideEffect
 
