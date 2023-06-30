ca52750b6ea50523a3812f0608d5c82581864d7e
Dmitry Popov <popovdd@melonfashion.com>
2023-06-30 12:24:07 +0300

    Add appModule

--- app/src/main/java/ru/zarina/zarina/ZarinaApplication.kt
+++ app/src/main/java/ru/zarina/zarina/ZarinaApplication.kt
@@ -5,15 +5,8 @@ import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
-import org.koin.ksp.generated.defaultModule
-import org.koin.ksp.generated.module
 import ru.zarina.zarina.base.application.extensions.base.ExtensionManager
-import ru.zarina.zarina.di.CoroutineModule
-import ru.zarina.zarina.di.DataStoreModule
-import ru.zarina.zarina.di.DatabaseModule
-import ru.zarina.zarina.di.NetworkModule
-import ru.zarina.zarina.di.PlayServicesModule
-import ru.zarina.zarina.di.PlayerModule
+import ru.zarina.zarina.di.appModule
 
 class ZarinaApplication : Application() {
 
@@ -25,15 +18,7 @@ class ZarinaApplication : Application() {
         startKoin {
             androidLogger()
             androidContext(this@ZarinaApplication)
-            modules(
-                defaultModule,
-                CoroutineModule().module,
-                DatabaseModule().module,
-                DataStoreModule().module,
-                NetworkModule().module,
-                PlayerModule().module,
-                PlayServicesModule().module
-            )
+            modules(appModule)
         }
 
         installExtensions()
