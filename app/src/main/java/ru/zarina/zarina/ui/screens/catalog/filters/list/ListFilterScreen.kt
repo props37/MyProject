b00d2c40059bb33e6aeac967f9f8f333f0f48bb0
Dmitry Popov <popovdd@melonfashion.com>
2023-07-06 18:19:08 +0300

    Add color circle to ListItem

--- app/src/main/java/ru/zarina/zarina/ui/screens/catalog/filters/list/ListFilterScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/catalog/filters/list/ListFilterScreen.kt
@@ -2,14 +2,22 @@ package ru.zarina.zarina.ui.screens.catalog.filters.list
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -18,6 +26,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,6 +42,7 @@ import ru.zarina.zarina.R
 import ru.zarina.zarina.domain.ListFilter
 import ru.zarina.zarina.ui.common.tooling.preview.DensityPreviews
 import ru.zarina.zarina.ui.common.tooling.preview.FontScalePreviews
+import ru.zarina.zarina.ui.common.utils.domain.toColorOr
 import ru.zarina.zarina.ui.theme.UiKitTheme
 import ru.zarina.zarina.ui.theme.ZarinaTheme
 
@@ -68,9 +79,20 @@ fun FilterListItem(
     Row(
         modifier = modifier
             .clickable(onClick = onClick)
+            .height(IntrinsicSize.Min)
             .padding(16.dp),
     ) {
-        // TODO add color circle
+        if (item.color != null) {
+            Box(
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .fillMaxHeight()
+                    .aspectRatio(1f)
+                    .clip(shape = CircleShape)
+                    .background(color = item.color.toColorOr(Color.Transparent))
+                    .border(2.dp, UiKitTheme.colors.colorPickerCircleBorder, CircleShape),
+            )
+        }
         Text(
             text = item.name,
             style = UiKitTheme.typography.circle1718,
