37bd8d2a09cf71e30e157afe52ff4a37998a7e3e
Dmitry Popov <popovdd@melonfashion.com>
2023-07-10 11:35:54 +0300

    Match ShopItem design

--- app/src/main/java/ru/zarina/zarina/ui/screens/catalog/filters/components/items/ShopItem.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/catalog/filters/components/items/ShopItem.kt
@@ -1,12 +1,15 @@
 package ru.zarina.zarina.ui.screens.catalog.filters.components.items
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -22,23 +25,32 @@ fun ShopItem(
     modifier: Modifier = Modifier,
 ) {
     Row(
+        verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
             .clickable(onClick = onClick)
-            .padding(16.dp)
+            .padding(horizontal = 16.dp, vertical = if (shop == null) 16.dp else 8.dp),
     ) {
-        Text(
-            text = stringResource(id = R.string.select_store),
-            style = UiKitTheme.typography.circle1718,
-            color = UiKitTheme.colors.primaryContentColor,
-            modifier = Modifier.padding(end = 8.dp)
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        Text(
-            text = shop?.name.orEmpty(),
-            style = UiKitTheme.typography.circle1718,
-            color = UiKitTheme.colors.hint,
-            modifier = Modifier.padding(end = 8.dp)
-        )
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            if (shop != null) {
+                Text(
+                    text = stringResource(id = R.string.pickup_at_shop),
+                    style = UiKitTheme.typography.circle1316,
+                    color = UiKitTheme.colors.hint,
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+            }
+            val primaryText =
+                shop?.name ?: stringResource(id = R.string.select_store)
+            Text(
+                text = primaryText,
+                style = UiKitTheme.typography.circle1718,
+                color = UiKitTheme.colors.primaryContentColor,
+                modifier = Modifier.padding(end = 8.dp)
+            )
+        }
         Icon(
             painter = painterResource(id = R.drawable.ic_chevron_right_24),
             contentDescription = null,
