4762fd4734bb3c11ea9840cc65187d3f505e4fb6
Dmitry Popov <popovdd@melonfashion.com>
2023-07-11 10:50:01 +0300

    Add ZarinaBottomNavigation

---
+++ app/src/main/java/ru/zarina/zarina/ui/common/components/ZarinaBottomNavigation.kt
@@ -0,0 +1,14 @@
+package ru.zarina.zarina.ui.common.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ZarinaBottomNavigation(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier,
+    )
+}
--- app/src/main/java/ru/zarina/zarina/ui/navigation/ZarinaNavigation.kt
+++ app/src/main/java/ru/zarina/zarina/ui/navigation/ZarinaNavigation.kt
@@ -1,16 +1,26 @@
 package ru.zarina.zarina.ui.navigation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.google.accompanist.navigation.material.ModalBottomSheetLayout
 import com.google.accompanist.navigation.material.rememberBottomSheetNavigator
+import ru.zarina.zarina.ui.common.components.ZarinaBottomNavigation
 import ru.zarina.zarina.ui.navigation.base.Destination
 import ru.zarina.zarina.ui.navigation.graphs.catalogGraph
 import ru.zarina.zarina.ui.navigation.graphs.orphans
 import ru.zarina.zarina.ui.navigation.graphs.pickupGraph
 import ru.zarina.zarina.ui.navigation.graphs.subscribeGraph
+import ru.zarina.zarina.ui.theme.UiKitTheme
 
 @OptIn(ExperimentalMaterialNavigationApi::class)
 @Composable
@@ -21,14 +31,28 @@ fun ZarinaNavigation(
     val navController = rememberNavController(bottomSheetNavigator)
 
     ModalBottomSheetLayout(bottomSheetNavigator) {
-        NavHost(
-            navController = navController,
-            startDestination = startDestination.routeSchema,
+        Column(
+            modifier = Modifier.fillMaxSize(),
         ) {
-            orphans(navController)
-            pickupGraph(navController)
-            subscribeGraph(navController)
-            catalogGraph(navController)
+            NavHost(
+                navController = navController,
+                startDestination = startDestination.routeSchema,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+            ) {
+                orphans(navController)
+                pickupGraph(navController)
+                subscribeGraph(navController)
+                catalogGraph(navController)
+            }
+            ZarinaBottomNavigation(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .background(UiKitTheme.colors.disabled)
+                    .navigationBarsPadding(),
+            )
         }
     }
 }
