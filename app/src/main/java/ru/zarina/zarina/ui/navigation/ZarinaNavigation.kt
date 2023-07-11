35d2d24142f63069768c7e4c9120c4a95a6ec205
Dmitry Popov <popovdd@melonfashion.com>
2023-07-11 10:42:53 +0300

    Move orphaned destinations into separate extension

--- app/src/main/java/ru/zarina/zarina/ui/navigation/ZarinaNavigation.kt
+++ app/src/main/java/ru/zarina/zarina/ui/navigation/ZarinaNavigation.kt
@@ -7,18 +7,10 @@ import com.google.accompanist.navigation.material.ExperimentalMaterialNavigation
 import com.google.accompanist.navigation.material.ModalBottomSheetLayout
 import com.google.accompanist.navigation.material.rememberBottomSheetNavigator
 import ru.zarina.zarina.ui.navigation.base.Destination
-import ru.zarina.zarina.ui.navigation.base.composableDestination
-import ru.zarina.zarina.ui.navigation.destinations.Catalog
-import ru.zarina.zarina.ui.navigation.destinations.Destinations
-import ru.zarina.zarina.ui.navigation.destinations.Pickup
 import ru.zarina.zarina.ui.navigation.graphs.catalogGraph
+import ru.zarina.zarina.ui.navigation.graphs.orphans
 import ru.zarina.zarina.ui.navigation.graphs.pickupGraph
 import ru.zarina.zarina.ui.navigation.graphs.subscribeGraph
-import ru.zarina.zarina.ui.screens.home.HomeScreen
-import ru.zarina.zarina.ui.screens.onboarding.OnboardingScreen
-import ru.zarina.zarina.ui.screens.product.ProductScreen
-import ru.zarina.zarina.ui.screens.selectcity.SelectCityScreen
-import ru.zarina.zarina.ui.screens.webpage.WebpageScreen
 
 @OptIn(ExperimentalMaterialNavigationApi::class)
 @Composable
@@ -33,62 +25,7 @@ fun ZarinaNavigation(
             navController = navController,
             startDestination = startDestination.routeSchema,
         ) {
-            composableDestination(Destinations.Home) {
-                HomeScreen(
-                    showProduct = { productId ->
-                        val arguments = Destinations.Product.Arguments(
-                            productId = productId,
-                        )
-                        navController.navigate(Destinations.Product.createRoute(arguments))
-                    },
-                    showCatalog = {
-                        navController.navigate(Catalog.createRoute(Unit))
-                    }
-                )
-            }
-            composableDestination(Destinations.Onboarding) {
-                OnboardingScreen(
-                    showHome = {
-                        navController.navigate(Destinations.Home.route) { popUpTo(0) }
-                    },
-                    showSelectCity = {
-                        navController.navigate(Destinations.SelectCity.route)
-                    }
-                )
-            }
-            composableDestination(Destinations.SelectCity) {
-                SelectCityScreen(
-                    showHome = {
-                        navController.navigate(Destinations.Home.route) { popUpTo(0) }
-                    }
-                )
-            }
-            composableDestination(Destinations.Product) {
-                ProductScreen(
-                    showProduct = { productId ->
-                        val arguments = Destinations.Product.Arguments(
-                            productId = productId,
-                        )
-                        navController.navigate(Destinations.Product.createRoute(arguments))
-                    },
-                    showPickup = { productId ->
-                        val arguments = Pickup.Arguments(
-                            productId = productId,
-                        )
-                        navController.navigate(Pickup.createRoute(arguments))
-                    },
-                    goBack = {
-                        navController.popBackStack(Destinations.Product.routeSchema, true)
-                    }
-                )
-            }
-            composableDestination(Destinations.Webpage) {
-                WebpageScreen(
-                    goBack = {
-                        navController.popBackStack(Destinations.Webpage.routeSchema, true)
-                    }
-                )
-            }
+            orphans(navController)
             pickupGraph(navController)
             subscribeGraph(navController)
             catalogGraph(navController)
