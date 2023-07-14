b2d1ae87bd35392bfe9baae01b20685d50c582a2
Dmitry Popov <popovdd@melonfashion.com>
2023-07-14 16:17:36 +0300

    Add banners to ViewModel

--- app/src/main/java/ru/zarina/zarina/ui/screens/home/HomeInteractor.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/home/HomeInteractor.kt
@@ -1,6 +1,12 @@
 package ru.zarina.zarina.ui.screens.home
 
 import org.koin.core.annotation.Factory
+import ru.zarina.zarina.usecase.content.GetBannersUseCase
+import ru.zarina.zarina.utils.clean.invoke
 
 @Factory
-class HomeInteractor
+class HomeInteractor(
+    private val getBannersUseCase: GetBannersUseCase,
+) {
+    suspend fun getBanners() = getBannersUseCase()
+}
--- app/src/main/java/ru/zarina/zarina/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/home/HomeViewModel.kt
@@ -1,7 +1,16 @@
 package ru.zarina.zarina.ui.screens.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import org.koin.android.annotation.KoinViewModel
+import ru.zarina.zarina.domain.Banner
 import ru.zarina.zarina.domain.Product
 import ru.zarina.zarina.ui.common.base.ISideEffectSource
 import ru.zarina.zarina.ui.common.base.SideEffectQueue
@@ -12,8 +21,29 @@ class HomeViewModel(
 ) : ViewModel(),
     ISideEffectSource<HomeViewModel.SideEffect> by SideEffectQueue() {
 
+    private val _banners = MutableStateFlow<Result<List<Banner>>?>(null)
+    val banners = _banners
+        .map { it?.getOrNull().orEmpty().toPersistentList() }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), persistentListOf())
+
+    // TODO add error display
+
+    init {
+        loadBanners()
+    }
+
+    private fun loadBanners() {
+        viewModelScope.launch {
+            _banners.value = interactor.getBanners()
+        }
+    }
+
     fun onProductClick() {
         sideEffect(SideEffect.ShowProduct(Product.Id("1329404704-50")))
+    }
+
+    fun onBannerClick(banner: Banner) {
+        // TODO handle banner click
     }
 
     sealed interface SideEffect : ISideEffectSource.ISideEffect {
