6183f326c96f6415c8241c2526d6fd69a5143726
Dmitry Popov <popovdd@melonfashion.com>
2023-07-19 15:52:46 +0300

    Fix FavoritesRepository

--- app/src/main/java/ru/zarina/zarina/data/favorites/FavoritesRepository.kt
+++ app/src/main/java/ru/zarina/zarina/data/favorites/FavoritesRepository.kt
@@ -1,11 +1,13 @@
 package ru.zarina.zarina.data.favorites
 
 import org.koin.core.annotation.Factory
+import ru.zarina.zarina.data.favorites.remote.IFavoritesRemoteSource
 import ru.zarina.zarina.domain.Product
 
 @Factory
-class FavoritesRepository : IFavoritesRepository {
-    override suspend fun setIsFavorite(product: Product, isFavorite: Boolean) {
-        TODO("Not yet implemented")
-    }
+class FavoritesRepository(
+    private val remote: IFavoritesRemoteSource,
+) : IFavoritesRepository {
+    override suspend fun setIsFavorite(product: Product, isFavorite: Boolean) =
+        remote.setIsFavorite(product, isFavorite)
 }
