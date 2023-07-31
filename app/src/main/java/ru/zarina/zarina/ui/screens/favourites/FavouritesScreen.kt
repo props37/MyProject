0d114f5b2e918ec0e621ff3bb7eeea8ec6d11ce2
Dmitry Popov <popovdd@melonfashion.com>
2023-07-31 17:13:28 +0300

    Display favorites

--- app/src/main/java/ru/zarina/zarina/ui/screens/favourites/FavouritesScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/favourites/FavouritesScreen.kt
@@ -1,17 +1,25 @@
 package ru.zarina.zarina.ui.screens.favourites
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.itemContentType
+import androidx.paging.compose.itemKey
 import kotlinx.coroutines.flow.Flow
 import org.koin.androidx.compose.koinViewModel
 import ru.zarina.zarina.R
 import ru.zarina.zarina.domain.Product
 import ru.zarina.zarina.ui.common.behavior.navigationbar.NavigationBarState
+import ru.zarina.zarina.ui.common.components.ProductRowCard
 import ru.zarina.zarina.ui.common.components.ZarinaScaffold
+import ru.zarina.zarina.ui.common.components.bottomNavigationPaddingValues
 import ru.zarina.zarina.ui.common.components.toolbar.ScreenToolbar
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -19,15 +27,34 @@ import ru.zarina.zarina.ui.common.components.toolbar.ScreenToolbar
 fun FavoritesScreenContent(
     favorites: LazyPagingItems<Product>,
 ) {
+    val productListState = rememberLazyListState()
     ZarinaScaffold(
         toolbar = {
             ScreenToolbar(
-                title = stringResource(id = R.string.favourites)
-                // TODO elevation on scroll
+                title = stringResource(id = R.string.favourites),
+                isElevated = productListState.canScrollBackward,
             )
         }
     ) {
-
+        LazyColumn(
+            state = productListState,
+            contentPadding = bottomNavigationPaddingValues(),
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            items(
+                count = favorites.itemCount,
+                key = favorites.itemKey { it.id.value },
+                contentType = favorites.itemContentType { null }
+            ) { index ->
+                val product = favorites[index]
+                if (product != null)
+                    ProductRowCard(
+                        product = product,
+                        onClick = {}, // TODO
+                        onFavoriteChange = {}, // TODO
+                    )
+            }
+        }
     }
 }
 
