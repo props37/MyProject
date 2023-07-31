fbbafc6c707e5e04fa9492acee390ec2cb96f592
Dmitry Popov <popovdd@melonfashion.com>
2023-07-31 18:19:48 +0300

    Add empty favorites placeholder

--- app/src/main/java/ru/zarina/zarina/ui/screens/favourites/FavouritesScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/favourites/FavouritesScreen.kt
@@ -1,10 +1,12 @@
 package ru.zarina.zarina.ui.screens.favourites
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -18,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemContentType
@@ -27,9 +30,13 @@ import kotlinx.coroutines.flow.Flow
 import org.koin.androidx.compose.koinViewModel
 import ru.zarina.zarina.R
 import ru.zarina.zarina.domain.Product
+import ru.zarina.zarina.ui.common.base.ErrorState
+import ru.zarina.zarina.ui.common.base.Text
 import ru.zarina.zarina.ui.common.behavior.navigationbar.NavigationBarState
+import ru.zarina.zarina.ui.common.components.ModalError
 import ru.zarina.zarina.ui.common.components.ProductRowCard
 import ru.zarina.zarina.ui.common.components.ZarinaScaffold
+import ru.zarina.zarina.ui.common.components.bottomNavigationPadding
 import ru.zarina.zarina.ui.common.components.bottomNavigationPaddingValues
 import ru.zarina.zarina.ui.common.components.toolbar.ScreenToolbar
 import ru.zarina.zarina.ui.theme.UiKitTheme
@@ -52,33 +59,56 @@ fun FavoritesScreenContent(
             )
         }
     ) {
-        LazyColumn(
-            state = productListState,
-            contentPadding = bottomNavigationPaddingValues() + WindowInsets.navigationBars.asPaddingValues(),
-            modifier = Modifier.fillMaxSize(),
-        ) {
-            items(
-                count = favorites.itemCount,
-                key = favorites.itemKey { it.id.value },
-                contentType = favorites.itemContentType { null }
-            ) { index ->
-                Divider(
-                    thickness = 1.dp,
-                    color = UiKitTheme.colors.listDivider,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
-                )
-                val product = favorites[index]
-                if (product != null)
-                    ProductRowCard(
-                        product = product,
-                        onClick = { onProductClick(product) },
-                        onFavoriteChange = { onFavoriteChange(product, it) },
-                        isFavoriteShaking = shakingFavorites.contains(product.id)
-                    )
-            }
+        val isLoading =
+            favorites.loadState.append == LoadState.Loading || favorites.loadState.refresh == LoadState.Loading
+        val errorState = when {
+            !isLoading && favorites.itemCount == 0 -> ErrorState(
+                icon = R.drawable.ic_heart_96,
+                title = Text.Resource(R.string.favorites_are_empty),
+                subtitle = Text.Resource(R.string.save_interesting_products_to_favorites),
+                isButtonVisible = false,
+                buttonText = null
+            )
+
+            else -> null
         }
+        if (errorState == null)
+            LazyColumn(
+                state = productListState,
+                contentPadding = bottomNavigationPaddingValues() + WindowInsets.navigationBars.asPaddingValues(),
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                items(
+                    count = favorites.itemCount,
+                    key = favorites.itemKey { it.id.value },
+                    contentType = favorites.itemContentType { null }
+                ) { index ->
+                    Divider(
+                        thickness = 1.dp,
+                        color = UiKitTheme.colors.listDivider,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                    )
+                    val product = favorites[index]
+                    if (product != null)
+                        ProductRowCard(
+                            product = product,
+                            onClick = { onProductClick(product) },
+                            onFavoriteChange = { onFavoriteChange(product, it) },
+                            isFavoriteShaking = shakingFavorites.contains(product.id)
+                        )
+                }
+            }
+        else
+            ModalError(
+                state = errorState,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(UiKitTheme.colors.screenBackground)
+                    .navigationBarsPadding()
+                    .bottomNavigationPadding()
+            )
     }
 }
 
