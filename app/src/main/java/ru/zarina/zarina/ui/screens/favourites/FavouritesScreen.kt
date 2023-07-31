6529d756ac9bb9f9eb8b6f5a68dbd876a5cec15e
Dmitry Popov <popovdd@melonfashion.com>
2023-07-31 16:03:53 +0300

    Add ZarinaScaffold

--- app/src/main/java/ru/zarina/zarina/ui/screens/favourites/FavouritesScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/favourites/FavouritesScreen.kt
@@ -1,19 +1,34 @@
 package ru.zarina.zarina.ui.screens.favourites
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.res.stringResource
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import kotlinx.coroutines.flow.Flow
 import org.koin.androidx.compose.koinViewModel
+import ru.zarina.zarina.R
 import ru.zarina.zarina.domain.Product
 import ru.zarina.zarina.ui.common.behavior.navigationbar.NavigationBarState
+import ru.zarina.zarina.ui.common.components.ZarinaScaffold
+import ru.zarina.zarina.ui.common.components.toolbar.ScreenToolbar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavoritesScreenContent(
     favorites: LazyPagingItems<Product>,
 ) {
+    ZarinaScaffold(
+        toolbar = {
+            ScreenToolbar(
+                title = stringResource(id = R.string.favourites)
+                // TODO elevation on scroll
+            )
+        }
+    ) {
 
+    }
 }
 
 @Composable
