caa8943a3ea49f8ff88f393b92b8849834b710f8
Dmitry Popov <popovdd@melonfashion.com>
2023-08-02 16:41:49 +0300

    Change non-discount price color

--- app/src/main/java/ru/zarina/zarina/ui/common/components/ProductPrice.kt
+++ app/src/main/java/ru/zarina/zarina/ui/common/components/ProductPrice.kt
@@ -34,10 +34,12 @@ fun ProductPrice(
                 maxLines = 1
             )
         }
+        val currentPriceColor =
+            if (price.isDiscounted) UiKitTheme.colors.retiredPrice else UiKitTheme.colors.price
         Text(
             text = stringResource(R.string.currency_amount_rubles, price.current),
             style = textStyle,
-            color = UiKitTheme.colors.price,
+            color = currentPriceColor,
             textAlign = TextAlign.Center,
             maxLines = 1
         )
