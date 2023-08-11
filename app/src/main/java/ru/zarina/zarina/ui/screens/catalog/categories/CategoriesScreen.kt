95833a0d945383d01cea06f8c902b9dca4c3b419
Dmitry Popov <popovdd@melonfashion.com>
2023-08-11 16:19:06 +0300

    Add SearchBar to CategoriesScreen

--- app/src/main/java/ru/zarina/zarina/ui/screens/catalog/categories/CategoriesScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screens/catalog/categories/CategoriesScreen.kt
@@ -9,17 +9,16 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -28,18 +27,17 @@ import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.toPersistentList
 import kotlinx.coroutines.flow.Flow
 import org.koin.androidx.compose.koinViewModel
-import ru.zarina.zarina.R
 import ru.zarina.zarina.domain.Category
 import ru.zarina.zarina.domain.Media
 import ru.zarina.zarina.ui.common.behavior.navigationbar.NavigationBarState
 import ru.zarina.zarina.ui.common.components.AsyncImageLoader
+import ru.zarina.zarina.ui.common.components.ElevationContainer
+import ru.zarina.zarina.ui.common.components.RedirectSearchBar
 import ru.zarina.zarina.ui.common.components.ZarinaScaffold
 import ru.zarina.zarina.ui.common.components.bottomNavigationPadding
-import ru.zarina.zarina.ui.common.components.toolbar.ScreenToolbar
 import ru.zarina.zarina.ui.common.tooling.preview.providers.domain.CategoryListProvider
 import ru.zarina.zarina.ui.theme.ZarinaTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CategoriesScreenContent(
     categories: PersistentList<Category>,
@@ -49,10 +47,15 @@ fun CategoriesScreenContent(
     val listState = rememberLazyListState()
     ZarinaScaffold(
         toolbar = {
-            ScreenToolbar(
-                title = stringResource(id = R.string.catalog),
+            ElevationContainer(
                 isElevated = listState.canScrollBackward,
-            )
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                RedirectSearchBar(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.statusBarsPadding(),
+                )
+            }
         },
         isModalLoaderVisible = isLoaderVisible,
     ) {
