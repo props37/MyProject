e9cdff562ea2429c782c2cd0e0ce31772d76cb4e
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-20 11:53:48 +0300

    Tech: Set up system bars colors

--- app/src/main/java/ru/zarina/zarina/ui/MainActivity.kt
+++ app/src/main/java/ru/zarina/zarina/ui/MainActivity.kt
@@ -1,19 +1,27 @@
 package ru.zarina.zarina.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import ru.zarina.zarina.ui.theme.ZarinaTheme
+import ru.zarina.zarina.utils.library.jetpack.activity.DefaultDarkScrim
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val splashScreen = installSplashScreen()
-        splashScreen.setKeepOnScreenCondition { true }
-        enableEdgeToEdge()
+        installSplashScreen()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                SystemBarStyle.DefaultDarkScrim,
+            ),
+        )
         super.onCreate(savedInstanceState)
 
         setContent {
