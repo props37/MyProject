3c8436ab26229779681e7b2d56e41658182b7747
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-21 13:14:06 +0300

    Feature ZRWEB-1060: Fix Hilt PermissionManager dependency scope

--- app/src/main/java/ru/zarina/zarina/di/reworked/ApplicationModule.kt
+++ app/src/main/java/ru/zarina/zarina/di/reworked/ApplicationModule.kt
@@ -6,12 +6,14 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import ru.zarina.zarina.data.permissionmanager.PermissionManager
 import ru.zarina.zarina.data.permissionmanager.PermissionManagerImpl
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class ApplicationModule {
 
     @Binds
+    @Singleton
     abstract fun bindPermissionManager(
         permissionManagerImpl: PermissionManagerImpl,
     ): PermissionManager
