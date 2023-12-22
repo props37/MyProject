84c84678a43411b49bc05d186d3dfcbb9e7194bc
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-22 17:45:25 +0300

    Feature ZRWEB-1060: Add AuthorizationEncryptedStorage

--- app/src/main/java/ru/zarina/zarina/data/authorization/AuthorizationRepository.kt
+++ app/src/main/java/ru/zarina/zarina/data/authorization/AuthorizationRepository.kt
@@ -1,10 +1,12 @@
 package ru.zarina.zarina.data.authorization
 
+import ru.zarina.zarina.data.authorization.local.AuthorizationLocalDataSource
 import ru.zarina.zarina.data.authorization.remote.AuthorizationRemoteDataSource
 import ru.zarina.zarina.domain.authorization.AuthorizationTokens
 import javax.inject.Inject
 
 class AuthorizationRepository @Inject constructor(
+    private val localDataSource: AuthorizationLocalDataSource,
     private val remoteDataSource: AuthorizationRemoteDataSource,
 ) {
     suspend fun getUnauthorizedUserAuthorizationTokens(): AuthorizationTokens {
