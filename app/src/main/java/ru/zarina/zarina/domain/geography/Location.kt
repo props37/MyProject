5e17d0aa80e51840a8497fd2ca9175fb6e1824bb
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-22 22:53:40 +0300

    Feature ZRWEB-1060: Add Location domain model

---
+++ app/src/main/java/ru/zarina/zarina/domain/geography/Location.kt
@@ -0,0 +1,10 @@
+package ru.zarina.zarina.domain.geography
+
+data class Location(
+    val latitude: Double,
+    val longitude: Double,
+) {
+    companion object {
+        val SAINT_PETERSBURG = Location(59.937500, 30.308611)
+    }
+}
