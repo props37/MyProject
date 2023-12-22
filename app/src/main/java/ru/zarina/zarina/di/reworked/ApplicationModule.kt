75b6c40ec625a2462317c44a1fc6da1116bfa317
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-22 16:24:53 +0300

    Feature ZRWEB-1060: Provide Json with Hilt

--- app/src/main/java/ru/zarina/zarina/di/reworked/ApplicationModule.kt
+++ app/src/main/java/ru/zarina/zarina/di/reworked/ApplicationModule.kt
@@ -2,8 +2,11 @@ package ru.zarina.zarina.di.reworked
 
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
 import ru.zarina.zarina.data.permissionmanager.PermissionManager
 import ru.zarina.zarina.data.permissionmanager.PermissionManagerImpl
 import javax.inject.Singleton
@@ -11,6 +14,16 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class ApplicationModule {
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Provides
+    @Singleton
+    fun provideJson(): Json = Json {
+        isLenient = true
+        ignoreUnknownKeys = true
+        encodeDefaults = true
+        explicitNulls = false
+    }
 
     @Binds
     @Singleton
