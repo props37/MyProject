91becdf1aceae20d0db786b0d803779ba1f31251
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-22 23:51:25 +0300

    Feature ZRWEB-1060: Add DetectCityUseCase

---
+++ app/src/main/java/ru/zarina/zarina/usecase/rework/geography/DetectCityUseCase.kt
@@ -0,0 +1,30 @@
+package ru.zarina.zarina.usecase.rework.geography
+
+import kotlinx.coroutines.CoroutineDispatcher
+import ru.zarina.zarina.base.clean.UseCase
+import ru.zarina.zarina.data.rework.geography.GeographyRepository
+import ru.zarina.zarina.data.rework.location.LocationRepository
+import ru.zarina.zarina.di.reworked.Qualifiers
+import ru.zarina.zarina.domain.rework.geography.City
+import timber.log.Timber
+import javax.inject.Inject
+
+class DetectCityUseCase @Inject constructor(
+    @Qualifiers.CoroutineDispatcher(Qualifiers.CoroutineDispatchers.IO)
+    dispatcher: CoroutineDispatcher,
+    private val locationRepository: LocationRepository,
+    private val geographyRepository: GeographyRepository,
+) : UseCase<Unit, City?>(dispatcher) {
+
+    override suspend fun execute(params: Unit): City? {
+        val location = locationRepository.getCurrentLocation()
+        return if (location != null) {
+            val city = geographyRepository.getCity(location)
+            Timber.v("The city is detected: $city")
+            return city
+        } else {
+            Timber.v("Could not detect the city because the location is unknown")
+            null
+        }
+    }
+}
