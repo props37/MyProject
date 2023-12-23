4b98b7446ece2eb47a7a49b4dbbfdaf5a7f46849
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-23 22:25:12 +0300

    Feature ZRWEB-1060: Refactor WindowInsetsSides.HorizontalAndBottom

--- app/src/main/java/ru/zarina/zarina/util/compose/WindowInsets.kt
+++ app/src/main/java/ru/zarina/zarina/util/compose/WindowInsets.kt
@@ -3,4 +3,4 @@ package ru.zarina.zarina.util.compose
 import androidx.compose.foundation.layout.WindowInsetsSides
 
 val WindowInsetsSides.Companion.HorizontalAndBottom: WindowInsetsSides
-    get() = Start + End + Bottom
+    get() = Horizontal + Bottom
