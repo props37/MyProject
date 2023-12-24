c9ab798f266960304eecdd954d69b399af398ac2
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-24 22:16:53 +0300

    Feature ZRWEB-1060: Wrap ZarinaApp into ModalBottomSheetLayout to support bottom sheet destinations within navigation

--- app/src/main/java/ru/zarina/zarina/ui/rework/ZarinaApp.kt
+++ app/src/main/java/ru/zarina/zarina/ui/rework/ZarinaApp.kt
@@ -7,9 +7,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+import com.google.accompanist.navigation.material.ModalBottomSheetLayout
 import ru.zarina.zarina.ui.bottomnavbar.ZarinaBottomNavBar
 import ru.zarina.zarina.ui.common.behavior.bottomnavbar.LocalBottomNavBarBehaviorController
 import ru.zarina.zarina.ui.common.behavior.bottomnavbar.rememberBottomNavBarBehaviorController
@@ -30,19 +34,28 @@ fun ZarinaApp(
     CompositionLocalProvider(
         LocalBottomNavBarBehaviorController provides bottomNavBarBehaviorController,
     ) {
-        Box(modifier = modifier) {
-            ZarinaNavigation(
-                navController = navController,
-                startDestination = viewModel.startDestination,
-                modifier = Modifier.fillMaxSize(),
-            )
+        ModalBottomSheetLayout(
+            bottomSheetNavigator = bottomSheetNavigator,
+            sheetShape = RectangleShape,
+            sheetElevation = 0.dp,
+            sheetBackgroundColor = Color.Unspecified,
+            sheetContentColor = Color.Unspecified,
+            scrimColor = Color.Unspecified,
+        ) {
+            Box(modifier = modifier) {
+                ZarinaNavigation(
+                    navController = navController,
+                    startDestination = viewModel.startDestination,
+                    modifier = Modifier.fillMaxSize(),
+                )
 
-            ZarinaBottomNavBar(
-                navController = navController,
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .fillMaxWidth(),
-            )
+                ZarinaBottomNavBar(
+                    navController = navController,
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .fillMaxWidth(),
+                )
+            }
         }
     }
 }
