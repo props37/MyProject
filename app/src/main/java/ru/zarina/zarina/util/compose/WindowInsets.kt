8ddaab277a08bf5940cce07d3cd81411849203f7
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-24 22:33:40 +0300

    Feature ZRWEB-1060: Add windowInsets parameter to ZarinaBottomSheet

--- app/src/main/java/ru/zarina/zarina/util/compose/WindowInsets.kt
+++ app/src/main/java/ru/zarina/zarina/util/compose/WindowInsets.kt
@@ -1,6 +1,10 @@
 package ru.zarina.zarina.util.compose
 
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
+
+val WindowInsets.Companion.none: WindowInsets
+    get() = WindowInsets(0, 0, 0, 0)
 
 val WindowInsetsSides.Companion.HorizontalAndBottom: WindowInsetsSides
     get() = Horizontal + Bottom
