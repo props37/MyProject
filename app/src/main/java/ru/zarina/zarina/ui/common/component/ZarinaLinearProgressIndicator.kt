e45336c9308fb067ec134bff9902d71fc94a9e6b
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-25 10:49:46 +0300

    Feature ZRWEB-1060: Migrate ZarinaLinearProgressIndicator to material 2 components

--- app/src/main/java/ru/zarina/zarina/ui/common/component/ZarinaLinearProgressIndicator.kt
+++ app/src/main/java/ru/zarina/zarina/ui/common/component/ZarinaLinearProgressIndicator.kt
@@ -2,7 +2,7 @@ package ru.zarina.zarina.ui.common.component
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -16,7 +16,7 @@ fun ZarinaLinearProgressIndicator(
     progress: Float,
     modifier: Modifier = Modifier,
     color: Color = UiKitTheme.colorsReworked.background.general.inverse.inverse,
-    trackColor: Color = UiKitTheme.colorsReworked.background.general.regular.muted,
+    backgroundColor: Color = UiKitTheme.colorsReworked.background.general.regular.muted,
     strokeCap: StrokeCap = StrokeCap.Round,
 ) {
     val animatedProgress by animateFloatAsState(
@@ -28,7 +28,7 @@ fun ZarinaLinearProgressIndicator(
     LinearProgressIndicator(
         progress = animatedProgress,
         color = color,
-        trackColor = trackColor,
+        backgroundColor = backgroundColor,
         strokeCap = strokeCap,
         modifier = modifier,
     )
