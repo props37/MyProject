1752a23f441698c0b25c3e7052a9cbf4a589a2f3
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-26 15:54:37 +0300

    Feature ZRWEB-1060: Add caching for city search

---
+++ app/src/main/java/ru/zarina/zarina/data/rework/geography/local/GeographyDataHolder.kt
@@ -0,0 +1,41 @@
+package ru.zarina.zarina.data.rework.geography.local
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
+import ru.zarina.zarina.domain.rework.geography.City
+import timber.log.Timber
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class GeographyDataHolder @Inject constructor() {
+    private val citiesToNameQuery = MutableStateFlow(mapOf<String?, List<City>>())
+
+    fun setCities(nameQuery: String?, cities: List<City>) {
+        citiesToNameQuery.update { it + (nameQuery to cities) }
+        ensureCitiesToNameQueryCacheSize()
+    }
+
+    fun getCities(nameQuery: String?): Flow<List<City>?> {
+        return citiesToNameQuery.map { map ->
+            map[nameQuery]
+        }
+    }
+
+    private fun ensureCitiesToNameQueryCacheSize() {
+        val cachedNameQueries = citiesToNameQuery.value.keys
+        if (cachedNameQueries.size > MAX_CACHED_CITY_NAME_QUERIES) {
+            val nameQueryToRemove = cachedNameQueries.firstOrNull { it != null }
+            if (nameQueryToRemove != null) {
+                Timber.v("Clear cached cities for name query $nameQueryToRemove")
+                citiesToNameQuery.update { it - nameQueryToRemove }
+            }
+        }
+    }
+
+    companion object {
+        private const val MAX_CACHED_CITY_NAME_QUERIES = 5
+    }
+}
