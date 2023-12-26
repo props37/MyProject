195be7a1f055eb9067e5e2f8b167f051db468e9b
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-26 22:13:59 +0300

    Feature ZRWEB-1060: Update insets handling

--- app/src/main/java/ru/zarina/zarina/ui/screen/onboarding/OnboardingScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screen/onboarding/OnboardingScreen.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.union
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -92,7 +94,9 @@ private fun ScreenContent(
             Column(
                 modifier = Modifier
                     .windowInsetsPadding(
-                        WindowInsets.safeDrawing.only(WindowInsetsSides.HorizontalAndBottom)
+                        WindowInsets.navigationBars
+                            .union(WindowInsets.displayCutout)
+                            .only(WindowInsetsSides.HorizontalAndBottom),
                     )
                     .padding(top = 24.dp, bottom = 20.dp),
             ) {
