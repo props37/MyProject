1e09a143956af03a9b47aebd0d439838acc785fa
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-27 12:33:10 +0300

    Feature ZRWEB-1077: Add HomeRepository::getBanners method

--- app/src/main/java/ru/zarina/zarina/data/rework/home/HomeRepository.kt
+++ app/src/main/java/ru/zarina/zarina/data/rework/home/HomeRepository.kt
@@ -1,8 +1,12 @@
 package ru.zarina.zarina.data.rework.home
 
+import kotlinx.coroutines.flow.Flow
 import ru.zarina.zarina.data.rework.home.remote.HomeRemoteDataSource
+import ru.zarina.zarina.domain.rework.home.HomeBanners
 import javax.inject.Inject
 
 class HomeRepository @Inject constructor(
     private val remoteDataSource: HomeRemoteDataSource,
-)
+) {
+    fun getBanners(): Flow<HomeBanners> = remoteDataSource.getBanners()
+}
---
+++ app/src/main/java/ru/zarina/zarina/data/rework/home/remote/api/dto/HomeBannersDto.kt
@@ -0,0 +1,70 @@
+package ru.zarina.zarina.data.rework.home.remote.api.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import ru.zarina.zarina.domain.rework.common.Url
+import ru.zarina.zarina.domain.rework.home.HomeBanners
+
+@Serializable
+data class HomeBannersDto(
+    @SerialName("woman")
+    val woman: List<Banner>? = null,
+    
+    @SerialName("man")
+    val man: List<Banner>? = null,
+) {
+    fun toHomeBanners(): HomeBanners = HomeBanners(
+        woman = woman?.map { it.toHomeBanner() } ?: emptyList(),
+        man = man?.map { it.toHomeBanner() } ?: emptyList(),
+    )
+
+    @Serializable
+    data class Banner(
+        @SerialName("id")
+        val id: Long? = null,
+
+        @SerialName("media_type")
+        val mediaType: MediaType? = null,
+
+        @SerialName("media_url")
+        val mediaUrl: String? = null,
+
+        @SerialName("view")
+        val view: View? = null,
+    ) {
+        fun toHomeBanner(): HomeBanners.Banner {
+            val id = checkNotNull(id) { "id is null" }
+            val mediaType = checkNotNull(mediaType) { "mediaType is null" }
+            val mediaUrl = checkNotNull(mediaUrl) { "mediaUrl is null" }
+            val view = checkNotNull(view) { "view is null" }
+            return HomeBanners.Banner(
+                id = HomeBanners.Banner.Id(id),
+                mediaType = mediaType.toHomeBannerMediaType(),
+                mediaUrl = Url(mediaUrl),
+                view = view.toHomeBannerView(),
+            )
+        }
+
+        // TODO: [High] Extract?
+        @Serializable
+        @JvmInline
+        value class MediaType(val value: String) {
+            fun toHomeBannerMediaType(): HomeBanners.Banner.MediaType = when (value) {
+                "image" -> HomeBanners.Banner.MediaType.IMAGE
+                "video" -> HomeBanners.Banner.MediaType.VIDEO
+                else -> error("Unknown media type $value")
+            }
+        }
+
+        // TODO: [High] Extract?
+        @Serializable
+        @JvmInline
+        value class View(val value: String) {
+            fun toHomeBannerView(): HomeBanners.Banner.View = when (value) {
+                "fullscreen" -> HomeBanners.Banner.View.FULLSCREEN
+                "grid" -> HomeBanners.Banner.View.GRID
+                else -> error("Unknown view $value")
+            }
+        }
+    }
+}
