1a704f9396df5d8eda5148506f6808e1aac48d65
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-27 09:47:55 +0300

    Feature ZRWEB-1060: Update insets handling

--- app/src/main/java/ru/zarina/zarina/ui/screen/cityselector/CitySelectorBottomSheetScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screen/cityselector/CitySelectorBottomSheetScreen.kt
@@ -7,11 +7,15 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.union
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -80,7 +84,11 @@ private fun ScreenContent(
         navigateBackward = navigateBackward,
     )
 
-    ZarinaBottomSheet(windowInsets = WindowInsets.statusBars) {
+    ZarinaBottomSheet(
+        windowInsets = WindowInsets.statusBars
+            .union(WindowInsets.displayCutout)
+            .only(WindowInsetsSides.Top),
+    ) {
         Column(modifier = Modifier.fillMaxSize()) {
             TopBar(onCloseClicked = onCloseClicked)
 
