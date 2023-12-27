b925369e3cefd1dbca08102940a06172fb2bc24d
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-27 11:46:29 +0300

    Feature ZRWEB-1060: Specify animation of navigating from Onboarding screen to Home screen

--- app/src/main/java/ru/zarina/zarina/ui/navigation/rework/destination/UnscopedDestinations.kt
+++ app/src/main/java/ru/zarina/zarina/ui/navigation/rework/destination/UnscopedDestinations.kt
@@ -1,5 +1,7 @@
 package ru.zarina.zarina.ui.navigation.rework.destination
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -15,7 +17,21 @@ import ru.zarina.zarina.ui.screen.onboarding.OnboardingScreenAction
 import ru.zarina.zarina.ui.screen.onboarding.OnboardingViewModel
 
 fun NavGraphBuilder.onboardingScreen(navController: NavHostController) {
-    composableDestination(UnscopedDestinations.Onboarding) {
+    composableDestination(
+        destination = UnscopedDestinations.Onboarding,
+        exitTransition = {
+            when (targetState.destination.route) {
+                HomeGraph.Home.routeSchema -> {
+                    slideOutOfContainer(
+                        towards = AnimatedContentTransitionScope.SlideDirection.Start,
+                        animationSpec = tween(durationMillis = 300),
+                    )
+                }
+
+                else -> null
+            }
+        },
+    ) {
         OnboardingScreen(
             viewModel = hiltViewModel { factory : OnboardingViewModel.Factory ->
                 factory.create(it.savedStateHandle)
