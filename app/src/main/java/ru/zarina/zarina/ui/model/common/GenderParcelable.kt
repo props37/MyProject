c89309727e0fbdc54b690686d5fb604697fdd53e
Artem Sidorov <sidorovaa@melonfashion.com>
2023-12-29 14:44:54 +0300

    Feature ZRWEB-1077: Introduce Gender and GenderParcelable models

---
+++ app/src/main/java/ru/zarina/zarina/ui/model/common/GenderParcelable.kt
@@ -0,0 +1,23 @@
+package ru.zarina.zarina.ui.model.common
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import ru.zarina.zarina.domain.rework.common.Gender
+
+@Parcelize
+enum class GenderParcelable : Parcelable {
+    FEMALE,
+    MALE;
+
+    fun toGender(): Gender = when (this) {
+        FEMALE -> Gender.FEMALE
+        MALE -> Gender.MALE
+    }
+
+    companion object {
+        fun fromGender(gender: Gender): GenderParcelable = when (gender) {
+            Gender.FEMALE -> FEMALE
+            Gender.MALE -> MALE
+        }
+    }
+}
