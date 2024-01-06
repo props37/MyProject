1fe07e897d7a0aba2917bf2c3b8c330168db95f0
Artem Sidorov <sidorovaa@melonfashion.com>
2024-01-07 00:36:37 +0300

    Feature ZRWEB-1077: Change default logo width of ZarinaLoadingScreen

--- app/src/main/java/ru/zarina/zarina/ui/common/component/screen/ZarinaLoadingScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/common/component/screen/ZarinaLoadingScreen.kt
@@ -12,10 +12,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import ru.zarina.zarina.R
 import ru.zarina.zarina.ui.common.component.ZarinaLogo
 import ru.zarina.zarina.ui.common.component.ZarinaLogoAspectRatio
+import ru.zarina.zarina.ui.common.util.SplashScreenLogoSize
 import ru.zarina.zarina.ui.theme.UiKitTheme
 import ru.zarina.zarina.ui.theme.rework.ZarinaTheme
 
@@ -23,7 +23,7 @@ import ru.zarina.zarina.ui.theme.rework.ZarinaTheme
 fun ZarinaLoadingScreen(
     modifier: Modifier = Modifier,
     contentDescription: String = stringResource(R.string.loading),
-    logoWidth: Dp = 150.dp,
+    logoWidth: Dp = SplashScreenLogoSize,
     logoColor: Color = UiKitTheme.colorsReworked.icon.regular.default,
     animate: Boolean = true,
 ) {
