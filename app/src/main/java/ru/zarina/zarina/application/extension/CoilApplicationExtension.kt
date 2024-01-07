37509e7ea395e0aca23861ba49525634ac003e72
Artem Sidorov <sidorovaa@melonfashion.com>
2024-01-07 16:29:43 +0300

    Feature ZRWEB-1077: Add Gif support for Coil

--- app/src/main/java/ru/zarina/zarina/application/extension/CoilApplicationExtension.kt
+++ app/src/main/java/ru/zarina/zarina/application/extension/CoilApplicationExtension.kt
@@ -1,9 +1,14 @@
 package ru.zarina.zarina.application.extension
 
 import android.app.Application
+import android.content.Context
+import android.os.Build
 import coil.Coil
+import coil.ComponentRegistry
 import coil.ImageLoader
 import coil.ImageLoaderFactory
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import coil.disk.DiskCache
 import coil.memory.MemoryCache
 import ru.zarina.zarina.application.extension.base.ApplicationExtension
@@ -14,21 +19,36 @@ class CoilApplicationExtension @Inject constructor() : ApplicationExtension {
         val factory = ImageLoaderFactory {
             ImageLoader.Builder(application)
                 .crossfade(true)
-                .memoryCache {
-                    MemoryCache.Builder(application)
-                        .maxSizePercent(MEMORY_CACHE_MAX_SIZE_PERCENT)
-                        .build()
+                .components {
+                    addGifComponent()
                 }
-                .diskCache {
-                    DiskCache.Builder()
-                        .directory(application.cacheDir.resolve(DISK_CACHE_DIRECTORY_NAME))
-                        .maxSizePercent(DISK_CACHE_MAX_SIZE_PERCENT)
-                        .build()
-                }
+                .memoryCache(createMemoryCache(application))
+                .diskCache(createDiskCache(application))
                 .build()
         }
 
         Coil.setImageLoader(factory)
+    }
+
+    private fun ComponentRegistry.Builder.addGifComponent() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            add(ImageDecoderDecoder.Factory())
+        } else {
+            add(GifDecoder.Factory())
+        }
+    }
+
+    private fun createMemoryCache(context: Context): MemoryCache {
+        return MemoryCache.Builder(context)
+            .maxSizePercent(MEMORY_CACHE_MAX_SIZE_PERCENT)
+            .build()
+    }
+
+    private fun createDiskCache(context: Context): DiskCache {
+        return DiskCache.Builder()
+            .directory(context.cacheDir.resolve(DISK_CACHE_DIRECTORY_NAME))
+            .maxSizePercent(DISK_CACHE_MAX_SIZE_PERCENT)
+            .build()
     }
 
     companion object {
