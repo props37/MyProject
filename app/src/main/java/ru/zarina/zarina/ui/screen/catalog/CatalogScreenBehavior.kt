6b2bc4934c72e3b6a783d20673c9714f3d97930d
Artem Sidorov <sidorovaa@melonfashion.com>
2024-01-15 18:31:20 +0300

    Migrate to LifecycleStartEffect to handle Catalog screen side-effects

--- app/src/main/java/ru/zarina/zarina/ui/screen/catalog/CatalogScreenBehavior.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screen/catalog/CatalogScreenBehavior.kt
@@ -1,11 +1,15 @@
 package ru.zarina.zarina.ui.screen.catalog
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LifecycleStartEffect
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import ru.zarina.zarina.ui.common.behavior.bottomnavbar.ForcedBottomNavBarBehavior
 import ru.zarina.zarina.ui.screen.catalog.CatalogViewModel.SideEffect
 
@@ -17,12 +21,19 @@ fun CatalogScreenBehavior(
 
     ForcedBottomNavBarBehavior(isVisible = true)
 
-    LaunchedEffect(sideEffects) {
-        sideEffects.collect { sideEffect ->
-            when (sideEffect) {
-                SideEffect.FreeSearchBarFocus -> updatedFocusManager.clearFocus(force = true)
+    // TODO: [Top] Test
+    LifecycleStartEffect(sideEffects) {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                sideEffects.collect { sideEffect ->
+                    when (sideEffect) {
+                        SideEffect.FreeSearchBarFocus -> updatedFocusManager.clearFocus(force = true)
+                    }
+                }
             }
         }
+
+        onStopOrDispose {}
     }
 }
 
