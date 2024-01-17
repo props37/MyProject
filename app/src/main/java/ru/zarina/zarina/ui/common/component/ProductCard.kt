e3f4e60890995ebc63d7e98db966c155b34c2757
Artem Sidorov <sidorovaa@melonfashion.com>
2024-01-17 18:15:56 +0300

    Update ProductCard

--- app/src/main/java/ru/zarina/zarina/ui/common/component/ProductCard.kt
+++ app/src/main/java/ru/zarina/zarina/ui/common/component/ProductCard.kt
@@ -3,7 +3,9 @@ package ru.zarina.zarina.ui.common.component
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,12 +13,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.LocalMinimumInteractiveComponentEnforcement
@@ -24,22 +28,27 @@ import androidx.compose.material.Text
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.key
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 import ru.zarina.zarina.R
 import ru.zarina.zarina.domain.rework.common.Media
 import ru.zarina.zarina.domain.rework.product.Product
+import ru.zarina.zarina.domain.rework.product.ProductColor
 import ru.zarina.zarina.domain.rework.product.currentPrice
 import ru.zarina.zarina.ui.common.component.base.button.LikeIconButton
 import ru.zarina.zarina.ui.common.component.base.button.ZarinaIconButton
@@ -47,9 +56,10 @@ import ru.zarina.zarina.ui.common.tooling.preview.DensityPreviews
 import ru.zarina.zarina.ui.common.tooling.preview.FontScalePreviews
 import ru.zarina.zarina.ui.common.tooling.preview.ZarinaPreview
 import ru.zarina.zarina.ui.common.tooling.preview.parameterprovider.ProductPreviewParameterProvider
+import ru.zarina.zarina.ui.common.util.domain.toComposeColor
 import ru.zarina.zarina.ui.theme.UiKitTheme
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
 @Composable
 fun ProductCard(
     product: Product,
@@ -77,16 +87,17 @@ fun ProductCard(
                 onClick = onAddToFavoritesClicked,
                 iconSize = 16.dp,
                 indication = rememberRipple(bounded = false, radius = 16.dp),
-                modifier = Modifier
-                    .align(Alignment.TopEnd),
+                modifier = Modifier.align(Alignment.TopEnd),
             )
 
             // TODO: [High] Add pager indicator
         }
+        
+        Spacer(modifier = Modifier.height(8.dp))
 
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(start = 16.dp, end = 4.dp),
+            modifier = Modifier.padding(start = 16.dp),
         ) {
             Text(
                 text = product.name,
@@ -97,11 +108,15 @@ fun ProductCard(
                 modifier = Modifier.weight(1f),
             )
             Spacer(modifier = Modifier.width(8.dp))
-            AddToCartIconButton(
-                isAdded = product.isInCart,
-                onClick = onAddToCartClicked,
-                modifier = Modifier.size(32.dp),
-            )
+            CompositionLocalProvider(LocalMinimumInteractiveComponentEnforcement provides false) {
+                AddToCartIconButton(
+                    isAdded = product.isInCart,
+                    onClick = onAddToCartClicked,
+                    modifier = Modifier
+                        .padding(end = 10.dp)
+                        .size(28.dp),
+                )
+            }
         }
 
         Row(
@@ -136,7 +151,14 @@ fun ProductCard(
             }
         }
 
-        // TODO: [High] Add colors
+        Spacer(modifier = Modifier.height(6.dp))
+
+        Colors(
+            colors = remember(product.colors) { product.colors.toImmutableList() },
+            modifier = Modifier.padding(horizontal = 16.dp),
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
 
@@ -162,7 +184,6 @@ private fun ImagePager(
 }
 
 // TODO: [Low] Extract?
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 private fun AddToCartIconButton(
     isAdded: Boolean,
@@ -170,32 +191,74 @@ private fun AddToCartIconButton(
     modifier: Modifier = Modifier,
     isLoading: Boolean = false,
 ) {
-    CompositionLocalProvider(LocalMinimumInteractiveComponentEnforcement provides false) {
-        val iconSize = 16.dp
-        ZarinaIconButton(
-            onClick = onClick,
-            isLoading = isLoading,
-            indication = rememberRipple(bounded = false, radius = iconSize),
-            modifier = modifier,
-        ) {
-            Crossfade(
-                targetState = isAdded,
-                label = "AddToCartIconButton",
-            ) { isAdded ->
-                val iconResId =
-                    if (isAdded) R.drawable.ic_cart_added_outline_24 else R.drawable.ic_cart_outline_24
-                val contentDescriptionResId =
-                    if (isAdded) R.string.remove_from_cart else R.string.add_to_cart
+    val iconSize = 16.dp
+    ZarinaIconButton(
+        onClick = onClick,
+        isLoading = isLoading,
+        indication = rememberRipple(bounded = false, radius = iconSize),
+        modifier = modifier,
+    ) {
+        Crossfade(
+            targetState = isAdded,
+            label = "AddToCartIconButton",
+        ) { isAdded ->
+            val iconResId =
+                if (isAdded) R.drawable.ic_cart_added_outline_24 else R.drawable.ic_cart_outline_24
+            val contentDescriptionResId =
+                if (isAdded) R.string.remove_from_cart else R.string.add_to_cart
 
-                Icon(
-                    painter = painterResource(iconResId),
-                    contentDescription = stringResource(contentDescriptionResId),
-                    tint = UiKitTheme.colorsReworked.icon.regular.default,
-                    modifier = Modifier.size(iconSize),
-                )
+            Icon(
+                painter = painterResource(iconResId),
+                contentDescription = stringResource(contentDescriptionResId),
+                tint = UiKitTheme.colorsReworked.icon.regular.default,
+                modifier = Modifier.size(iconSize),
+            )
+        }
+    }
+}
+
+// TODO: [Medium] Respect available width
+@Composable
+private fun Colors(
+    colors: ImmutableList<ProductColor>,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(ColorSize),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier,
+    ) {
+        colors.forEach {
+            key(it.id.value) {
+                Color(color = it.color.toComposeColor())
             }
         }
     }
+}
+
+@Composable
+private fun Color(
+    color: Color,
+    modifier: Modifier = Modifier,
+) {
+    val shape = CircleShape
+    val borderModifier = if (color.luminance() >= WhiteColorLuminanceThreshold) {
+        Modifier.border(
+            width = 0.5.dp,
+            color = UiKitTheme.colorsReworked.border.general.disabled,
+            shape = shape,
+        )
+    } else {
+        Modifier
+    }
+
+    Box(
+        modifier = modifier
+            .size(ColorSize)
+            .clip(shape)
+            .background(color)
+            .then(borderModifier),
+    )
 }
 
 @Preview
@@ -218,3 +281,7 @@ private fun ProductCardPreview(
 }
 
 private const val ImagePagerAspectRatio = 0.69f
+
+private const val WhiteColorLuminanceThreshold = 0.95f
+
+private val ColorSize: Dp get() = 8.dp
