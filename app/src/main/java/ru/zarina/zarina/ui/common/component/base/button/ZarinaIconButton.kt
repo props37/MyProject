d66074694f38426114a1b59ca7ebe6ecc3efb575
Artem Sidorov <sidorovaa@melonfashion.com>
2024-01-18 18:28:02 +0300

    Fix ZarinaIconButton modifier order

--- app/src/main/java/ru/zarina/zarina/ui/common/component/base/button/ZarinaIconButton.kt
+++ app/src/main/java/ru/zarina/zarina/ui/common/component/base/button/ZarinaIconButton.kt
@@ -46,14 +46,14 @@ fun ZarinaIconButton(
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier
-            .minimumInteractiveComponentSize()
             .clickable(
                 onClick = onClick,
                 enabled = isEnabled,
                 role = Role.Button,
                 interactionSource = interactionSource,
                 indication = indication,
-            ),
+            )
+            .minimumInteractiveComponentSize(),
     ) {
         AnimatedContent(
             targetState = isLoading,
