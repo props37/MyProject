18c25b6d6953d8b3d003661d5d67640b57e71262
Artem Sidorov <sidorovaa@melonfashion.com>
2024-01-23 11:18:16 +0300

    Migrate from query parameters to body for /api/v1/products request

--- app/src/main/java/ru/zarina/zarina/data/rework/product/remote/api/ProductApi.kt
+++ app/src/main/java/ru/zarina/zarina/data/rework/product/remote/api/ProductApi.kt
@@ -2,29 +2,41 @@ package ru.zarina.zarina.data.rework.product.remote.api
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.request.get
-import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import ru.zarina.zarina.data.rework.common.remote.api.dto.SortingDto
 import ru.zarina.zarina.data.rework.product.remote.api.dto.FilteredProductsDto
 import ru.zarina.zarina.di.rework.Qualifiers
 import ru.zarina.zarina.domain.rework.category.Category
 import ru.zarina.zarina.domain.rework.common.Sorting
+import ru.zarina.zarina.util.library.ktor.setJsonBody
 import javax.inject.Inject
 
 class ProductApi @Inject constructor(
     @Qualifiers.ZarinaApi(Qualifiers.ZarinaApis.AUTHORIZED)
     private val httpClient: HttpClient,
 ) {
-    // TODO: [High] Migrate from parameters to body?
     suspend fun getFilteredProducts(
         categoryId: Category.Id,
         page: Int,
         sorting: Sorting,
     ): FilteredProductsDto {
-        return httpClient.get("/api/v1/products") {
-            parameter("category_id", categoryId.value)
-            parameter("page", page)
-            parameter("sort", SortingDto.from(sorting).value)
+        val body = GetFilteredProductsBody(categoryId.value, SortingDto.from(sorting), page)
+        return httpClient.post("/api/v1/products") {
+            setJsonBody(body)
         }.body()
     }
+
+    @Serializable
+    private data class GetFilteredProductsBody(
+        @SerialName("category_id")
+        val categoryId: Long,
+
+        @SerialName("sort")
+        val sort: SortingDto,
+
+        @SerialName("page")
+        val page: Int,
+    )
 }
