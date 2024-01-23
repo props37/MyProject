f5e943e69d55b1101d4150e5ac77f9c71fed5b15
Artem Sidorov <sidorovaa@melonfashion.com>
2024-01-23 16:35:28 +0300

    Remove SortFilter

--- app/src/main/java/ru/zarina/zarina/domain/rework/filter/Filtration.kt
+++ app/src/main/java/ru/zarina/zarina/domain/rework/filter/Filtration.kt
@@ -1,7 +1,6 @@
 package ru.zarina.zarina.domain.rework.filter
 
 data class Filtration(
-    val sorting: ListFilter<SortFilter>?,
     val price: PriceFilter?,
     val composition: ListFilter<CompositionFilter>?,
     val sizes: ListFilter<SizeFilter>?,
--- app/src/main/java/ru/zarina/zarina/domain/rework/filter/SortFilter.kt
+++
@@ -1,6 +0,0 @@
-package ru.zarina.zarina.domain.rework.filter
-
-data class SortFilter(
-    override val id: Id,
-    override val name: String,
-) : ListFilter.Item(id, name)
