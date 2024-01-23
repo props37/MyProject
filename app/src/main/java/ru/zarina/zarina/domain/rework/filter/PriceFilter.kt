271d8000bec626839e6dc2ad88336f25d5688e52
Artem Sidorov <sidorovaa@melonfashion.com>
2024-01-23 17:17:16 +0300

    Remove "from" and "to" properties from PriceFilter

--- app/src/main/java/ru/zarina/zarina/domain/rework/filter/PriceFilter.kt
+++ app/src/main/java/ru/zarina/zarina/domain/rework/filter/PriceFilter.kt
@@ -1,14 +1,10 @@
 package ru.zarina.zarina.domain.rework.filter
 
 data class PriceFilter(
-    val from: Long,
-    val to: Long,
     val min: Long,
     val max: Long,
 ) : Filter {
     init {
-        require(to >= from) { "\"to\" $to must be equal to or larger than \"from\" $from" }
-        require(from >= min) { "\"from\" $from must be equal to or larger than \"min\" $min" }
-        require(to <= max) { "\"to\" $to must be smaller than or equal to \"max\" $max" }
+        require(max >= min) { "\"max\" $max must be equal to or larger than \"min\" $min" }
     }
 }
