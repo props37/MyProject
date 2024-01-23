73c053e9334630021bcc09b79e5c5d972bccfb3f
Artem Sidorov <sidorovaa@melonfashion.com>
2024-01-23 17:01:35 +0300

    Add from and to properties to PriceFilter

--- app/src/main/java/ru/zarina/zarina/domain/rework/filter/PriceFilter.kt
+++ app/src/main/java/ru/zarina/zarina/domain/rework/filter/PriceFilter.kt
@@ -1,6 +1,14 @@
 package ru.zarina.zarina.domain.rework.filter
 
 data class PriceFilter(
+    val from: Long,
+    val to: Long,
     val min: Long,
     val max: Long,
-) : Filter
+) : Filter {
+    init {
+        require(to >= from) { "\"to\" $to must be equal to or larger than \"from\" $from" }
+        require(from >= min) { "\"from\" $from must be equal to or larger than \"min\" $min" }
+        require(to <= max) { "\"to\" $to must be smaller than or equal to \"max\" $max" }
+    }
+}
