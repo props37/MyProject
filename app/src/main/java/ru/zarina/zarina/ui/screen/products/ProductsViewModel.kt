efe19467118fb7f11e61602ef6c4275f7493ffdc
Artem Sidorov <sidorovaa@melonfashion.com>
2024-01-24 15:17:11 +0300

    Refactor ProductsViewModel

--- app/src/main/java/ru/zarina/zarina/ui/screen/products/ProductsViewModel.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screen/products/ProductsViewModel.kt
@@ -26,6 +26,7 @@ import ru.zarina.zarina.ui.navigation.rework.graph.UnscopedDestinations
 import ru.zarina.zarina.ui.screen.products.ProductsViewModel.SideEffect
 import ru.zarina.zarina.usecase.rework.category.GetCategoryFlowUseCase
 import ru.zarina.zarina.usecase.rework.product.GetProductPagingDataFlowUseCase
+import ru.zarina.zarina.util.library.coroutines.WhileAndroidUiSubscribed
 import ru.zarina.zarina.util.library.coroutines.mapState
 import javax.inject.Inject
 
@@ -63,7 +64,7 @@ class ProductsViewModel @Inject constructor(
         }
         .stateIn(
             scope = viewModelScope,
-            started = SharingStarted.Eagerly,
+            started = SharingStarted.WhileSubscribed(),
             initialValue = null,
         )
 
@@ -73,7 +74,7 @@ class ProductsViewModel @Inject constructor(
         }
         .stateIn(
             scope = viewModelScope,
-            started = SharingStarted.Eagerly,
+            started = SharingStarted.WhileAndroidUiSubscribed,
             initialValue = null,
         )
 
