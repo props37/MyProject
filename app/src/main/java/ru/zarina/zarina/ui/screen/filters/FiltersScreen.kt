e4e52fdb0093703f6c3afddd26e64950caeb1743
Artem Sidorov <sidorovaa@melonfashion.com>
2024-01-24 16:09:12 +0300

    Add top bar to Filters screen

--- app/src/main/java/ru/zarina/zarina/ui/screen/filters/FiltersScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screen/filters/FiltersScreen.kt
@@ -1,12 +1,26 @@
 package ru.zarina.zarina.ui.screen.filters
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.union
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.flow.Flow
+import ru.zarina.zarina.ui.bottomnavbar.bottomNavBarPadding
 import ru.zarina.zarina.ui.common.tooling.preview.DensityPreviews
 import ru.zarina.zarina.ui.common.tooling.preview.FontScalePreviews
 import ru.zarina.zarina.ui.common.tooling.preview.ZarinaPreview
+import ru.zarina.zarina.ui.screen.filters.FiltersScreenComponents.TopBar
+import ru.zarina.zarina.ui.theme.UiKitTheme
 
 @Composable
 fun FiltersScreen(
@@ -23,7 +37,23 @@ private fun ScreenContent(
 ) {
     FiltersScreenBehavior(sideEffects = sideEffects)
 
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(UiKitTheme.colorsReworked.background.general.regular.default)
+            .windowInsetsPadding(
+                WindowInsets.statusBars
+                    .union(WindowInsets.displayCutout)
+                    .only(WindowInsetsSides.Top),
+            )
+            .bottomNavBarPadding(),
+    ) {
+        TopBar(
+            onBackClicked = { /*TODO*/ },
+            isResetButtonVisible = false, // TODO: [High] Implement
+            onResetClicked = { /*TODO*/ },
+        )
+    }
 }
 
 @Preview
---
+++ app/src/main/java/ru/zarina/zarina/ui/screen/filters/FiltersScreenComponents.kt
@@ -0,0 +1,50 @@
+package ru.zarina.zarina.ui.screen.filters
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import ru.zarina.zarina.R
+import ru.zarina.zarina.ui.common.component.TopBarDefaults
+import ru.zarina.zarina.ui.common.component.ZarinaTopBar
+import ru.zarina.zarina.ui.common.component.button.BackIconButton
+import ru.zarina.zarina.ui.theme.UiKitTheme
+
+object FiltersScreenComponents {
+
+    @Composable
+    fun TopBar(
+        onBackClicked: () -> Unit,
+        isResetButtonVisible: Boolean,
+        onResetClicked: () -> Unit,
+        modifier: Modifier = Modifier,
+    ) {
+        ZarinaTopBar(
+            startContent = {
+                BackIconButton(
+                    onClick = onBackClicked,
+                    iconSize = 20.dp,
+                    modifier = Modifier.padding(start = 2.dp),
+                )
+            },
+            centerContent = {
+                Text(
+                    text = stringResource(R.string.filters),
+                    style = UiKitTheme.typographyReworked.primary.regular,
+                    color = UiKitTheme.colorsReworked.text.general.regular.default,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+            },
+            endContent = {
+                // TODO: [High] Implement
+            },
+            contentPadding = PaddingValues(vertical = TopBarDefaults.VerticalPadding),
+            modifier = modifier,
+        )
+    }
+}
