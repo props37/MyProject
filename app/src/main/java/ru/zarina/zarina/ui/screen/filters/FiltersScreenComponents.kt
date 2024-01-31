d706cf7000d6ae363e72c077bda536a16da947d4
Artem Sidorov <sidorovaa@melonfashion.com>
2024-01-31 15:43:47 +0300

    Update insets handling

--- app/src/main/java/ru/zarina/zarina/ui/screen/filters/FiltersScreenComponents.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screen/filters/FiltersScreenComponents.kt
@@ -17,8 +17,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeGestures
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemGestures
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -201,7 +201,7 @@ object FiltersScreenComponents {
                         when (filter) {
                             is PriceFilter -> {
                                 val horizontalPadding = 16.dp
-                                val systemGestureHorizontalPadding = WindowInsets.systemGestures
+                                val systemGestureHorizontalPadding = WindowInsets.safeGestures
                                     .asPaddingValues()
                                     .calculateLeftPadding(LocalLayoutDirection.current)
                                 val sliderHorizontalPadding =
