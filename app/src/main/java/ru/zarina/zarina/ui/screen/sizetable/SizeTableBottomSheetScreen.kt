a85be342dc7b5f79635a2083247cce340de37d3e
Artem Sidorov <sidorovaa@melonfashion.com>
2024-02-05 18:58:55 +0300

    Add SizeTable label UI component

--- app/src/main/java/ru/zarina/zarina/ui/screen/sizetable/SizeTableBottomSheetScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screen/sizetable/SizeTableBottomSheetScreen.kt
@@ -1,12 +1,22 @@
 package ru.zarina.zarina.ui.screen.sizetable
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.flow.Flow
+import ru.zarina.zarina.ui.common.component.ZarinaBottomSheet
 import ru.zarina.zarina.ui.common.tooling.preview.DensityPreviews
 import ru.zarina.zarina.ui.common.tooling.preview.FontScalePreviews
 import ru.zarina.zarina.ui.common.tooling.preview.ZarinaPreview
+import ru.zarina.zarina.ui.screen.sizetable.SizeTableScreenComponents.SizeTableLabel
 import ru.zarina.zarina.ui.screen.sizetable.SizeTableViewModel.SideEffect
 
 @Composable
@@ -24,7 +34,16 @@ private fun ScreenContent(
 ) {
     SizeTableScreenBehavior(sideEffects = sideEffects)
 
+    // TODO: [High] Handle insets
+    // TODO: [High] Handle status bar inset?
+    Column(modifier = Modifier.fillMaxWidth()) {
+        SizeTableLabel(modifier = Modifier.padding(start = 16.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
+        ZarinaBottomSheet(modifier = Modifier.fillMaxWidth()) {
+            Box(modifier = Modifier.height(200.dp))
+        }
+    }
 }
 
 @Preview
--- app/src/main/java/ru/zarina/zarina/ui/screen/sizetable/SizeTableScreenComponents.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screen/sizetable/SizeTableScreenComponents.kt
@@ -1,3 +1,44 @@
 package ru.zarina.zarina.ui.screen.sizetable
 
-object SizeTableScreenComponents
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import ru.zarina.zarina.R
+import ru.zarina.zarina.ui.common.component.button.ZarinaButton
+import ru.zarina.zarina.ui.common.component.button.ZarinaButtonDefaults
+import ru.zarina.zarina.ui.common.component.button.ZarinaButtonSize
+import ru.zarina.zarina.ui.theme.UiKitTheme
+
+object SizeTableScreenComponents {
+
+    @Composable
+    fun SizeTableLabel(modifier: Modifier = Modifier) {
+        ZarinaButton(
+            onClick = {},
+            isEnabled = false,
+            size = ZarinaButtonSize.Medium,
+            colors = ZarinaButtonDefaults.secondaryColors(
+                disabledBackgroundColor = UiKitTheme.colorsReworked.background.button.secondary.default,
+                disabledContentColor = UiKitTheme.colorsReworked.text.button.secondary.default,
+            ),
+            modifier = modifier,
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_ruler_24),
+                contentDescription = null,
+            )
+            Spacer(modifier = Modifier.width(6.dp))
+            Text(
+                text = stringResource(R.string.size_table).uppercase(),
+                modifier = Modifier.padding(top = 2.dp), // Circe font padding
+            )
+        }
+    }
+}
