3e4d20854b170a71b3895a501bf99cbe3178aa6e
Artem Sidorov <sidorovaa@melonfashion.com>
2024-02-21 19:03:05 +0300

    Add CartRepository::fetchCartProductIds method

--- app/src/main/java/ru/zarina/zarina/data/rework/cart/CartRepository.kt
+++ app/src/main/java/ru/zarina/zarina/data/rework/cart/CartRepository.kt
@@ -1,17 +1,30 @@
 package ru.zarina.zarina.data.rework.cart
 
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import ru.zarina.zarina.data.rework.cart.local.CartLocalDataSource
 import ru.zarina.zarina.data.rework.cart.remote.CartRemoteDataSource
+import ru.zarina.zarina.domain.rework.cart.CartProductIds
 import ru.zarina.zarina.domain.rework.cart.ProductAdditionToCartResult
 import ru.zarina.zarina.domain.rework.common.Barcode
+import ru.zarina.zarina.domain.rework.product.Product
 import javax.inject.Inject
 
 class CartRepository @Inject constructor(
     private val localDataSource: CartLocalDataSource,
     private val remoteDataSource: CartRemoteDataSource,
 ) {
+    val cartProductIds: StateFlow<Set<Product.Id>> = localDataSource.cartProductIds
+    val areCartProductIdsFetched: StateFlow<Boolean> = localDataSource.areCartProductIdsFetched
     val cartProductCount: StateFlow<Int> = localDataSource.cartProductCount
+
+    suspend fun fetchCartProductIds(): CartProductIds {
+        val cartProductIds = remoteDataSource.getCartProductIds().first()
+        localDataSource.setCartProductIds(cartProductIds.cartProductIds)
+        localDataSource.setCartProductCount(cartProductIds.cartProductCount)
+        localDataSource.setAreCartProductIdsFetched(true)
+        return cartProductIds
+    }
 
     suspend fun addProductToCart(barcode: Barcode, count: Int): ProductAdditionToCartResult {
         return remoteDataSource.addProductToCart(barcode, count)
@@ -19,5 +32,9 @@ class CartRepository @Inject constructor(
 
     fun setCartProductCount(count: Int) {
         localDataSource.setCartProductCount(count)
+    }
+
+    fun clear() {
+        localDataSource.clear()
     }
 }
--- app/src/main/java/ru/zarina/zarina/data/rework/cart/local/CartDataHolder.kt
+++ app/src/main/java/ru/zarina/zarina/data/rework/cart/local/CartDataHolder.kt
@@ -3,15 +3,45 @@ package ru.zarina.zarina.data.rework.cart.local
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import ru.zarina.zarina.domain.rework.product.Product
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class CartDataHolder @Inject constructor() {
+    private val _cartProductIds = MutableStateFlow<Set<Product.Id>>(emptySet())
+    val cartProductIds: StateFlow<Set<Product.Id>> = _cartProductIds.asStateFlow()
+
+    private val _areCartProductIdsFetched = MutableStateFlow(false)
+    val areCartProductIdsFetched: StateFlow<Boolean> = _areCartProductIdsFetched.asStateFlow()
+
     private val _cartProductCount = MutableStateFlow(0)
+
+    /**
+     * Can be larger than [cartProductIds] since a product can be added to the cart several times.
+     */
     val cartProductCount: StateFlow<Int> = _cartProductCount.asStateFlow()
+
+    fun setCartProductIds(ids: Set<Product.Id>) {
+        _cartProductIds.value = ids
+    }
+
+    fun setAreCartProductIdsFetched(fetched: Boolean) {
+        _areCartProductIdsFetched.value = fetched
+    }
+
+    fun addProductToCart(productId: Product.Id) {
+        _cartProductIds.update { it + productId }
+    }
 
     fun setCartProductCount(count: Int) {
         _cartProductCount.value = count
+    }
+
+    fun clear() {
+        _cartProductIds.value = emptySet()
+        _areCartProductIdsFetched.value = false
+        _cartProductCount.value = 0
     }
 }
