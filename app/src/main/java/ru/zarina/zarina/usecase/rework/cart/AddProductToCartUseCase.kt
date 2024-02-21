9ac7331774808b875b4c847150f0b4c4b6a47a23
Artem Sidorov <sidorovaa@melonfashion.com>
2024-02-21 19:22:45 +0300

    Update AddProductToCartUseCase

--- app/src/main/java/ru/zarina/zarina/usecase/rework/cart/AddProductToCartUseCase.kt
+++ app/src/main/java/ru/zarina/zarina/usecase/rework/cart/AddProductToCartUseCase.kt
@@ -6,12 +6,15 @@ import ru.zarina.zarina.di.rework.Qualifiers
 import ru.zarina.zarina.domain.rework.common.Barcode
 import ru.zarina.zarina.domain.rework.product.Product
 import ru.zarina.zarina.usecase.base.UseCase
+import ru.zarina.zarina.utils.clean.invoke
+import timber.log.Timber
 import javax.inject.Inject
 
 class AddProductToCartUseCase @Inject constructor(
     @Qualifiers.CoroutineDispatcher(Qualifiers.CoroutineDispatchers.IO)
     dispatcher: CoroutineDispatcher,
     private val cartRepository: CartRepository,
+    private val fetchCartProductIdsUseCase: FetchCartProductIdsUseCase,
 ) : UseCase<AddProductToCartUseCase.Params, Unit>(dispatcher) {
 
     override suspend fun execute(params: Params) {
@@ -20,6 +23,10 @@ class AddProductToCartUseCase @Inject constructor(
         val count = params.count
         val result = cartRepository.addProductToCart(productId, barcode, count)
         cartRepository.setCartProductCount(result.cartProductCount)
+        if (!cartRepository.areCartProductIdsFetched.value) {
+            Timber.w("Cart product IDs are not fetched. Trying to fetch")
+            fetchCartProductIdsUseCase()
+        }
     }
 
     data class Params(val productId: Product.Id, val barcode: Barcode, val count: Int)
