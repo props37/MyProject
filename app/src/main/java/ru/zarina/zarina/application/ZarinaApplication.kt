dae501a6be058d04fc36c7bb8e28ecff387d596c
Artem Sidorov <sidorovaa@melonfashion.com>
2024-03-01 16:51:11 +0300

    Disable Koin

--- app/src/main/java/ru/zarina/zarina/application/ZarinaApplication.kt
+++ app/src/main/java/ru/zarina/zarina/application/ZarinaApplication.kt
@@ -2,11 +2,7 @@ package ru.zarina.zarina.application
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.startKoin
 import ru.zarina.zarina.application.extension.ApplicationExtensionManager
-import ru.zarina.zarina.di.appModule
 import javax.inject.Inject
 
 @HiltAndroidApp
@@ -17,13 +13,6 @@ class ZarinaApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        startKoin {
-            androidLogger()
-            androidContext(this@ZarinaApplication)
-            modules(appModule)
-        }
-
         installApplicationExtensions()
     }
 
