e0ab74ed8841623832af507da20ec7302f2f8898
Artem Sidorov <sidorovaa@melonfashion.com>
2024-03-01 15:15:54 +0300

    Remove favorites and cart logic from RefreshAuthorizationTokensUseCase

--- app/src/main/java/ru/zarina/zarina/usecase/rework/authorization/RefreshAuthorizationTokensUseCase.kt
+++ app/src/main/java/ru/zarina/zarina/usecase/rework/authorization/RefreshAuthorizationTokensUseCase.kt
@@ -4,12 +4,9 @@ import kotlinx.coroutines.CoroutineDispatcher
 import ru.zarina.zarina.data.rework.authorization.AuthorizationRepository
 import ru.zarina.zarina.di.rework.Qualifiers
 import ru.zarina.zarina.usecase.base.UseCase
-import ru.zarina.zarina.usecase.rework.cart.FetchCartProductIdsUseCase
-import ru.zarina.zarina.usecase.rework.favorite.FetchFavoriteProductIdsUseCase
 import ru.zarina.zarina.utils.clean.invoke
 import timber.log.Timber
 import javax.inject.Inject
-import javax.inject.Provider
 
 // TODO: [Medium] Is synchronization needed?
 
@@ -18,9 +15,6 @@ class RefreshAuthorizationTokensUseCase @Inject constructor(
     dispatcher: CoroutineDispatcher,
     private val authorizationRepository: AuthorizationRepository,
     private val fetchUnauthorizedUserAuthorizationTokensUseCase: FetchUnauthorizedUserAuthorizationTokensUseCase,
-    // Inject Providers to avoid dependency loops
-    private val fetchFavoriteProductIdsUseCaseProvider: Provider<FetchFavoriteProductIdsUseCase>,
-    private val fetchCartProductIdsUseCaseProvider: Provider<FetchCartProductIdsUseCase>,
 ) : UseCase<Unit, Unit>(dispatcher) {
 
     override suspend fun execute(params: Unit) {
@@ -29,8 +23,6 @@ class RefreshAuthorizationTokensUseCase @Inject constructor(
             val newTokens = authorizationRepository.refreshAuthorizationTokens(currentTokens)
             authorizationRepository.setAuthorizationTokens(newTokens)
             Timber.v("Authorization tokens refreshed")
-            fetchFavoriteProductIdsUseCaseProvider.get().invoke()
-            fetchCartProductIdsUseCaseProvider.get().invoke()
         } else {
             fetchUnauthorizedUserAuthorizationTokensUseCase()
         }
