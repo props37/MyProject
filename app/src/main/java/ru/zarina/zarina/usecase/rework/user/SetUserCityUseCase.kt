64aa42bee2fcc9033fa6cafd41c42ed1ec4b5d99
Artem Sidorov <sidorovaa@melonfashion.com>
2024-03-07 11:34:37 +0300

    Fetch cart product IDs when the user changes their city

--- app/src/main/java/ru/zarina/zarina/usecase/rework/user/SetUserCityUseCase.kt
+++ app/src/main/java/ru/zarina/zarina/usecase/rework/user/SetUserCityUseCase.kt
@@ -5,17 +5,21 @@ import ru.zarina.zarina.data.rework.user.UserRepository
 import ru.zarina.zarina.di.rework.Qualifiers
 import ru.zarina.zarina.domain.rework.geography.City
 import ru.zarina.zarina.usecase.base.UseCase
+import ru.zarina.zarina.usecase.rework.cart.FetchCartProductIdsUseCase
+import ru.zarina.zarina.utils.clean.invoke
 import javax.inject.Inject
 
 class SetUserCityUseCase @Inject constructor(
     @Qualifiers.CoroutineDispatcher(Qualifiers.CoroutineDispatchers.IO)
     dispatcher: CoroutineDispatcher,
     private val userRepository: UserRepository,
+    private val fetchCartProductIdsUseCase: FetchCartProductIdsUseCase,
 ) : UseCase<SetUserCityUseCase.Params, Unit>(dispatcher) {
 
     override suspend fun execute(params: Params) {
         val city = params.city
         userRepository.setUserCity(city)
+        fetchCartProductIdsUseCase()
     }
 
     data class Params(val city: City)
