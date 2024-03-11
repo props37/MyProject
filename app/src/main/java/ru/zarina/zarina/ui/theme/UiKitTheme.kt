02b78c561bb3e6996ff2888460270105bb469520
Artem Sidorov <sidorovaa@melonfashion.com>
2024-03-11 19:05:31 +0300

    Add Deprecation annotations

--- app/src/main/java/ru/zarina/zarina/ui/theme/UiKitTheme.kt
+++ app/src/main/java/ru/zarina/zarina/ui/theme/UiKitTheme.kt
@@ -15,10 +15,18 @@ object UiKitTheme {
         @Composable
         get() = LocalUiKitTypography.current
 
+    @Deprecated(
+        message = "Use UiKitTheme.colors instead.",
+        replaceWith = ReplaceWith("UiKitTheme.colors"),
+    )
     val colorsOld: UiKitColorsOld
         @Composable
         get() = LocalUiKitColorsOld.current
 
+    @Deprecated(
+        message = "Use UiKitTheme.typography instead.",
+        replaceWith = ReplaceWith("UiKitTheme.typography"),
+    )
     val typographyOld: UiKitTypographyOld
         @Composable
         get() = LocalUiKitTypographyOld.current
