a87c341accaece353ce8bd9664b54481ffe3ab1f
Artem Sidorov <sidorovaa@melonfashion.com>
2024-03-12 15:15:34 +0300

    Add ZarinaButton cellColors

--- app/src/main/java/ru/zarina/zarina/ui/common/component/button/ZarinaButton.kt
+++ app/src/main/java/ru/zarina/zarina/ui/common/component/button/ZarinaButton.kt
@@ -244,9 +244,25 @@ object ZarinaButtonDefaults {
         disabledBorderColor = disabledBorderColor,
     )
 
-    // Also used for Cell buttons from design kit
     @Composable
     fun backlessColors(
+        backgroundColor: Color = Color.Unspecified,
+        contentColor: Color = UiKitTheme.colors.text.button.backless.default,
+        disabledBackgroundColor: Color = Color.Unspecified,
+        disabledContentColor: Color = UiKitTheme.colors.text.button.backless.disabled,
+        borderColor: Color = Color.Unspecified,
+        disabledBorderColor: Color = Color.Unspecified,
+    ): ZarinaButtonColors = ZarinaButtonColors(
+        backgroundColor = backgroundColor,
+        contentColor = contentColor,
+        disabledBackgroundColor = disabledBackgroundColor,
+        disabledContentColor = disabledContentColor,
+        borderColor = borderColor,
+        disabledBorderColor = disabledBorderColor,
+    )
+
+    @Composable
+    fun cellColors(
         backgroundColor: Color = Color.Unspecified,
         contentColor: Color = UiKitTheme.colors.text.button.backless.default,
         disabledBackgroundColor: Color = Color.Unspecified,
