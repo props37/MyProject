194665fd34e548e7f43fc3ec7bd874682729e9c5
Artem Sidorov <sidorovaa@melonfashion.com>
2024-03-12 15:40:51 +0300

    Move files

--- app/src/main/java/ru/zarina/zarina/ui/common/component/screen/ZarinaScreenPlaceholder.kt
+++ app/src/main/java/ru/zarina/zarina/ui/common/component/screen/ZarinaScreenPlaceholder.kt
@@ -1,4 +1,4 @@
-package ru.zarina.zarina.ui.common.component
+package ru.zarina.zarina.ui.common.component.screen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
--- app/src/main/java/ru/zarina/zarina/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screen/profile/ProfileScreen.kt
@@ -3,7 +3,7 @@ package ru.zarina.zarina.ui.screen.profile
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import ru.zarina.zarina.ui.common.behavior.bottomnavbar.ForcedBottomNavBarBehavior
-import ru.zarina.zarina.ui.common.component.ZarinaScreenPlaceholder
+import ru.zarina.zarina.ui.common.component.screen.ZarinaScreenPlaceholder
 import ru.zarina.zarina.ui.common.tooling.preview.ZarinaPreview
 
 @Composable
