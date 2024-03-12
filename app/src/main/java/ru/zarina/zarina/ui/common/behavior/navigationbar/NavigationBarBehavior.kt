2fd89029c955ead976f44590a669bf9591fec4d3
Artem Sidorov <sidorovaa@melonfashion.com>
2024-03-12 11:36:26 +0300

    Remove unused code

--- app/src/main/java/ru/zarina/zarina/ui/common/behavior/navigationbar/NavigationBarBehavior.kt
+++ app/src/main/java/ru/zarina/zarina/ui/common/behavior/navigationbar/NavigationBarBehavior.kt
@@ -2,6 +2,13 @@ package ru.zarina.zarina.ui.common.behavior.navigationbar
 
 import ru.zarina.zarina.base.behavior.Behavior
 
+@Deprecated(
+    message = "Use BottomNavBarBehavior instead.",
+    replaceWith = ReplaceWith(
+        expression = "BottomNavBarBehavior",
+        "ru.zarina.zarina.ui.common.behavior.bottomnavbar",
+    )
+)
 sealed interface NavigationBarBehavior : Behavior {
 
     val isAnimated: Boolean
--- app/src/main/java/ru/zarina/zarina/ui/common/behavior/navigationbar/NavigationBarController.kt
+++ app/src/main/java/ru/zarina/zarina/ui/common/behavior/navigationbar/NavigationBarController.kt
@@ -4,8 +4,22 @@ import androidx.compose.runtime.staticCompositionLocalOf
 import ru.zarina.zarina.base.behavior.BehaviorController
 import ru.zarina.zarina.base.behavior.NoOpBehaviorController
 
+@Deprecated(
+    message = "Use BottomNavBarBehaviorController instead.",
+    replaceWith = ReplaceWith(
+        expression = "BottomNavBarBehaviorController",
+        "ru.zarina.zarina.ui.common.behavior.bottomnavbar",
+    ),
+)
 typealias NavigationBarController = BehaviorController<NavigationBarBehavior>
 
+@Deprecated(
+    message = "Use LocalBottomNavBarBehaviorController instead.",
+    replaceWith = ReplaceWith(
+        expression = "LocalBottomNavBarBehaviorController",
+        "ru.zarina.zarina.ui.common.behavior.bottomnavbar",
+    ),
+)
 val LocalNavigationBarController = staticCompositionLocalOf<NavigationBarController> {
     NoOpBehaviorController(NavigationBarBehavior.DEFAULT)
 }
