fb9a471ea9e2faaab9b873a28a63ea8b6a116ada
Artem Sidorov <sidorovaa@melonfashion.com>
2024-03-12 16:33:31 +0300

    Move files

--- app/src/main/java/ru/zarina/zarina/ui/common/component/pricefilter/PriceFilter.kt
+++ app/src/main/java/ru/zarina/zarina/ui/common/component/pricefilter/PriceFilter.kt
@@ -1,4 +1,4 @@
-package ru.zarina.zarina.ui.common.component
+package ru.zarina.zarina.ui.common.component.pricefilter
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Canvas
