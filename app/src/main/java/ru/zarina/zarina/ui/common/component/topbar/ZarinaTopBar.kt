b38375777353458946dd8a9c878b353eac0c12ec
Artem Sidorov <sidorovaa@melonfashion.com>
2024-03-24 21:56:12 +0500

    Add contentColor parameter to ZarinaTopBar

--- app/src/main/java/ru/zarina/zarina/ui/common/component/topbar/ZarinaTopBar.kt
+++ app/src/main/java/ru/zarina/zarina/ui/common/component/topbar/ZarinaTopBar.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
@@ -29,6 +31,7 @@ fun ZarinaTopBar(
     centerContent: (@Composable () -> Unit)? = null,
     endContent: (@Composable () -> Unit)? = null,
     backgroundColor: Color = UiKitTheme.colors.background.general.regular.default,
+    contentColor: Color = UiKitTheme.colors.text.general.regular.default,
     contentPadding: PaddingValues = TopBarDefaults.ContentPadding,
 ) {
     val content = @Composable {
@@ -37,58 +40,60 @@ fun ZarinaTopBar(
         Box(modifier = Modifier.layoutId(LayoutId.EndContent)) { endContent?.invoke() }
     }
 
-    Layout(
-        content = content,
-        modifier = modifier
-            .fillMaxWidth()
-            .heightIn(min = TopBarDefaults.MinHeight)
-            .background(backgroundColor)
-            .padding(contentPadding),
-    ) { measurables, constraints ->
-        val startContentMeasurable = measurables.find { it.layoutId == LayoutId.StartContent }
-        val centerContentMeasurable = measurables.find { it.layoutId == LayoutId.CenterContent }
-        val endContentMeasurable = measurables.find { it.layoutId == LayoutId.EndContent }
+    CompositionLocalProvider(LocalContentColor provides contentColor) {
+        Layout(
+            content = content,
+            modifier = modifier
+                .fillMaxWidth()
+                .heightIn(min = TopBarDefaults.MinHeight)
+                .background(backgroundColor)
+                .padding(contentPadding),
+        ) { measurables, constraints ->
+            val startContentMeasurable = measurables.find { it.layoutId == LayoutId.StartContent }
+            val centerContentMeasurable = measurables.find { it.layoutId == LayoutId.CenterContent }
+            val endContentMeasurable = measurables.find { it.layoutId == LayoutId.EndContent }
 
-        val sideContentConstraints = constraints.copy(minWidth = 0, minHeight = 0)
-        val startContentPlaceable = startContentMeasurable?.measure(sideContentConstraints)
-        val endContentPlaceable = endContentMeasurable?.measure(sideContentConstraints)
-        val sideContentMaxWidth = maxOf(
-            startContentPlaceable?.width ?: 0,
-            endContentPlaceable?.width ?: 0,
-        )
-
-        val centerContentMaxWidth = constraints.maxWidth -
-                sideContentMaxWidth * 2 -
-                CenterContentHorizontalPadding.roundToPx() * 2
-        val centerContentConstraints = constraints.copy(
-            minWidth = 0,
-            minHeight = 0,
-            maxWidth = centerContentMaxWidth,
-        )
-        val centerContentPlaceable = centerContentMeasurable?.measure(centerContentConstraints)
-
-        val contentMaxHeight = maxOf(
-            startContentPlaceable?.height ?: 0,
-            centerContentPlaceable?.height ?: 0,
-            endContentPlaceable?.height ?: 0,
-        )
-
-        val layoutWidth = constraints.maxWidth
-        val layoutHeight = maxOf(constraints.minHeight, contentMaxHeight)
-
-        layout(layoutWidth, layoutHeight) {
-            startContentPlaceable?.placeRelative(
-                x = 0,
-                y = (layoutHeight - startContentPlaceable.height) / 2,
+            val sideContentConstraints = constraints.copy(minWidth = 0, minHeight = 0)
+            val startContentPlaceable = startContentMeasurable?.measure(sideContentConstraints)
+            val endContentPlaceable = endContentMeasurable?.measure(sideContentConstraints)
+            val sideContentMaxWidth = maxOf(
+                startContentPlaceable?.width ?: 0,
+                endContentPlaceable?.width ?: 0,
             )
-            centerContentPlaceable?.placeRelative(
-                x = layoutWidth / 2 - centerContentPlaceable.width / 2,
-                y = (layoutHeight - centerContentPlaceable.height) / 2,
+
+            val centerContentMaxWidth = constraints.maxWidth -
+                    sideContentMaxWidth * 2 -
+                    CenterContentHorizontalPadding.roundToPx() * 2
+            val centerContentConstraints = constraints.copy(
+                minWidth = 0,
+                minHeight = 0,
+                maxWidth = centerContentMaxWidth,
             )
-            endContentPlaceable?.placeRelative(
-                x = layoutWidth - endContentPlaceable.width,
-                y = (layoutHeight - endContentPlaceable.height) / 2,
+            val centerContentPlaceable = centerContentMeasurable?.measure(centerContentConstraints)
+
+            val contentMaxHeight = maxOf(
+                startContentPlaceable?.height ?: 0,
+                centerContentPlaceable?.height ?: 0,
+                endContentPlaceable?.height ?: 0,
             )
+
+            val layoutWidth = constraints.maxWidth
+            val layoutHeight = maxOf(constraints.minHeight, contentMaxHeight)
+
+            layout(layoutWidth, layoutHeight) {
+                startContentPlaceable?.placeRelative(
+                    x = 0,
+                    y = (layoutHeight - startContentPlaceable.height) / 2,
+                )
+                centerContentPlaceable?.placeRelative(
+                    x = layoutWidth / 2 - centerContentPlaceable.width / 2,
+                    y = (layoutHeight - centerContentPlaceable.height) / 2,
+                )
+                endContentPlaceable?.placeRelative(
+                    x = layoutWidth - endContentPlaceable.width,
+                    y = (layoutHeight - endContentPlaceable.height) / 2,
+                )
+            }
         }
     }
 }
