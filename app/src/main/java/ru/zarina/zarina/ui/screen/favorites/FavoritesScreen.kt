68aa0aba5ab6224f1a252cd521cbe65e98e87021
Artem Sidorov <sidorovaa@melonfashion.com>
2024-03-26 08:06:32 +0500

    Implement Favorites screen UI

--- app/src/main/java/ru/zarina/zarina/ui/screen/favorites/FavoritesScreen.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screen/favorites/FavoritesScreen.kt
@@ -6,19 +6,23 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.union
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
 import ru.zarina.zarina.domain.product.Product
+import ru.zarina.zarina.ui.common.component.ProductGrid
 import ru.zarina.zarina.ui.common.tooling.preview.DensityPreviews
 import ru.zarina.zarina.ui.common.tooling.preview.FontScalePreviews
 import ru.zarina.zarina.ui.common.tooling.preview.ZarinaPreview
+import ru.zarina.zarina.ui.screen.favorites.FavoritesScreenComponents.FavoriteProductsNotFoundPlaceholder
 import ru.zarina.zarina.ui.screen.favorites.FavoritesScreenComponents.TopBar
 import ru.zarina.zarina.ui.screen.favorites.FavoritesViewModel.SideEffect
 import ru.zarina.zarina.ui.theme.UiKitTheme
@@ -29,6 +33,14 @@ fun FavoritesScreen(
 ) {
     ScreenContent(
         productPagingDataFlow = viewModel.productPagingDataFlow,
+        onProductClicked = { /* TODO */ },
+        onAddProductToFavoritesClicked = { /* TODO */ },
+        onAddProductToCartClicked = { /* TODO */ },
+        onSubscribeToProductClicked = { /* TODO */ },
+        onRefreshProducts = { /* TODO */ },
+        onProductsErrorRefreshClicked = { /* TODO */ },
+        onClearFavoritesClicked = { /* TODO */ },
+        onGoToCatalogClicked = { /* TODO */ },
         sideEffects = viewModel.sideEffects,
     )
 }
@@ -36,6 +48,14 @@ fun FavoritesScreen(
 @Composable
 private fun ScreenContent(
     productPagingDataFlow: Flow<PagingData<Product>>,
+    onProductClicked: (Product) -> Unit,
+    onAddProductToFavoritesClicked: (Product) -> Unit,
+    onAddProductToCartClicked: (Product) -> Unit,
+    onSubscribeToProductClicked: (Product) -> Unit,
+    onRefreshProducts: () -> Unit,
+    onProductsErrorRefreshClicked: () -> Unit,
+    onClearFavoritesClicked: () -> Unit,
+    onGoToCatalogClicked: () -> Unit,
     sideEffects: Flow<SideEffect>,
 ) {
     FavoritesScreenBehavior(sideEffects = sideEffects)
@@ -51,8 +71,27 @@ private fun ScreenContent(
     ) {
         TopBar(
             isClearButtonVisible = true, // TODO: [High] Implement
-            onClearClicked = { /*TODO*/ },
+            onClearClicked = onClearFavoritesClicked,
             modifier = Modifier.fillMaxWidth(),
+        )
+
+        ProductGrid(
+            productPagingDataFlow = productPagingDataFlow,
+            onProductClicked = onProductClicked,
+            onAddToFavoritesClicked = onAddProductToFavoritesClicked,
+            onAddToCartClicked = onAddProductToCartClicked,
+            onSubscribeClicked = onSubscribeToProductClicked,
+            onRefreshProducts = onRefreshProducts,
+            onProductsErrorRefreshClicked = onProductsErrorRefreshClicked,
+            noProductsPlaceholder = {
+                FavoriteProductsNotFoundPlaceholder(
+                    onGoToCatalogClicked = onGoToCatalogClicked,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                )
+            },
+            modifier = Modifier.fillMaxSize(),
         )
     }
 }
--- app/src/main/java/ru/zarina/zarina/ui/screen/favorites/FavoritesScreenComponents.kt
+++ app/src/main/java/ru/zarina/zarina/ui/screen/favorites/FavoritesScreenComponents.kt
@@ -6,9 +6,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import ru.zarina.zarina.R
+import ru.zarina.zarina.ui.base.rememberErrorState
 import ru.zarina.zarina.ui.common.component.button.ZarinaButton
 import ru.zarina.zarina.ui.common.component.button.ZarinaButtonDefaults
 import ru.zarina.zarina.ui.common.component.button.ZarinaButtonSize
+import ru.zarina.zarina.ui.common.component.screen.ZarinaErrorScreen
 import ru.zarina.zarina.ui.common.component.topbar.ZarinaTopBar
 import ru.zarina.zarina.ui.theme.UiKitTheme
 import ru.zarina.zarina.util.compose.animation.AnimatedContentDefaultEnterTransition
@@ -46,6 +48,25 @@ object FavoritesScreenComponents {
                     }
                 }
             },
+            modifier = modifier,
+        )
+    }
+
+    @Composable
+    fun FavoriteProductsNotFoundPlaceholder(
+        onGoToCatalogClicked: () -> Unit,
+        modifier: Modifier = Modifier,
+    ) {
+        val state = rememberErrorState(
+            iconResId = R.drawable.ic_heart_outline_64,
+            title = stringResource(R.string.favorites_screen_no_favorites_placeholder_title),
+            body = stringResource(R.string.favorites_screen_no_favorites_placeholder_body),
+            buttonText = stringResource(R.string.go_to_catalog)
+        )
+
+        ZarinaErrorScreen(
+            state = state,
+            onButtonClicked = onGoToCatalogClicked,
             modifier = modifier,
         )
     }
