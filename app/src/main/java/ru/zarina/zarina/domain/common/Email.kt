e735f08d0c93cf3124f9f1c704ea1336b2c24328
Artem Sidorov <sidorovaa@melonfashion.com>
2024-04-02 16:56:39 +0300

    Introduce Email and PhoneNumber domain models

---
+++ app/src/main/java/ru/zarina/zarina/domain/common/Email.kt
@@ -0,0 +1,4 @@
+package ru.zarina.zarina.domain.common
+
+@JvmInline
+value class Email(val value: String)
---
+++ app/src/main/java/ru/zarina/zarina/domain/common/PhoneNumber.kt
@@ -0,0 +1,4 @@
+package ru.zarina.zarina.domain.common
+
+@JvmInline
+value class PhoneNumber(val value: String)
