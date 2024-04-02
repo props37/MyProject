0a52edb90185910312cddf227aa70709cb5c6768
Artem Sidorov <sidorovaa@melonfashion.com>
2024-04-02 17:34:30 +0300

    Add PhoneNumber::create factory method

--- app/src/main/java/ru/zarina/zarina/domain/common/PhoneNumber.kt
+++ app/src/main/java/ru/zarina/zarina/domain/common/PhoneNumber.kt
@@ -1,4 +1,13 @@
 package ru.zarina.zarina.domain.common
 
+import android.telephony.PhoneNumberUtils
+
 @JvmInline
-value class PhoneNumber(val value: String)
+value class PhoneNumber private constructor(val value: String) {
+    companion object {
+        fun create(phone: String): PhoneNumber {
+            val normalizedPhone = PhoneNumberUtils.normalizeNumber(phone)
+            return PhoneNumber(normalizedPhone)
+        }
+    }
+}
