d944954d83e8f1ce939ec3d80e6b957213144566
Artem Sidorov <sidorovaa@melonfashion.com>
2024-04-02 17:39:19 +0300

    Introduce ValidatePhoneNumberUseCase

---
+++ app/src/main/java/ru/zarina/zarina/usecase/user/ValidatePhoneNumberUseCase.kt
@@ -0,0 +1,26 @@
+package ru.zarina.zarina.usecase.user
+
+import android.telephony.PhoneNumberUtils
+import kotlinx.coroutines.CoroutineDispatcher
+import ru.zarina.zarina.base.usecase.UseCase
+import ru.zarina.zarina.di.Qualifiers
+import ru.zarina.zarina.domain.common.PhoneNumber
+import ru.zarina.zarina.domain.exception.EmptyPhoneNumberException
+import ru.zarina.zarina.domain.exception.InvalidPhoneNumberException
+import javax.inject.Inject
+
+class ValidatePhoneNumberUseCase @Inject constructor(
+    @Qualifiers.CoroutineDispatcher(Qualifiers.CoroutineDispatchers.IO)
+    dispatcher: CoroutineDispatcher,
+) : UseCase<ValidatePhoneNumberUseCase.Params, Unit>(dispatcher) {
+
+    override suspend fun execute(params: Params) {
+        val phone = params.phoneNumber.value.trim()
+        when {
+            phone.isEmpty() -> throw EmptyPhoneNumberException()
+            !PhoneNumberUtils.isGlobalPhoneNumber(phone) -> throw InvalidPhoneNumberException()
+        }
+    }
+
+    data class Params(val phoneNumber: PhoneNumber)
+}
