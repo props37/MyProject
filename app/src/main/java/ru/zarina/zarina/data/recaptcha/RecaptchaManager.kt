47b09d7f91420de3e01b4c66a409fab3303d9aa3
Artem Sidorov <sidorovaa@melonfashion.com>
2024-04-03 10:37:10 +0300

    Update RecaptchaManager

--- app/src/main/java/ru/zarina/zarina/data/recaptcha/RecaptchaManager.kt
+++ app/src/main/java/ru/zarina/zarina/data/recaptcha/RecaptchaManager.kt
@@ -2,20 +2,38 @@ package ru.zarina.zarina.data.recaptcha
 
 import android.app.Application
 import com.google.android.recaptcha.Recaptcha
+import com.google.android.recaptcha.RecaptchaAction
 import com.google.android.recaptcha.RecaptchaClient
 import com.google.android.recaptcha.RecaptchaException
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import ru.zarina.zarina.BuildConfig
+import ru.zarina.zarina.domain.common.Token
 import timber.log.Timber
+import java.lang.ref.WeakReference
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class RecaptchaManager @Inject constructor() {
-    private val initMutex = Mutex()
+    private var applicationRef: WeakReference<Application>? = null
     private var client: RecaptchaClient? = null
+    private val initMutex = Mutex()
 
     suspend fun init(application: Application) {
+        applicationRef = WeakReference(application)
         getClient(application)
+    }
+
+    suspend fun execute(action: RecaptchaAction): Token {
+        val application = applicationRef?.get()
+        checkNotNull(application) { "Application is null. Did you forget to call RecaptchaManager.init()?" }
+
+        val client = getClient(application)
+        checkNotNull(client) { "RecaptchaClient is not initialized" }
+
+        val token = client.execute(action).getOrThrow()
+        return Token(token)
     }
 
     private suspend fun getClient(application: Application): RecaptchaClient? {
@@ -29,17 +47,13 @@ class RecaptchaManager @Inject constructor() {
             val result = Recaptcha.getClient(application, BuildConfig.RECAPTCHA_KEY)
                 .onSuccess { client = it }
                 .onFailure { e ->
-                    if (e is RecaptchaException) {
-                        Timber.tag(TAG).e(
-                            e,
-                            "Recaptcha client initialization failed. " +
-                                    "Code: ${e.errorCode}, message: ${e.errorMessage}"
-                        )
+                    val message = if (e is RecaptchaException) {
+                        "Recaptcha client initialization failed. Code: ${e.errorCode}, message: ${e.errorMessage}"
                     } else {
-                        Timber.tag(TAG).e(e, "Recaptcha client initialization failed")
+                        "Recaptcha client initialization failed"
                     }
+                    Timber.tag(TAG).e(e, message)
                 }
-
             result.getOrNull()
         }
     }
