94b34b191b18862f1c5f362cb0a4719640984210
Artem Sidorov <sidorovaa@melonfashion.com>
2024-04-05 16:56:52 +0300

    Add User domain model

--- app/src/main/java/ru/livetyping/zarina/data/user/local/database/entity/UserEntity.kt
+++ app/src/main/java/ru/livetyping/zarina/data/user/local/database/entity/UserEntity.kt
@@ -3,6 +3,10 @@ package ru.livetyping.zarina.data.user.local.database.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import ru.livetyping.zarina.domain.common.Email
+import ru.livetyping.zarina.domain.common.PhoneNumber
+import ru.livetyping.zarina.domain.user.User
+import java.time.LocalDate
 
 @Entity(tableName = UserEntity.TABLE_NAME)
 data class UserEntity(
@@ -14,17 +18,26 @@ data class UserEntity(
     val email: String,
 
     @ColumnInfo(name = FIELD_PHONE)
-    val phone: String,
+    val phone: String?,
 
     @ColumnInfo(name = FIELD_FIRST_NAME)
-    val firstName: String,
+    val firstName: String?,
 
     @ColumnInfo(name = FIELD_LAST_NAME)
     val lastName: String?,
 
     @ColumnInfo(name = FIELD_BIRTH_DATE)
-    val birthDate: String,
+    val birthDate: String?,
 ) {
+    fun toUser(): User = User(
+        id = User.Id(id),
+        email = Email.create(email),
+        phone = phone?.let { PhoneNumber.create(it) },
+        firstName = firstName,
+        lastName = lastName,
+        birthDate = birthDate?.let { LocalDate.parse(it) },
+    )
+
     companion object {
         const val TABLE_NAME = "user"
 
@@ -34,5 +47,14 @@ data class UserEntity(
         const val FIELD_FIRST_NAME = "user_first_name"
         const val FIELD_LAST_NAME = "user_last_name"
         const val FIELD_BIRTH_DATE = "user_birth_date"
+
+        fun from(user: User): UserEntity = UserEntity(
+            id = user.id.value,
+            email = user.email.value,
+            phone = user.phone?.value,
+            firstName = user.firstName,
+            lastName = user.lastName,
+            birthDate = user.birthDate.toString(),
+        )
     }
 }
