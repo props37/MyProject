e75f531757aa16d2a5b4369d95fce2dc38393304
Artem Sidorov <sidorovaa@melonfashion.com>
2024-04-08 16:58:36 +0300

    Complete ConfirmSignUpUseCase

--- app/src/main/java/ru/livetyping/zarina/usecase/user/ConfirmSignUpUseCase.kt
+++ app/src/main/java/ru/livetyping/zarina/usecase/user/ConfirmSignUpUseCase.kt
@@ -5,6 +5,7 @@ import ru.livetyping.zarina.base.usecase.UseCase
 import ru.livetyping.zarina.data.user.UserRepository
 import ru.livetyping.zarina.di.Qualifiers
 import ru.livetyping.zarina.domain.common.PhoneNumber
+import ru.livetyping.zarina.usecase.authorization.SetAuthorizationTokensUseCase
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -12,6 +13,8 @@ class ConfirmSignUpUseCase @Inject constructor(
     @Qualifiers.CoroutineDispatcher(Qualifiers.CoroutineDispatchers.IO)
     dispatcher: CoroutineDispatcher,
     private val userRepository: UserRepository,
+    private val setAuthorizationTokensUseCase: SetAuthorizationTokensUseCase,
+    private val setUserUseCase: SetUserUseCase,
 ) : UseCase<ConfirmSignUpUseCase.Params, Unit>(dispatcher) {
 
     override suspend fun execute(params: Params) {
@@ -19,8 +22,15 @@ class ConfirmSignUpUseCase @Inject constructor(
         val otp = params.otp
         Timber.v("Confirm sign up. Phone: $phone, otp: $otp")
 
-        userRepository.confirmSignUp(phone, otp)
-        // TODO: [High] Implement
+        val authorizationResult = userRepository.confirmSignUp(phone, otp)
+        val authTokens = authorizationResult.tokens
+        val user = authorizationResult.user
+
+        val setAuthorizationTokensParams = SetAuthorizationTokensUseCase.Params(authTokens)
+        setAuthorizationTokensUseCase(setAuthorizationTokensParams)
+
+        val setUserParams = SetUserUseCase.Params(user)
+        setUserUseCase(setUserParams)
     }
 
     data class Params(val phone: PhoneNumber, val otp: String)
