146b730cdf03802c6ebed6fbccdd7534d66a47e8
Artem Sidorov <sidorovaa@melonfashion.com>
2024-04-08 16:04:37 +0300

    Add HttpClientAuthorizationTokensCleaner

---
+++ app/src/main/java/ru/livetyping/zarina/data/common/remote/ktor/HttpClientAuthorizationTokensCleaner.kt
@@ -0,0 +1,27 @@
+package ru.livetyping.zarina.data.common.remote.ktor
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.auth.providers.BearerAuthProvider
+import io.ktor.client.plugins.pluginOrNull
+import ru.livetyping.zarina.data.common.remote.ktor.plugin.ZarinaAuth
+import ru.livetyping.zarina.di.Qualifiers
+import timber.log.Timber
+import javax.inject.Inject
+
+class HttpClientAuthorizationTokensCleaner @Inject constructor(
+    @Qualifiers.ZarinaApi(Qualifiers.ZarinaApis.AUTHORIZED)
+    private val httpClient: HttpClient,
+) {
+    fun clearAuthorizationTokens() {
+        Timber.v("Clear HttpClient authorization tokens")
+        httpClient.clearBearerTokens()
+    }
+}
+
+private fun HttpClient.clearBearerTokens() {
+    this.pluginOrNull(ZarinaAuth)
+        ?.providers
+        ?.filterIsInstance<BearerAuthProvider>()
+        ?.firstOrNull()
+        ?.clearToken()
+}
