7328e6a16b4de62ebeae0d0d8778542aabb0a1b5
Artem Sidorov <sidorovaa@melonfashion.com>
2024-04-08 16:39:58 +0300

    Clear HttpClient tokens on SetAuthorizationTokensUseCase

--- app/src/main/java/ru/livetyping/zarina/usecase/authorization/SetAuthorizationTokensUseCase.kt
+++ app/src/main/java/ru/livetyping/zarina/usecase/authorization/SetAuthorizationTokensUseCase.kt
@@ -3,6 +3,7 @@ package ru.livetyping.zarina.usecase.authorization
 import kotlinx.coroutines.CoroutineDispatcher
 import ru.livetyping.zarina.base.usecase.UseCase
 import ru.livetyping.zarina.data.authorization.AuthorizationRepository
+import ru.livetyping.zarina.data.common.remote.ktor.HttpClientAuthorizationTokensCleaner
 import ru.livetyping.zarina.di.Qualifiers
 import ru.livetyping.zarina.domain.authorization.AuthorizationTokens
 import timber.log.Timber
@@ -12,12 +13,14 @@ class SetAuthorizationTokensUseCase @Inject constructor(
     @Qualifiers.CoroutineDispatcher(Qualifiers.CoroutineDispatchers.IO)
     dispatcher: CoroutineDispatcher,
     private val authorizationRepository: AuthorizationRepository,
+    private val httpClientAuthorizationTokensCleaner: HttpClientAuthorizationTokensCleaner,
 ) : UseCase<SetAuthorizationTokensUseCase.Params, Unit>(dispatcher) {
 
     override suspend fun execute(params: Params) {
         val tokens = params.tokens
         Timber.v("Set authorization tokens: $tokens")
         authorizationRepository.setAuthorizationTokens(tokens)
+        httpClientAuthorizationTokensCleaner.clearAuthorizationTokens()
     }
 
     data class Params(val tokens: AuthorizationTokens)
