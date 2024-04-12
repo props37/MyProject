cc0268458b366d881cf5c23491700d20cb295f6b
Artem Sidorov <sidorovaa@melonfashion.com>
2024-04-12 16:41:13 +0300

    Extract SmsOtpScreenContent

--- app/src/main/java/ru/livetyping/zarina/ui/screen/common/otp/OtpScreenComponents.kt
+++ app/src/main/java/ru/livetyping/zarina/ui/screen/common/otp/OtpScreenComponents.kt
@@ -1,23 +1,22 @@
-package ru.livetyping.zarina.ui.screen.signup.otp
+package ru.livetyping.zarina.ui.screen.common.otp
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import ru.livetyping.zarina.R
 import ru.livetyping.zarina.ui.common.component.button.ZarinaBackIconButton
 import ru.livetyping.zarina.ui.common.component.topbar.TopBarDefaults
 import ru.livetyping.zarina.ui.common.component.topbar.ZarinaTopBar
 import ru.livetyping.zarina.ui.theme.UiKitTheme
 
-object SignUpOtpScreenComponents {
+object OtpScreenComponents {
 
     @Composable
     fun TopBar(
+        title: String,
         onBackClicked: () -> Unit,
         modifier: Modifier = Modifier,
     ) {
@@ -31,7 +30,7 @@ object SignUpOtpScreenComponents {
             },
             centerContent = {
                 Text(
-                    text = stringResource(R.string.registration),
+                    text = title,
                     style = UiKitTheme.typography.primary.regular,
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
