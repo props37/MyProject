da9eaa3642b572aa90ac72f4ae749f24f0c5c576
Artem Sidorov <sidorovaa@melonfashion.com>
2024-04-15 18:47:45 +0300

    Replace GetProductPagingDataFlowUseCase with ProductsPager

--- app/src/main/java/ru/livetyping/zarina/ui/screen/products/ProductsInteractor.kt
+++ app/src/main/java/ru/livetyping/zarina/ui/screen/products/ProductsInteractor.kt
@@ -1,16 +1,16 @@
 package ru.livetyping.zarina.ui.screen.products
 
+import ru.livetyping.zarina.ui.screen.products.paging.ProductPager
 import ru.livetyping.zarina.usecase.cart.AddProductToCartUseCase
 import ru.livetyping.zarina.usecase.cart.GetCartProductIdsFlowUseCase
 import ru.livetyping.zarina.usecase.category.GetCategoryFlowUseCase
 import ru.livetyping.zarina.usecase.favorite.GetFavoriteProductIdsFlowUseCase
 import ru.livetyping.zarina.usecase.favorite.ToggleProductPresenceInFavoritesUseCase
-import ru.livetyping.zarina.usecase.product.GetProductPagingDataFlowUseCase
 import javax.inject.Inject
 
 class ProductsInteractor @Inject constructor(
     val getCategoryFlow: GetCategoryFlowUseCase,
-    val getProductPagingDataFlow: GetProductPagingDataFlowUseCase,
+    val productPager: ProductPager,
     val getFavoriteProductIdsFlow: GetFavoriteProductIdsFlowUseCase,
     val toggleProductPresenceInFavorites: ToggleProductPresenceInFavoritesUseCase,
     val getCartProductIdsFlow: GetCartProductIdsFlowUseCase,
--- app/src/main/java/ru/livetyping/zarina/ui/screen/products/paging/ProductPager.kt
+++ app/src/main/java/ru/livetyping/zarina/ui/screen/products/paging/ProductPager.kt
@@ -1,10 +1,9 @@
-package ru.livetyping.zarina.usecase.product
+package ru.livetyping.zarina.ui.screen.products.paging
 
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
-import ru.livetyping.zarina.base.usecase.BasicUseCase
 import ru.livetyping.zarina.data.product.ProductRepository
 import ru.livetyping.zarina.data.product.pagination.ProductPagingSource
 import ru.livetyping.zarina.domain.category.Category
@@ -14,24 +13,26 @@ import ru.livetyping.zarina.domain.product.Product
 import timber.log.Timber
 import javax.inject.Inject
 
-// TODO: [Medium] Move logic to repository
-
-class GetProductPagingDataFlowUseCase @Inject constructor(
+class ProductPager @Inject constructor(
     private val productRepository: ProductRepository,
-) : BasicUseCase<GetProductPagingDataFlowUseCase.Params, Flow<PagingData<Product>>> {
-
-    override fun invoke(params: Params): Flow<PagingData<Product>> {
+) {
+    fun getProductPagingDataFlow(
+        categoryId: Category.Id,
+        filters: Filters?,
+        sorting: Sorting,
+        onAvailableFiltersReceived: (Filters) -> Unit,
+    ): Flow<PagingData<Product>> {
         return Pager(
             config = getPagingConfig(),
             pagingSourceFactory = {
                 ProductPagingSource(
-                    categoryId = params.categoryId,
-                    filters = params.filters,
-                    sorting = params.sorting,
+                    categoryId = categoryId,
+                    filters = filters,
+                    sorting = sorting,
                     productRepository = productRepository,
                     onAvailableFiltersReceived = {
                         Timber.tag(TAG).v("Available filters received: $it")
-                        params.onAvailableFiltersReceived(it)
+                        onAvailableFiltersReceived(it)
                     },
                 )
             },
@@ -48,19 +49,12 @@ class GetProductPagingDataFlowUseCase @Inject constructor(
         )
     }
 
-    data class Params(
-        val categoryId: Category.Id,
-        val filters: Filters?,
-        val sorting: Sorting,
-        val onAvailableFiltersReceived: (Filters) -> Unit,
-    )
-
     companion object {
         private const val PAGE_SIZE = 20
         private const val PREFETCH_DISTANCE = PAGE_SIZE
         private const val INITIAL_LOAD_SIZE = PAGE_SIZE * 2
         private const val MAX_SIZE = 300
 
-        private const val TAG = "GetProductPagingDataFlowUseCase"
+        private const val TAG = "ProductsPager"
     }
 }
