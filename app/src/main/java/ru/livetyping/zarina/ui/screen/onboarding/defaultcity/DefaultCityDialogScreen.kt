53b4f597dff803ec5ff1c8ced0954c69056daa3c
Artem Sidorov <sidorovaa@melonfashion.com>
2024-04-16 17:23:39 +0300

    Refactor DefaultCityDialogScreen

--- app/src/main/java/ru/livetyping/zarina/ui/screen/onboarding/defaultcity/DefaultCityDialogScreen.kt
+++ app/src/main/java/ru/livetyping/zarina/ui/screen/onboarding/defaultcity/DefaultCityDialogScreen.kt
@@ -1,8 +1,6 @@
 package ru.livetyping.zarina.ui.screen.onboarding.defaultcity
 
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -10,7 +8,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.Flow
@@ -24,7 +21,6 @@ import ru.livetyping.zarina.ui.common.tooling.preview.FontScalePreviews
 import ru.livetyping.zarina.ui.common.tooling.preview.ZarinaPreview
 import ru.livetyping.zarina.ui.screen.onboarding.defaultcity.DefaultCityScreenComponents.bodyText
 import ru.livetyping.zarina.ui.screen.onboarding.defaultcity.DefaultCityViewModel.SideEffect
-import ru.livetyping.zarina.ui.theme.UiKitTheme
 
 @Composable
 fun DefaultCityDialogScreen(
@@ -53,31 +49,22 @@ private fun ScreenContent(
         navigate = navigate,
     )
 
-    // TODO: [High] Migrate to Slot version
-    ZarinaDialogContainer {
-        Text(
-            text = stringResource(R.string.default_city_dialog_title),
-            style = UiKitTheme.typography.primary.bold,
-            color = UiKitTheme.colors.text.general.regular.default,
-        )
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Text(
-            text = bodyText(defaultCity),
-            style = UiKitTheme.typography.secondary.regular,
-            color = UiKitTheme.colors.text.general.regular.default,
-        )
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        ZarinaButton(
-            onClick = onCloseClicked,
-            modifier = Modifier.fillMaxWidth(),
-        ) {
-            Text(text = stringResource(R.string.close).uppercase())
-        }
-    }
+    ZarinaDialogContainer(
+        title = {
+            Text(text = stringResource(R.string.default_city_dialog_title))
+        },
+        body = {
+            Text(text = bodyText(defaultCity))
+        },
+        buttons = {
+            ZarinaButton(
+                onClick = onCloseClicked,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Text(text = stringResource(R.string.close).uppercase())
+            }
+        },
+    )
 }
 
 @Preview
