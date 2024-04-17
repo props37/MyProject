28b753b8ac0d1c5ee8b1a58f6b459ee53081b58a
Artem Sidorov <sidorovaa@melonfashion.com>
2024-04-17 16:17:36 +0300

    Fix OrderApi::getOrders

--- app/src/main/java/ru/livetyping/zarina/data/order/remote/api/OrderApi.kt
+++ app/src/main/java/ru/livetyping/zarina/data/order/remote/api/OrderApi.kt
@@ -3,10 +3,9 @@ package ru.livetyping.zarina.data.order.remote.api
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 import ru.livetyping.zarina.data.order.remote.api.dto.GetOrdersDto
-import ru.livetyping.zarina.data.order.remote.api.dto.GetOrdersRequestBody
 import ru.livetyping.zarina.di.Qualifiers
-import ru.livetyping.zarina.util.library.ktor.setJsonBody
 import javax.inject.Inject
 
 class OrderApi @Inject constructor(
@@ -14,9 +13,8 @@ class OrderApi @Inject constructor(
     private val httpClient: HttpClient,
 ) {
     suspend fun getOrders(page: Int): GetOrdersDto {
-        val body = GetOrdersRequestBody(page)
         return httpClient.get("/api/v1/orders") {
-            setJsonBody(body)
+            parameter("page", page)
         }.body()
     }
 }
