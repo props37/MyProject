c6517af476bd79040bcb99b2acd3d23727a99763
Artem Sidorov <sidorovaa@melonfashion.com>
2024-04-27 16:02:29 +0300

    Perform forced logout if attempt to refresh authorization tokens has failed

--- app/src/main/java/ru/livetyping/zarina/usecase/authorization/RefreshAuthorizationTokensUseCase.kt
+++ app/src/main/java/ru/livetyping/zarina/usecase/authorization/RefreshAuthorizationTokensUseCase.kt
@@ -5,6 +5,8 @@ import kotlinx.coroutines.flow.firstOrNull
 import ru.livetyping.zarina.base.usecase.UseCase
 import ru.livetyping.zarina.data.authorization.AuthorizationRepository
 import ru.livetyping.zarina.di.Qualifiers
+import ru.livetyping.zarina.domain.authorization.AuthorizationTokens
+import ru.livetyping.zarina.usecase.user.ForcedSignOutUseCase
 import ru.livetyping.zarina.util.base.usecase.invoke
 import timber.log.Timber
 import javax.inject.Inject
@@ -16,17 +18,28 @@ class RefreshAuthorizationTokensUseCase @Inject constructor(
     dispatcher: CoroutineDispatcher,
     private val authorizationRepository: AuthorizationRepository,
     private val fetchUnauthorizedUserAuthorizationTokensUseCase: FetchUnauthorizedUserAuthorizationTokensUseCase,
+    private val forcedSignOutUseCase: ForcedSignOutUseCase,
 ) : UseCase<Unit, Unit>(dispatcher) {
 
     override suspend fun execute(params: Unit) {
         Timber.v("Refresh authorization tokens")
         val currentTokens = authorizationRepository.getAuthorizationTokensFlow().firstOrNull()
         if (currentTokens != null) {
+            refreshTokens(currentTokens)
+        } else {
+            fetchUnauthorizedUserAuthorizationTokensUseCase()
+        }
+    }
+
+    private suspend fun refreshTokens(currentTokens: AuthorizationTokens) {
+        try {
             val newTokens = authorizationRepository.refreshAuthorizationTokens(currentTokens)
             authorizationRepository.setAuthorizationTokens(newTokens)
             Timber.v("Authorization tokens refreshed. New tokens: $newTokens")
-        } else {
-            fetchUnauthorizedUserAuthorizationTokensUseCase()
+        } catch (e: Exception) {
+            Timber.e(e, "Failed to refresh authorization tokens. Perform forced sign out")
+            forcedSignOutUseCase().getOrThrow()
+            throw e
         }
     }
 }
