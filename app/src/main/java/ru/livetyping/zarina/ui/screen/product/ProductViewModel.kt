ac7d67d673e7f4c7b1339bfacb9ceea9ca1c12d6
Artem Sidorov <sidorovaa@melonfashion.com>
2024-05-02 17:06:07 +0300

    Retrieve product ID on ProductViewModel

--- app/src/main/java/ru/livetyping/zarina/ui/screen/product/ProductViewModel.kt
+++ app/src/main/java/ru/livetyping/zarina/ui/screen/product/ProductViewModel.kt
@@ -1,20 +1,41 @@
 package ru.livetyping.zarina.ui.screen.product
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import ru.livetyping.zarina.base.sideeffectsource.SideEffectSource
 import ru.livetyping.zarina.base.sideeffectsource.SideEffectSourceImpl
 import ru.livetyping.zarina.base.throttler.Throttler
+import ru.livetyping.zarina.domain.product.Product
 import ru.livetyping.zarina.ui.common.util.getNavigationThrottler
+import ru.livetyping.zarina.ui.navigation.destination.UnscopedDestinations
 import ru.livetyping.zarina.ui.screen.product.ProductViewModel.SideEffect
+import ru.livetyping.zarina.util.library.coroutines.mapState
 import javax.inject.Inject
 
 @HiltViewModel
 class ProductViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
     private val interactor: ProductInteractor,
 ) : ViewModel(), SideEffectSource<SideEffect> by SideEffectSourceImpl() {
 
     private val navigationThrottler = Throttler.getNavigationThrottler()
+
+    private val productId: StateFlow<Product.Id> = savedStateHandle
+        .getStateFlow<String?>(
+            key = UnscopedDestinations.Product.ARG_KEY_PRODUCT_ID,
+            initialValue = null,
+        )
+        .mapState(
+            scope = viewModelScope,
+            started = SharingStarted.Eagerly,
+        ) { value ->
+            checkNotNull(value) { "productId is null" }
+            Product.Id(value)
+        }
 
     fun onBackClicked() {
         navigationThrottler.throttle {
