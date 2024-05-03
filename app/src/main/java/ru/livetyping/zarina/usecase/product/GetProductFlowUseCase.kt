14caba835ab5383ada3fb6281dc4af570b0065f4
Artem Sidorov <sidorovaa@melonfashion.com>
2024-05-03 14:00:37 +0300

    Introduce GetProductFlowUseCase

---
+++ app/src/main/java/ru/livetyping/zarina/usecase/product/GetProductFlowUseCase.kt
@@ -0,0 +1,38 @@
+package ru.livetyping.zarina.usecase.product
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import ru.livetyping.zarina.base.usecase.FlowUseCase
+import ru.livetyping.zarina.data.cart.CartRepository
+import ru.livetyping.zarina.data.favorite.FavoriteRepository
+import ru.livetyping.zarina.data.product.ProductRepository
+import ru.livetyping.zarina.di.Qualifiers
+import ru.livetyping.zarina.domain.product.Product
+import ru.livetyping.zarina.domain.product.ProductDetails
+import javax.inject.Inject
+
+class GetProductFlowUseCase @Inject constructor(
+    @Qualifiers.CoroutineDispatcher(Qualifiers.CoroutineDispatchers.IO)
+    dispatcher: CoroutineDispatcher,
+    private val productRepository: ProductRepository,
+    private val cartRepository: CartRepository,
+    private val favoriteRepository: FavoriteRepository,
+) : FlowUseCase<GetProductFlowUseCase.Params, ProductDetails>(dispatcher) {
+
+    override fun execute(params: Params): Flow<ProductDetails> {
+        val productFlow = productRepository.getProductFlow(params.productId)
+        return combine(
+            productFlow,
+            cartRepository.cartProductIds,
+            favoriteRepository.favoriteProductIds
+        ) { product, cartProductIds, favoriteProductIds ->
+            product.copy(
+                isInFavorites = product.id in favoriteProductIds,
+                isInCart = product.id in cartProductIds,
+            )
+        }
+    }
+
+    data class Params(val productId: Product.Id)
+}
