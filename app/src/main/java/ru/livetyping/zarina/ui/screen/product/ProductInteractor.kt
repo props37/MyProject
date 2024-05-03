79b9c2392d7a5f139e0e0c1e116767b62eb6d70d
Artem Sidorov <sidorovaa@melonfashion.com>
2024-05-03 14:12:54 +0300

    Add productState property on ProductViewModel

--- app/src/main/java/ru/livetyping/zarina/ui/screen/product/ProductInteractor.kt
+++ app/src/main/java/ru/livetyping/zarina/ui/screen/product/ProductInteractor.kt
@@ -1,5 +1,8 @@
 package ru.livetyping.zarina.ui.screen.product
 
+import ru.livetyping.zarina.usecase.product.GetProductFlowUseCase
 import javax.inject.Inject
 
-class ProductInteractor @Inject constructor()
+class ProductInteractor @Inject constructor(
+    val getProductFlow: GetProductFlowUseCase,
+)
--- app/src/main/java/ru/livetyping/zarina/ui/screen/product/ProductViewModel.kt
+++ app/src/main/java/ru/livetyping/zarina/ui/screen/product/ProductViewModel.kt
@@ -1,18 +1,26 @@
 package ru.livetyping.zarina.ui.screen.product
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 import ru.livetyping.zarina.base.sideeffectsource.SideEffectSource
 import ru.livetyping.zarina.base.sideeffectsource.SideEffectSourceImpl
 import ru.livetyping.zarina.base.throttler.Throttler
 import ru.livetyping.zarina.domain.product.Product
+import ru.livetyping.zarina.domain.product.ProductDetails
+import ru.livetyping.zarina.ui.common.error.ErrorState
+import ru.livetyping.zarina.ui.common.error.from
 import ru.livetyping.zarina.ui.common.util.getNavigationThrottler
 import ru.livetyping.zarina.ui.navigation.destination.UnscopedDestinations
 import ru.livetyping.zarina.ui.screen.product.ProductViewModel.SideEffect
+import ru.livetyping.zarina.usecase.product.GetProductFlowUseCase
+import ru.livetyping.zarina.util.library.coroutines.WhileUiSubscribed
 import ru.livetyping.zarina.util.library.coroutines.mapState
 import javax.inject.Inject
 
@@ -37,6 +45,30 @@ class ProductViewModel @Inject constructor(
             Product.Id(value)
         }
 
+    private val productResult: StateFlow<Result<ProductDetails>?> =
+        interactor.getProductFlow(GetProductFlowUseCase.Params(productId.value))
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(),
+                initialValue = null,
+            )
+
+    val productState: StateFlow<ProductState> = productResult
+        .mapState(
+            scope = viewModelScope,
+            started = SharingStarted.WhileUiSubscribed,
+        ) { result ->
+            result?.fold(
+                onSuccess = { product ->
+                    ProductState.Success(product)
+                },
+                onFailure = {
+                    val state = ErrorState.from(it)
+                    ProductState.Error(state)
+                },
+            ) ?: ProductState.Loading
+        }
+
     fun onBackClicked() {
         navigationThrottler.throttle {
             val action = ProductScreenAction.ScreenClosed
@@ -46,5 +78,16 @@ class ProductViewModel @Inject constructor(
 
     sealed interface SideEffect : SideEffectSource.SideEffect {
         data class Navigate(val action: ProductScreenAction) : SideEffect
+    }
+
+    @Stable
+    sealed class ProductState {
+        @Immutable
+        data class Success(val product: ProductDetails) : ProductState()
+
+        data object Loading : ProductState()
+
+        @Immutable
+        data class Error(val state: ErrorState) : ProductState()
     }
 }
