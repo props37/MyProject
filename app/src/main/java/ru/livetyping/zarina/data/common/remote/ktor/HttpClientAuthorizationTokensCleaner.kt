e066b7d5447b76730083340945b20ab30cdcb3a5
Artem Sidorov <sidorovaa@melonfashion.com>
2024-05-14 18:44:07 +0300

    Return back to Auth Ktor plugin from custom ZarinaAuth
    There are no need to refresh auth tokens as a result of receiving 403 errors

--- app/src/main/java/ru/livetyping/zarina/data/common/remote/ktor/HttpClientAuthorizationTokensCleaner.kt
+++ app/src/main/java/ru/livetyping/zarina/data/common/remote/ktor/HttpClientAuthorizationTokensCleaner.kt
@@ -4,7 +4,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.plugins.auth.Auth
 import io.ktor.client.plugins.auth.providers.BearerAuthProvider
 import io.ktor.client.plugins.pluginOrNull
-import ru.livetyping.zarina.data.common.remote.ktor.plugin.ZarinaAuth
 import ru.livetyping.zarina.di.Qualifiers
 import timber.log.Timber
 import javax.inject.Inject
@@ -21,11 +20,6 @@ class HttpClientAuthorizationTokensCleaner @Inject constructor(
 
 private fun HttpClient.clearBearerTokens() {
     this.pluginOrNull(Auth)
-        ?.providers
-        ?.filterIsInstance<BearerAuthProvider>()
-        ?.firstOrNull()
-        ?.clearToken()
-    this.pluginOrNull(ZarinaAuth)
         ?.providers
         ?.filterIsInstance<BearerAuthProvider>()
         ?.firstOrNull()
