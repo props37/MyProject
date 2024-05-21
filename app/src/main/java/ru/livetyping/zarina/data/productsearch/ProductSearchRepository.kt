75477b2823efbc56a3c662d7f2524bd2d07531d2
Artem Sidorov <sidorovaa@melonfashion.com>
2024-05-21 16:16:48 +0300

    Introduce GetProductSearchSuggestionsFlowUseCase

--- app/src/main/java/ru/livetyping/zarina/data/productsearch/ProductSearchRepository.kt
+++ app/src/main/java/ru/livetyping/zarina/data/productsearch/ProductSearchRepository.kt
@@ -1,5 +1,6 @@
 package ru.livetyping.zarina.data.productsearch
 
+import kotlinx.coroutines.flow.Flow
 import ru.livetyping.zarina.data.productsearch.remote.ProductSearchRemoteDataSource
 import ru.livetyping.zarina.domain.productsearch.ProductSearchSuggestions
 import javax.inject.Inject
@@ -7,7 +8,7 @@ import javax.inject.Inject
 class ProductSearchRepository @Inject constructor(
     private val remoteDataSource: ProductSearchRemoteDataSource,
 ) {
-    suspend fun getSearchSuggestions(query: String): ProductSearchSuggestions {
-        return remoteDataSource.getSearchSuggestions(query)
+    fun getSearchSuggestionsFlow(query: String): Flow<ProductSearchSuggestions> {
+        return remoteDataSource.getSearchSuggestionsFlow(query)
     }
 }
--- app/src/main/java/ru/livetyping/zarina/data/productsearch/remote/ProductSearchRemoteDataSource.kt
+++ app/src/main/java/ru/livetyping/zarina/data/productsearch/remote/ProductSearchRemoteDataSource.kt
@@ -1,5 +1,7 @@
 package ru.livetyping.zarina.data.productsearch.remote
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import ru.livetyping.zarina.data.productsearch.remote.api.ProductSearchApi
 import ru.livetyping.zarina.domain.productsearch.ProductSearchSuggestions
 import javax.inject.Inject
@@ -7,7 +9,8 @@ import javax.inject.Inject
 class ProductSearchRemoteDataSource @Inject constructor(
     private val api: ProductSearchApi,
 ) {
-    suspend fun getSearchSuggestions(query: String): ProductSearchSuggestions {
-        return api.getSearchSuggestions(query).toProductSearchSuggestions()
+    fun getSearchSuggestionsFlow(query: String): Flow<ProductSearchSuggestions> = flow {
+        val suggestions = api.getSearchSuggestions(query).toProductSearchSuggestions()
+        emit(suggestions)
     }
 }
