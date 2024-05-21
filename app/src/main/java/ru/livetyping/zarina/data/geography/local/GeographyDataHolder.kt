3b930c4df00499cc6dfe55bb845cb4b3526ad7a9
Artem Sidorov <sidorovaa@melonfashion.com>
2024-05-21 15:40:19 +0300

    Renaming

--- app/src/main/java/ru/livetyping/zarina/data/geography/local/GeographyDataHolder.kt
+++ app/src/main/java/ru/livetyping/zarina/data/geography/local/GeographyDataHolder.kt
@@ -11,27 +11,27 @@ import javax.inject.Singleton
 
 @Singleton
 class GeographyDataHolder @Inject constructor() {
-    private val citiesToNameQuery = MutableStateFlow(mapOf<String?, List<City>>())
+    private val nameQueryToCities = MutableStateFlow(mapOf<String?, List<City>>())
 
     fun setCities(nameQuery: String?, cities: List<City>) {
         Timber.v("Set cities for name query $nameQuery: $cities")
-        citiesToNameQuery.update { it + (nameQuery to cities) }
-        ensureCitiesToNameQueryCacheSize()
+        nameQueryToCities.update { it + (nameQuery to cities) }
+        ensureCityCacheSize()
     }
 
     fun getCitiesFlow(nameQuery: String?): Flow<List<City>?> {
-        return citiesToNameQuery.map { map ->
+        return nameQueryToCities.map { map ->
             map[nameQuery]
         }
     }
 
-    private fun ensureCitiesToNameQueryCacheSize() {
-        val cachedNameQueries = citiesToNameQuery.value.keys
+    private fun ensureCityCacheSize() {
+        val cachedNameQueries = nameQueryToCities.value.keys
         if (cachedNameQueries.size > MAX_CACHED_CITY_NAME_QUERIES) {
             val nameQueryToRemove = cachedNameQueries.firstOrNull { it != null }
             if (nameQueryToRemove != null) {
                 Timber.v("Clear cached cities for name query $nameQueryToRemove")
-                citiesToNameQuery.update { it - nameQueryToRemove }
+                nameQueryToCities.update { it - nameQueryToRemove }
             }
         }
     }
