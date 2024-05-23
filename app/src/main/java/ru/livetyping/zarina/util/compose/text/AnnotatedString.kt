4e06fbe8366aa9d53a6eaad74353c67721f00a33
Artem Sidorov <sidorovaa@melonfashion.com>
2024-05-23 15:41:34 +0300

    Remove unused code

--- app/src/main/java/ru/livetyping/zarina/util/compose/text/AnnotatedString.kt
+++ app/src/main/java/ru/livetyping/zarina/util/compose/text/AnnotatedString.kt
@@ -3,13 +3,10 @@ package ru.livetyping.zarina.util.compose.text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.UrlAnnotation
 import androidx.compose.ui.text.buildAnnotatedString
 import ru.livetyping.zarina.util.kotlin.findSubstringBounds
-import timber.log.Timber
 
 @Composable
 fun rememberStringWithLinks(
@@ -67,59 +64,5 @@ fun rememberStringWithLinks(
                 }
             }
         }
-    }
-}
-
-fun AnnotatedString.Builder.addStyles(
-    substringToStyles: Map<String, List<SpanStyle>>,
-) {
-    substringToStyles.forEach { (substring, styles) ->
-        val annotatedString = this.toAnnotatedString()
-        val substringBounds = annotatedString.findSubstringBounds(substring)
-        if (substringBounds != null) {
-            styles.forEach { style ->
-                addStyle(style, substringBounds.first, substringBounds.last)
-            }
-        } else {
-            Timber.e("Could not add styles because substring \"$substring\" is not found in string \"$annotatedString\"")
-        }
-    }
-}
-
-@OptIn(ExperimentalTextApi::class)
-fun AnnotatedString.Builder.addUrlAnnotations(
-    substringToStyles: Map<String, List<UrlAnnotation>>,
-) {
-    substringToStyles.forEach { (substring, annotations) ->
-        val annotatedString = this.toAnnotatedString()
-        val substringBounds = annotatedString.findSubstringBounds(substring)
-        if (substringBounds != null) {
-            annotations.forEach { annotation ->
-                addUrlAnnotation(annotation, substringBounds.first, substringBounds.last)
-            }
-        } else {
-            Timber.e("Could not add style because substring \"$substring\" is not found in string \"$annotatedString\"")
-        }
-    }
-}
-
-fun AnnotatedString.Builder.addStyle(substring: String, style: SpanStyle) {
-    val annotatedString = this.toAnnotatedString()
-    val substringBounds = annotatedString.findSubstringBounds(substring)
-    if (substringBounds != null) {
-        addStyle(style, substringBounds.first, substringBounds.last)
-    } else {
-        Timber.e("Could not add style because substring $substring is not found in $this")
-    }
-}
-
-@OptIn(ExperimentalTextApi::class)
-fun AnnotatedString.Builder.addUrlAnnotation(substring: String, annotation: UrlAnnotation) {
-    val annotatedString = this.toAnnotatedString()
-    val substringBounds = annotatedString.findSubstringBounds(substring)
-    if (substringBounds != null) {
-        addUrlAnnotation(annotation, substringBounds.first, substringBounds.last)
-    } else {
-        Timber.e("Could not add URL annotation because substring \"$substring\" is not found in string \"$annotatedString\"")
     }
 }
