833c82924e1513ea2b2af6cd6cae48261eeb85d3
Artem Sidorov <sidorovaa@melonfashion.com>
2024-06-05 15:04:44 +0300

    Set ZarinaDatabase version to 1

--- app/src/main/java/ru/livetyping/zarina/data/database/ZarinaDatabase.kt
+++ app/src/main/java/ru/livetyping/zarina/data/database/ZarinaDatabase.kt
@@ -1,6 +1,5 @@
 package ru.livetyping.zarina.data.database
 
-import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import ru.livetyping.zarina.data.productsearch.local.database.dao.ProductSearchHistoryQueryDao
@@ -9,11 +8,8 @@ import ru.livetyping.zarina.data.user.local.database.dao.UserDao
 import ru.livetyping.zarina.data.user.local.database.entity.UserEntity
 
 @Database(
-    version = 2,
+    version = 1,
     entities = [UserEntity::class, ProductSearchHistoryQueryEntity::class],
-    autoMigrations = [
-        AutoMigration(from = 1, to = 2),
-    ],
 )
 abstract class ZarinaDatabase : RoomDatabase() {
     abstract fun getUserDao(): UserDao
