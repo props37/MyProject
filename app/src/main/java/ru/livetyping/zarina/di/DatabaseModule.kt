d65b71a5ed2b588b7c5f1c5d1c7179e0951a04be
Artem Sidorov <sidorovaa@melonfashion.com>
2024-06-06 12:55:19 +0300

    Perform forced logout if the database had destructive migration

--- app/src/main/java/ru/livetyping/zarina/di/DatabaseModule.kt
+++ app/src/main/java/ru/livetyping/zarina/di/DatabaseModule.kt
@@ -2,14 +2,22 @@ package ru.livetyping.zarina.di
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import dagger.Lazy
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import ru.livetyping.zarina.data.database.ZarinaDatabase
 import ru.livetyping.zarina.data.productsearch.local.database.dao.ProductSearchHistoryQueryDao
 import ru.livetyping.zarina.data.user.local.database.dao.UserDao
+import ru.livetyping.zarina.usecase.user.ForcedSignOutUseCase
+import ru.livetyping.zarina.util.base.usecase.invoke
+import timber.log.Timber
 import javax.inject.Singleton
 
 @Module
@@ -21,9 +29,19 @@ class DatabaseModule {
     fun provideZarinaDatabase(
         @ApplicationContext
         context: Context,
+        coroutineScope: CoroutineScope,
+        forcedSignOutUseCase: Lazy<ForcedSignOutUseCase>,
     ): ZarinaDatabase {
         return Room.databaseBuilder(context, ZarinaDatabase::class.java, DATABASE_NAME)
             .fallbackToDestructiveMigration()
+            .addCallback(object : RoomDatabase.Callback() {
+                override fun onDestructiveMigration(db: SupportSQLiteDatabase) {
+                    Timber.tag(DATABASE_NAME).w("onDestructiveMigration")
+                    coroutineScope.launch {
+                        forcedSignOutUseCase.get().invoke()
+                    }
+                }
+            })
             .build()
     }
 
