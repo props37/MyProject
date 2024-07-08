d56593a65400251df07de1e35fb2a7d7719b43fe
Artem Sidorov <sidorovaa@melonfashion.com>
2024-07-08 12:02:30 +0300

    Revert "Migrate to Kotlin 2.0"
    This reverts commit c99fa9c4

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.test) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.ksp) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
-    alias(libs.plugins.kotlin.compose.compiler) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.googlePlayServices) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
