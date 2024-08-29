9893fa849da206afc49b92b85166f9d20b1682cd
Artem Sidorov <sidorovaa@melonfashion.com>
2024-08-29 15:47:28 +0100

    Add GetPostDeliveryOptionsFlowUseCase

--- app/src/main/java/ru/livetyping/zarina/presentation/screen/checkout/postdelivery/CheckoutPostDeliveryInteractor.kt
+++ app/src/main/java/ru/livetyping/zarina/presentation/screen/checkout/postdelivery/CheckoutPostDeliveryInteractor.kt
@@ -1,8 +1,10 @@
 package ru.livetyping.zarina.presentation.screen.checkout.postdelivery
 
+import ru.livetyping.zarina.usecase.checkout.GetPostDeliveryOptionsFlowUseCase
 import ru.livetyping.zarina.usecase.user.GetUserCityFlowUseCase
 import javax.inject.Inject
 
 class CheckoutPostDeliveryInteractor @Inject constructor(
     val getUserCityFlow: GetUserCityFlowUseCase,
+    val getPostDeliveryOptionsFlow: GetPostDeliveryOptionsFlowUseCase,
 )
