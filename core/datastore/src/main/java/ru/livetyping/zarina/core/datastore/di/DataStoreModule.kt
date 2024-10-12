2ca60d4392d140609429e4213ffd64475fad145c
Artem Sidorov <sidorovaa@melonfashion.com>
2024-10-12 10:06:19 +0100

    Add DataStoreModule

---
+++ core/datastore/src/main/java/ru/livetyping/zarina/core/datastore/di/DataStoreModule.kt
@@ -0,0 +1,31 @@
+package ru.livetyping.zarina.core.datastore.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal class DataStoreModule {
+    private val Context.preferencesDataStore by preferencesDataStore(PREFERENCES_DATA_STORE_NAME)
+
+    @Provides
+    @Singleton
+    fun providePreferencesDataStore(
+        @ApplicationContext
+        context: Context,
+    ): DataStore<Preferences> {
+        return context.preferencesDataStore
+    }
+
+    private companion object {
+        private const val PREFERENCES_DATA_STORE_NAME = "preferences_data_store"
+    }
+}
