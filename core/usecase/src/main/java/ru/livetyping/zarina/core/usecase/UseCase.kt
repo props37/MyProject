2730cfd968e80678b4ca8197ab008fa879b22a7f
Artem Sidorov <sidorovaa@melonfashion.com>
2024-10-13 17:03:23 +0100

    Remove execution time tracking from base UseCase

--- core/usecase/src/main/java/ru/livetyping/zarina/core/usecase/UseCase.kt
+++ core/usecase/src/main/java/ru/livetyping/zarina/core/usecase/UseCase.kt
@@ -1,19 +1,13 @@
 package ru.livetyping.zarina.core.usecase
 
 import kotlin.coroutines.cancellation.CancellationException
-import kotlin.system.measureTimeMillis
-import kotlin.time.Duration.Companion.milliseconds
 
 public abstract class UseCase<in P, out R>(private val logger: UseCaseLogger?) {
     private val className by lazy { this.javaClass.simpleName ?: TAG }
 
     public suspend operator fun invoke(params: P): Result<R> {
         return try {
-            val executionResult: R
-            val executionDuration = measureTimeMillis {
-                executionResult = execute(params)
-            }.milliseconds
-            logger?.v(className, "Execution of $className took $executionDuration")
+            val executionResult = execute(params)
             Result.success(executionResult)
         } catch (e: CancellationException) {
             throw e
