50869adbc5db76d700b70cea3ae1de89dd1f488b
Artem Sidorov <sidorovaa@melonfashion.com>
2024-10-13 09:38:51 +0100

    Add HomeContentRepository::getOnboardingBannerUrl method

--- feature/home/data/src/main/java/ru/livetyping/zarina/feature/home/data/impl/remote/HomeContentRemoteDataSourceImpl.kt
+++ feature/home/data/src/main/java/ru/livetyping/zarina/feature/home/data/impl/remote/HomeContentRemoteDataSourceImpl.kt
@@ -2,6 +2,7 @@ package ru.livetyping.zarina.feature.home.data.impl.remote
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import ru.livetyping.zarina.core.domain.model.common.Url
 import ru.livetyping.zarina.feature.home.data.impl.remote.api.HomeContentApi
 import ru.livetyping.zarina.feature.home.domain.model.HomeContent
 import javax.inject.Inject
@@ -9,6 +10,10 @@ import javax.inject.Inject
 internal class HomeContentRemoteDataSourceImpl @Inject constructor(
     private val api: HomeContentApi,
 ) : HomeContentRemoteDataSource {
+    override fun getOnboardingBannerUrl(): Url {
+        return api.getOnboardingBannerUrl()
+    }
+
     override fun getHomeContentFlow(): Flow<HomeContent> = flow {
         val homeContent = api.getHomeContent().toHomeContent()
         emit(homeContent)
--- feature/home/data/src/main/java/ru/livetyping/zarina/feature/home/data/repository/HomeContentRepositoryImpl.kt
+++ feature/home/data/src/main/java/ru/livetyping/zarina/feature/home/data/repository/HomeContentRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package ru.livetyping.zarina.feature.home.data.repository
 
 import kotlinx.coroutines.flow.Flow
+import ru.livetyping.zarina.core.domain.model.common.Url
 import ru.livetyping.zarina.feature.home.data.impl.remote.HomeContentRemoteDataSource
 import ru.livetyping.zarina.feature.home.domain.model.HomeContent
 import ru.livetyping.zarina.feature.home.domain.repository.HomeContentRepository
@@ -9,6 +10,10 @@ import javax.inject.Inject
 internal class HomeContentRepositoryImpl @Inject constructor(
     private val remoteDataSource: HomeContentRemoteDataSource,
 ) : HomeContentRepository {
+    override fun getOnboardingBannerUrl(): Url {
+        return remoteDataSource.getOnboardingBannerUrl()
+    }
+
     override fun getHomeContentFlow(): Flow<HomeContent> {
         return remoteDataSource.getHomeContentFlow()
     }
