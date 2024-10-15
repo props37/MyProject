9d827b8f478ac6808f7fb571ea4e471df9227991
Artem Sidorov <sidorovaa@melonfashion.com>
2024-10-15 10:52:20 +0100

    Configure Compose compiler for :feature:home:ui:impl module

--- feature/home/ui/impl/build.gradle.kts
+++ feature/home/ui/impl/build.gradle.kts
@@ -44,7 +44,10 @@ kotlin {
     explicitApi()
 }
 
-// TODO: [High] Specify Compose reports destination and Compose stability config
+composeCompiler {
+    reportsDestination = layout.buildDirectory.dir("compose_compiler")
+    stabilityConfigurationFile = rootProject.layout.projectDirectory.file("config/compose/stability_config.txt")
+}
 
 dependencies {
     implementation(projects.feature.home.ui.api)
