9b1755621267fb4d4dac06448213bde558e2cda2
Artem Sidorov <sidorovaa@melonfashion.com>
2024-10-15 14:52:23 +0100

    Specify bottom nav bar behavior for Home screen

--- feature/home/ui/impl/src/main/java/ru/livetyping/zarina/feature/home/ui/impl/impl/HomeScreenBehavior.kt
+++ feature/home/ui/impl/src/main/java/ru/livetyping/zarina/feature/home/ui/impl/impl/HomeScreenBehavior.kt
@@ -1,9 +1,10 @@
 package ru.livetyping.zarina.feature.home.ui.impl.impl
 
 import androidx.compose.runtime.Composable
+import ru.livetyping.zarina.core.ui.kit.bottomnavbar.behavior.BottomNavBarBehavior
 
 // TODO: [Top] Implement
 @Composable
 internal fun HomeScreenBehavior() {
-
+    BottomNavBarBehavior(isVisible = true)
 }
