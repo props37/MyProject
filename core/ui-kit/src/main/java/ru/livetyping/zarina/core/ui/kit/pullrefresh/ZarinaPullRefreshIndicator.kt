73ae56038bedef0611dd2115930820a532ef407e
Artem Sidorov <sidorovaa@melonfashion.com>
2024-10-15 14:33:37 +0100

    Add ZarinaPullRefreshIndicator

---
+++ core/ui-kit/src/main/java/ru/livetyping/zarina/core/ui/kit/pullrefresh/ZarinaPullRefreshIndicator.kt
@@ -0,0 +1,39 @@
+package ru.livetyping.zarina.core.ui.kit.pullrefresh
+
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.PullRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import ru.livetyping.zarina.core.ui.kit.theme.UiKitTheme
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+public fun ZarinaPullRefreshIndicator(
+    isRefreshing: Boolean,
+    state: PullRefreshState,
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = ZarinaPullRefreshIndicatorDefaults.BackgroundColor,
+    contentColor: Color = ZarinaPullRefreshIndicatorDefaults.ContentColor,
+    scale: Boolean = false,
+) {
+    PullRefreshIndicator(
+        refreshing = isRefreshing,
+        state = state,
+        modifier = modifier,
+        backgroundColor = backgroundColor,
+        contentColor = contentColor,
+        scale = scale,
+    )
+}
+
+public object ZarinaPullRefreshIndicatorDefaults {
+    internal val BackgroundColor: Color
+        @Composable
+        get() = UiKitTheme.colors.background.general.regular.default
+
+    internal val ContentColor: Color
+        @Composable
+        get() = UiKitTheme.colors.icon.regular.default
+}
