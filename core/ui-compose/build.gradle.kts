3c6d9b3089d021c36dce1a2bb4b89e06a0e6a13f
Artem Sidorov <sidorovaa@melonfashion.com>
2024-10-23 16:42:41 +0100

    Enable Compose in :core:ui-compose module

--- core/ui-compose/build.gradle.kts
+++ core/ui-compose/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose.compiler)
 }
 
 android {
@@ -31,6 +32,10 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
+    }
+
+    buildFeatures {
+        compose = true
     }
 }
 
