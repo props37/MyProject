19fc781f977d007347142b1007763924b1b16395
Artem Sidorov <sidorovaa@melonfashion.com>
2024-10-23 17:26:22 +0100

    Add ZarinaBouncingIconButton

---
+++ core/ui-kit/src/main/java/ru/livetyping/zarina/core/uikit/button/ZarinaBouncingIconButton.kt
@@ -0,0 +1,98 @@
+package ru.livetyping.zarina.core.uikit.button
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.ripple
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+@Composable
+public fun ZarinaBouncingIconButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    isEnabled: Boolean = true,
+    isLoading: Boolean = false,
+    loaderSize: Dp = ZarinaIconButtonDefaults.LoaderSize,
+    loaderColor: Color = ZarinaIconButtonDefaults.LoaderColor,
+    isBouncingEnabled: Boolean = true,
+    pressedScale: Float = ZarinaBouncingIconButtonDefaults.PressedScale,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    indication: Indication? = ripple(bounded = false, radius = 24.dp),
+    content: @Composable () -> Unit,
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scaleAnimatable = remember { Animatable(1f) }
+    val scaleAnimationSpec = remember {
+        spring(
+            dampingRatio = Spring.DampingRatioHighBouncy,
+            stiffness = Spring.StiffnessMediumLow,
+            visibilityThreshold = ZarinaBouncingIconButtonDefaults.VisibilityThreshold,
+        )
+    }
+    val isScaleAnimationInterruptible = remember { mutableStateOf(true) }
+
+    ZarinaIconButton(
+        onClick = {
+            onClick()
+            coroutineScope.launch {
+                isScaleAnimationInterruptible.value = false
+                scaleAnimatable.animateTo(pressedScale, scaleAnimationSpec)
+                isScaleAnimationInterruptible.value = true
+                scaleAnimatable.animateTo(1f, scaleAnimationSpec)
+            }
+        },
+        isEnabled = isEnabled,
+        isLoading = isLoading,
+        loaderSize = loaderSize,
+        loaderColor = loaderColor,
+        interactionSource = interactionSource,
+        indication = indication,
+        modifier = modifier,
+    ) {
+        val isPressed = interactionSource.collectIsPressedAsState()
+        LaunchedEffect(scaleAnimatable) {
+            snapshotFlow { isPressed.value }.collectLatest { isPressed ->
+                if (isScaleAnimationInterruptible.value) {
+                    val targetValue = if (isPressed) pressedScale else 1f
+                    scaleAnimatable.animateTo(
+                        targetValue = targetValue,
+                        animationSpec = scaleAnimationSpec,
+                    )
+                }
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .graphicsLayer {
+                    if (isBouncingEnabled) {
+                        scaleX = scaleAnimatable.value
+                        scaleY = scaleAnimatable.value
+                    }
+                },
+        ) {
+            content()
+        }
+    }
+}
+
+public object ZarinaBouncingIconButtonDefaults {
+    public const val PressedScale: Float = 0.8f
+    public const val VisibilityThreshold: Float = 0.01f
+}
