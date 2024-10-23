66a7f0af5eccfd7e4b1f6880f1d0d91adc12d975
Artem Sidorov <sidorovaa@melonfashion.com>
2024-10-23 17:28:57 +0100

    Add ZarinaCloseIconButton

---
+++ core/ui-kit/src/main/java/ru/livetyping/zarina/core/uikit/button/ZarinaCloseIconButton.kt
@@ -0,0 +1,46 @@
+package ru.livetyping.zarina.core.uikit.button
+
+import androidx.compose.foundation.Indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.ripple
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.Dp
+import ru.livetyping.zarina.core.resource.R
+
+@Composable
+public fun ZarinaCloseIconButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    contentDescription: String = stringResource(R.string.close),
+    isEnabled: Boolean = true,
+    isLoading: Boolean = false,
+    iconSize: Dp = ZarinaIconButtonDefaults.IconSize,
+    tint: Color = ZarinaIconButtonDefaults.IconColor,
+    interactionSource: MutableInteractionSource? = null,
+    indication: Indication = ripple(bounded = false, radius = iconSize),
+) {
+    ZarinaIconButton(
+        onClick = onClick,
+        isEnabled = isEnabled,
+        isLoading = isLoading,
+        loaderSize = iconSize,
+        loaderColor = tint,
+        interactionSource = interactionSource,
+        indication = indication,
+        modifier = modifier,
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_cross_24),
+            contentDescription = contentDescription,
+            tint = tint,
+            modifier = Modifier.size(iconSize),
+        )
+    }
+}
