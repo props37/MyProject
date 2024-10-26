4449bd32f6110eb5bee18a5ebe15062ad451c939
Artem Sidorov <sidorovaa@melonfashion.com>
2024-10-26 11:07:18 +0100

    Refactor TabRowState::currentTabIndex property

--- core/ui-model/src/main/java/ru/livetyping/zarina/core/uimodel/tab/TabRowState.kt
+++ core/ui-model/src/main/java/ru/livetyping/zarina/core/uimodel/tab/TabRowState.kt
@@ -6,6 +6,9 @@ public data class TabRowState<T>(
     val tabs: ImmutableList<T>,
     val currentTab: T,
 ) {
-    public val currentTabIndex: Int
-        get() = tabs.indexOf(currentTab)
+    public val currentTabIndex: Int by lazy {
+        val index = tabs.indexOf(currentTab)
+        check(index >= 0) { "There is no current tab $currentTab in tabs $tabs" }
+        index
+    }
 }
