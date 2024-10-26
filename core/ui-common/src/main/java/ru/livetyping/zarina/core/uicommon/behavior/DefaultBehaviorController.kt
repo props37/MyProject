f6d25e7feac5f5dec61132b7f477207190b2f0fb
Artem Sidorov <sidorovaa@melonfashion.com>
2024-10-26 14:49:19 +0100

    Use ConcurrentLinkedDeque instead of synchronization on DefaultBehaviorController

--- core/ui-common/src/main/java/ru/livetyping/zarina/core/uicommon/behavior/DefaultBehaviorController.kt
+++ core/ui-common/src/main/java/ru/livetyping/zarina/core/uicommon/behavior/DefaultBehaviorController.kt
@@ -3,13 +3,12 @@ package ru.livetyping.zarina.core.uicommon.behavior
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import java.util.concurrent.ConcurrentLinkedDeque
 
 public class DefaultBehaviorController<T : Behavior>(
     private var defaultBehavior: T,
 ) : BehaviorController<T> {
-    // TODO: [High] Migrate to concurrent collection
-    private val behaviorStack = ArrayDeque<T>()
-    private val behaviorStackLock = Any()
+    private val behaviorDeque = ConcurrentLinkedDeque<T>()
 
     private val _currentBehavior = MutableStateFlow(defaultBehavior)
     override val currentBehavior: StateFlow<T> = _currentBehavior.asStateFlow()
@@ -20,20 +19,16 @@ public class DefaultBehaviorController<T : Behavior>(
     }
 
     override fun push(behavior: T) {
-        synchronized(behaviorStackLock) {
-            behaviorStack.addFirst(behavior)
-        }
+        behaviorDeque.addFirst(behavior)
         updateCurrentBehavior()
     }
 
     override fun pop(behavior: T) {
-        synchronized(behaviorStackLock) {
-            behaviorStack.remove(behavior)
-        }
+        behaviorDeque.remove(behavior)
         updateCurrentBehavior()
     }
 
     private fun updateCurrentBehavior() {
-        _currentBehavior.value = behaviorStack.firstOrNull() ?: defaultBehavior
+        _currentBehavior.value = behaviorDeque.peekFirst() ?: defaultBehavior
     }
 }
