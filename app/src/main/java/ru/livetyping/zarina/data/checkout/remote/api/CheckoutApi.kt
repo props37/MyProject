c3fca00c558e971e2f407087481a5caf5f90decb
Artem Sidorov <sidorovaa@melonfashion.com>
2024-11-05 14:50:43 +0000

    Fix CheckoutApi::removeGiftCertificate method

--- app/src/main/java/ru/livetyping/zarina/data/checkout/remote/api/CheckoutApi.kt
+++ app/src/main/java/ru/livetyping/zarina/data/checkout/remote/api/CheckoutApi.kt
@@ -2,6 +2,7 @@ package ru.livetyping.zarina.data.checkout.remote.api
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import io.ktor.client.request.post
@@ -187,7 +188,7 @@ class CheckoutApi @Inject constructor(
         val body = RemoveGiftCertificateRequestBody(
             paymentMethodType = PaymentMethodTypeDto.from(paymentMethodType),
         )
-        httpClient.post("/api/gift-card/cancel") {
+        httpClient.delete("/api/gift-card/cancel") {
             setJsonBody(body)
         }
     }
