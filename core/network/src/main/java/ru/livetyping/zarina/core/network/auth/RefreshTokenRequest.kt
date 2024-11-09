7c933f378103b1805e7ceef6cab6f0ab6aca3177
Artem Sidorov <sidorovaa@melonfashion.com>
2024-11-09 14:15:49 +0000

    Move code

--- core/network/src/main/java/ru/livetyping/zarina/core/network/auth/RefreshTokenRequest.kt
+++
@@ -1,8 +0,0 @@
-package ru.livetyping.zarina.core.network.auth
-
-import io.ktor.client.plugins.auth.AuthCircuitBreaker
-import io.ktor.client.request.HttpRequestBuilder
-
-public fun HttpRequestBuilder.markAsRefreshTokenRequest() {
-    attributes.put(AuthCircuitBreaker, Unit)
-}
--- core/network/src/main/java/ru/livetyping/zarina/core/network/util/HttpRequestBuilder.kt
+++ core/network/src/main/java/ru/livetyping/zarina/core/network/util/HttpRequestBuilder.kt
@@ -1,5 +1,6 @@
 package ru.livetyping.zarina.core.network.util
 
+import io.ktor.client.plugins.auth.AuthCircuitBreaker
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
@@ -8,4 +9,8 @@ import io.ktor.http.contentType
 public inline fun <reified T> HttpRequestBuilder.setJsonBody(body: T) {
     contentType(ContentType.Application.Json)
     setBody(body)
+}
+
+public fun HttpRequestBuilder.markAsRefreshTokenRequest() {
+    attributes.put(AuthCircuitBreaker, Unit)
 }
