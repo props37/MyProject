df1a525d5a3136cbe37a42f9bcc4047e85ae3ca3
Artem Sidorov <sidorovaa@melonfashion.com>
2024-11-11 12:16:03 +0000

    Remove dispatcher from SignInByEmailUseCase

--- app/src/main/java/ru/livetyping/zarina/usecase/user/SignInByEmailUseCase.kt
+++ app/src/main/java/ru/livetyping/zarina/usecase/user/SignInByEmailUseCase.kt
@@ -1,21 +1,17 @@
 package ru.livetyping.zarina.usecase.user
 
-import kotlinx.coroutines.CoroutineDispatcher
 import ru.livetyping.zarina.base.usecase.UseCase
 import ru.livetyping.zarina.data.user.UserRepository
-import ru.livetyping.zarina.di.Qualifiers
 import ru.livetyping.zarina.domain.captcha.YandexCaptchaToken
 import ru.livetyping.zarina.domain.common.Email
 import timber.log.Timber
 import javax.inject.Inject
 
 class SignInByEmailUseCase @Inject constructor(
-    @Qualifiers.CoroutineDispatcher(Qualifiers.CoroutineDispatchers.IO)
-    dispatcher: CoroutineDispatcher,
     private val userRepository: UserRepository,
     private val validateFieldsUseCase: ValidateSignInByEmailFieldsUseCase,
     private val setUserWithAuthorizationTokensUseCase: SetUserWithAuthorizationTokensUseCase,
-) : UseCase<SignInByEmailUseCase.Params, Unit>(dispatcher) {
+) : UseCase<SignInByEmailUseCase.Params, Unit>() {
 
     override suspend fun execute(params: Params) {
         val email = params.email
