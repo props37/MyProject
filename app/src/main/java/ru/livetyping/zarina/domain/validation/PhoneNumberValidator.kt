5a2a2ac032817b2f61675c0263456902e0b01f7f
Artem Sidorov <sidorovaa@melonfashion.com>
2024-11-11 12:48:42 +0000

    Improve PhoneNumberValidator

--- app/src/main/java/ru/livetyping/zarina/domain/validation/PhoneNumberValidator.kt
+++ app/src/main/java/ru/livetyping/zarina/domain/validation/PhoneNumberValidator.kt
@@ -4,12 +4,12 @@ import ru.livetyping.zarina.domain.common.PhoneNumber
 import ru.livetyping.zarina.domain.user.exception.EmptyPhoneNumberException
 import ru.livetyping.zarina.domain.user.exception.InvalidPhoneNumberException
 
-// TODO: [Medium] Improve
 class PhoneNumberValidator : Validator<PhoneNumber> {
     override fun validate(input: PhoneNumber) {
         val phoneString = input.value.trim()
         when {
             phoneString.isBlank() -> throw EmptyPhoneNumberException()
+            phoneString.length < PHONE_MIN_LENGTH -> throw InvalidPhoneNumberException()
             !phoneString.matches(PHONE_REGEX_PATTERN.toRegex()) -> {
                 throw InvalidPhoneNumberException()
             }
@@ -17,6 +17,8 @@ class PhoneNumberValidator : Validator<PhoneNumber> {
     }
 
     companion object {
+        private const val PHONE_MIN_LENGTH = 11
+
         // Source: android.telephony.PhoneNumberUtils.GLOBAL_PHONE_NUMBER_PATTERN
         private const val PHONE_REGEX_PATTERN = "[\\+]?[0-9.-]+"
     }
