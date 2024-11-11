20fbbd1734d29f25ea27b71d71e23807d26956bc
Artem Sidorov <sidorovaa@melonfashion.com>
2024-11-11 11:40:56 +0000

    Remove previous Zarina toast message when showing a new one

--- app/src/main/java/ru/livetyping/zarina/presentation/common/zarinatoast/controller/ZarinaToastControllerImpl.kt
+++ app/src/main/java/ru/livetyping/zarina/presentation/common/zarinatoast/controller/ZarinaToastControllerImpl.kt
@@ -12,7 +12,10 @@ class ZarinaToastControllerImpl(
 
     override val currentMessage: StateFlow<ZarinaToastMessage?> = messageQueue.currentMessage
 
-    override fun show(message: ZarinaToastMessage) {
+    override fun show(message: ZarinaToastMessage, removePreviousMessage: Boolean) {
+        if (removePreviousMessage) {
+            messageQueue.removeCurrentMessage()
+        }
         messageQueue.addMessage(message)
     }
 
