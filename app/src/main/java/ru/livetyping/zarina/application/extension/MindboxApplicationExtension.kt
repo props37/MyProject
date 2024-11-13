cbee72d4e5c0217c7523abce56da0cc0e1677cd8
Artem Sidorov <sidorovaa@melonfashion.com>
2024-11-13 12:54:24 +0000

    Configure Mindbox SDK

--- app/src/main/java/ru/livetyping/zarina/application/extension/MindboxApplicationExtension.kt
+++ app/src/main/java/ru/livetyping/zarina/application/extension/MindboxApplicationExtension.kt
@@ -1,16 +1,29 @@
 package ru.livetyping.zarina.application.extension
 
 import android.app.Application
+import cloud.mindbox.mindbox_firebase.MindboxFirebase
 import cloud.mindbox.mobile_sdk.Mindbox
 import cloud.mindbox.mobile_sdk.MindboxConfiguration
+import cloud.mindbox.mobile_sdk.logger.Level
 import ru.livetyping.zarina.BuildConfig
 import ru.livetyping.zarina.application.extension.base.ApplicationExtension
+import timber.log.Timber
 import javax.inject.Inject
 
 class MindboxApplicationExtension @Inject constructor() : ApplicationExtension {
     override fun install(application: Application) {
         val configuration = getConfiguration(application)
-        Mindbox.init(application, configuration, emptyList())
+        Mindbox.init(
+            application = application,
+            configuration = configuration,
+            pushServices = listOf(MindboxFirebase),
+        )
+        if (BuildConfig.IS_LOGGING_ENABLED) {
+            Mindbox.setLogLevel(Level.DEBUG)
+        }
+        Mindbox.subscribeDeviceUuid { uuid ->
+            Timber.tag(TAG).v("Mindbox device UUID: $uuid")
+        }
     }
 
     private fun getConfiguration(application: Application): MindboxConfiguration {
@@ -19,12 +32,14 @@ class MindboxApplicationExtension @Inject constructor() : ApplicationExtension {
             domain = DOMAIN,
             endpointId = BuildConfig.MINDBOX_ENDPOINT,
         )
-            .shouldCreateCustomer(false)
+            .shouldCreateCustomer(true)
             .subscribeCustomerIfCreated(true)
             .build()
     }
 
     companion object {
         const val DOMAIN = "api.mindbox.ru"
+
+        private const val TAG = "Mindbox"
     }
 }
