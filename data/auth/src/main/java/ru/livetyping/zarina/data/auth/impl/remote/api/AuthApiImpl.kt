f58270502328eabbb1e779257cf0372c80106345
Artem Sidorov <sidorovaa@melonfashion.com>
2024-11-15 13:28:30 +0000

    Optimize imports

--- data/auth/src/main/java/ru/livetyping/zarina/data/auth/impl/remote/api/AuthApiImpl.kt
+++ data/auth/src/main/java/ru/livetyping/zarina/data/auth/impl/remote/api/AuthApiImpl.kt
@@ -5,9 +5,9 @@ import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import ru.livetyping.zarina.core.domain.model.auth.BearerTokens
-import ru.livetyping.zarina.core.network.auth.markAsRefreshTokenRequest
-import ru.livetyping.zarina.core.network.di.ZarinaApiType
 import ru.livetyping.zarina.core.network.di.ZarinaApi
+import ru.livetyping.zarina.core.network.di.ZarinaApiType
+import ru.livetyping.zarina.core.network.util.markAsRefreshTokenRequest
 import ru.livetyping.zarina.data.auth.impl.remote.api.dto.BearerTokensDto
 import javax.inject.Inject
 
