fdd925f782cda5a066964a717574b2f01b92213f
Artem Sidorov <sidorovaa@melonfashion.com>
2024-11-16 14:25:42 +0000

    Add transition parameters to ComplexFeatureEntry::navigation

--- core/feature/src/main/java/ru/livetyping/zarina/core/feature/ComplexFeatureEntry.kt
+++ core/feature/src/main/java/ru/livetyping/zarina/core/feature/ComplexFeatureEntry.kt
@@ -1,5 +1,10 @@
 package ru.livetyping.zarina.core.feature
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.SizeTransform
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
 import ru.livetyping.zarina.core.navigation.NavigationActions
 import ru.livetyping.zarina.core.navigation.NavigationEntry
@@ -7,5 +12,12 @@ import ru.livetyping.zarina.core.navigation.NavigationEntry
 public interface ComplexFeatureEntry<NavEntry : NavigationEntry, NavActions : NavigationActions> :
     FeatureEntry<NavEntry, NavActions> {
 
-    public fun NavGraphBuilder.navigation(actions: NavActions)
+    public fun NavGraphBuilder.navigation(
+        actions: NavActions,
+        enterTransition: (AnimatedContentTransitionScope<NavBackStackEntry>.() -> @JvmSuppressWildcards EnterTransition?)? = null,
+        exitTransition: (AnimatedContentTransitionScope<NavBackStackEntry>.() -> @JvmSuppressWildcards ExitTransition?)? = null,
+        popEnterTransition: (AnimatedContentTransitionScope<NavBackStackEntry>.() -> @JvmSuppressWildcards EnterTransition?)? = enterTransition,
+        popExitTransition: (AnimatedContentTransitionScope<NavBackStackEntry>.() -> @JvmSuppressWildcards ExitTransition?)? = exitTransition,
+        sizeTransform: (AnimatedContentTransitionScope<NavBackStackEntry>.() -> @JvmSuppressWildcards SizeTransform?)? = null,
+    )
 }
