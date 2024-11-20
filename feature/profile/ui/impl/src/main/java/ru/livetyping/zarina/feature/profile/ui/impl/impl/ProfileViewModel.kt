84a8465069e8fa343085b5827b210f6b7255e9e6
Artem Sidorov <sidorovaa@melonfashion.com>
2024-11-20 10:01:30 +0000

    Add userState to ProfileViewModel

--- feature/profile/ui/impl/src/main/java/ru/livetyping/zarina/feature/profile/ui/impl/impl/ProfileViewModel.kt
+++ feature/profile/ui/impl/src/main/java/ru/livetyping/zarina/feature/profile/ui/impl/impl/ProfileViewModel.kt
@@ -2,18 +2,41 @@ package ru.livetyping.zarina.feature.profile.ui.impl.impl
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import ru.livetyping.zarina.core.domain.cache.CachePolicy
+import ru.livetyping.zarina.core.domain.usecase.user.GetUserFlowUseCase
 import ru.livetyping.zarina.core.uicommon.sideeffect.SideEffectSource
 import ru.livetyping.zarina.core.uicommon.sideeffect.SideEffectSourceImpl
 import ru.livetyping.zarina.core.uicommon.throttler.Throttler
+import ru.livetyping.zarina.feature.profile.ui.impl.impl.model.ProfileState
+import ru.livetyping.zarina.feature.profile.ui.impl.impl.model.UserState
 import javax.inject.Inject
 
 @HiltViewModel
 internal class ProfileViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
+    getUserFlowUseCase: GetUserFlowUseCase,
 ) : ViewModel(), SideEffectSource<ProfileSideEffect> by SideEffectSourceImpl() {
 
     private val navigationThrottler = Throttler.getNavigationThrottler()
+
+    private val getUserUseCaseParams = GetUserFlowUseCase.Params(CachePolicy.LocalOnly)
+    private val userState: StateFlow<UserState> = getUserFlowUseCase(getUserUseCaseParams)
+        .map { result ->
+            UserState.Success(user = result.getOrNull())
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = UserState.Loading,
+        )
+
+    val profileState: StateFlow<ProfileState> = TODO()
 
     fun onBackClicked() {
         navigationThrottler.throttle {
