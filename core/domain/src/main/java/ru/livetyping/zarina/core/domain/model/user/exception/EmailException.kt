686b550253ccb668a6add540875bf481d55470c2
Artem Sidorov <sidorovaa@melonfashion.com>
2024-11-25 13:39:42 +0000

    Refactor email exceptions

--- core/domain/src/main/java/ru/livetyping/zarina/core/domain/model/user/exception/EmailException.kt
+++ core/domain/src/main/java/ru/livetyping/zarina/core/domain/model/user/exception/EmailException.kt
@@ -2,9 +2,9 @@ package ru.livetyping.zarina.core.domain.model.user.exception
 
 public open class EmailException(message: String) : Exception(message)
 
-public class InvalidEmailException(message: String = "Invalid email") : EmailException(message)
+public open class InvalidEmailException(message: String = "Invalid email") : EmailException(message)
 
 public class EmptyEmailException(message: String = "Email can not be empty") :
-    EmailException(message)
+    InvalidEmailException(message)
 
 public class EmailAlreadyUsed(message: String = "Email is already used") : EmailException(message)
