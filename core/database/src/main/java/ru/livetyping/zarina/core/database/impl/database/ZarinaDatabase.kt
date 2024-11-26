cbede855ce7abf3f0140d8b2e9580243f6537fab
Artem Sidorov <sidorovaa@melonfashion.com>
2024-11-26 17:48:11 +0000

    Set ZarinaDatabase version to 1

--- core/database/src/main/java/ru/livetyping/zarina/core/database/impl/database/ZarinaDatabase.kt
+++ core/database/src/main/java/ru/livetyping/zarina/core/database/impl/database/ZarinaDatabase.kt
@@ -6,7 +6,7 @@ import ru.livetyping.zarina.core.database.user.UserDao
 import ru.livetyping.zarina.core.database.user.UserEntity
 
 @Database(
-    version = 2,
+    version = 1,
     entities = [UserEntity::class],
 )
 internal abstract class ZarinaDatabase : RoomDatabase() {
