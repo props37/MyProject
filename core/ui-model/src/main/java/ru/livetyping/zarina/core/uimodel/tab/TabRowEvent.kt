ee433e600da515ef23aefaae9247ba83fce8dcb4
Artem Sidorov <sidorovaa@melonfashion.com>
2024-11-26 13:39:18 +0000

    Refactoring

--- core/ui-model/src/main/java/ru/livetyping/zarina/core/uimodel/tab/TabRowEvent.kt
+++ core/ui-model/src/main/java/ru/livetyping/zarina/core/uimodel/tab/TabRowEvent.kt
@@ -1,7 +1,6 @@
 package ru.livetyping.zarina.core.uimodel.tab
 
 public sealed interface TabRowEvent<T> {
-
     public data class TabChanged<T>(val tab: T) : TabRowEvent<T>
 
     public data class TabReselected<T>(val tab: T) : TabRowEvent<T>
