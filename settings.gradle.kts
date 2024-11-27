472105fc2be837b015cf1a62d42bbb8a5d3dca48
Artem Sidorov <sidorovaa@melonfashion.com>
2024-11-27 08:31:31 +0000

    Introduce :feature:signup:ui:* modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -44,6 +44,9 @@ include(":feature:city-selector:ui:impl")
 include(":feature:signin:ui:api")
 include(":feature:signin:ui:impl")
 
+include(":feature:signup:ui:api")
+include(":feature:signup:ui:impl")
+
 include(":feature:product-list:ui:api")
 include(":feature:product-list:ui:impl")
 
