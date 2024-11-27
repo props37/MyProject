03218d573c6b74a1a715bbde1ed2e431c05fd1f4
Artem Sidorov <sidorovaa@melonfashion.com>
2024-11-27 14:07:22 +0000

    Update OrderListViewModel

--- feature/profile/ui/impl/src/main/java/ru/livetyping/zarina/feature/profile/ui/impl/impl/orderlist/OrderListViewModel.kt
+++ feature/profile/ui/impl/src/main/java/ru/livetyping/zarina/feature/profile/ui/impl/impl/orderlist/OrderListViewModel.kt
@@ -1,24 +1,39 @@
 package ru.livetyping.zarina.feature.profile.ui.impl.impl.orderlist
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import ru.livetyping.zarina.core.domain.model.order.Order
+import ru.livetyping.zarina.core.domain.model.order.OrderShort
 import ru.livetyping.zarina.core.uicommon.Throttler
 import ru.livetyping.zarina.core.uicommon.sideeffect.SideEffectSource
 import ru.livetyping.zarina.core.uicommon.sideeffect.SideEffectSourceImpl
+import ru.livetyping.zarina.feature.profile.ui.impl.impl.orderlist.paging.OrderPager
 import javax.inject.Inject
 
 @HiltViewModel
 internal class OrderListViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle,
+    orderPager: OrderPager,
 ) : ViewModel(), SideEffectSource<OrderListSideEffect> by SideEffectSourceImpl() {
 
     private val navigationThrottler = Throttler.getNavigationThrottler()
+
+    val orderPagingDataFlow: Flow<PagingData<OrderShort>> = orderPager
+        .getOrderPagingDataFlow()
+        .cachedIn(viewModelScope)
 
     fun onBackClicked() {
         navigationThrottler.throttle {
             val action = OrderListScreenAction.ScreenClosed
             emitSideEffect(OrderListSideEffect.Navigate(action))
         }
+    }
+
+    fun onOrderClicked(order: Order) {
+        TODO()
+        // TODO: [Top] Implement
     }
 }
