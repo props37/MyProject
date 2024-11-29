7217093d2531fb8bab1770e4cfe9c875479ebc92
Artem Sidorov <sidorovaa@melonfashion.com>
2024-11-29 14:05:04 +0000

    Optimize imports

--- feature/signup/ui/impl/src/main/java/ru/livetyping/zarina/feature/signup/ui/impl/impl/signup/component/SignUpScreenContent.kt
+++ feature/signup/ui/impl/src/main/java/ru/livetyping/zarina/feature/signup/ui/impl/impl/signup/component/SignUpScreenContent.kt
@@ -23,7 +23,6 @@ import androidx.compose.material.LocalMinimumInteractiveComponentEnforcement
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
