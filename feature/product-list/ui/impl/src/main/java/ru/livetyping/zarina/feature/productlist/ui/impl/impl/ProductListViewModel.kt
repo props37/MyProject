75ef48c09e3bb612fe91dd1f5771b03d0676a382
Artem Sidorov <sidorovaa@melonfashion.com>
2024-12-04 14:55:17 +0000

    Update ProductListViewModel

--- feature/product-list/ui/impl/src/main/java/ru/livetyping/zarina/feature/productlist/ui/impl/impl/ProductListViewModel.kt
+++ feature/product-list/ui/impl/src/main/java/ru/livetyping/zarina/feature/productlist/ui/impl/impl/ProductListViewModel.kt
@@ -2,8 +2,19 @@ package ru.livetyping.zarina.feature.productlist.ui.impl.impl
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.plus
+import ru.livetyping.zarina.core.coroutinesutil.FlowRequest
+import ru.livetyping.zarina.core.coroutinesutil.FlowRequester
+import ru.livetyping.zarina.core.domain.cache.CachePolicy
+import ru.livetyping.zarina.core.domain.model.category.Category
+import ru.livetyping.zarina.core.domain.usecase.category.GetCategoryFlowUseCase
 import ru.livetyping.zarina.core.uicommon.Throttler
 import ru.livetyping.zarina.core.uicommon.sideeffect.SideEffectSource
 import ru.livetyping.zarina.core.uicommon.sideeffect.SideEffectSourceImpl
@@ -13,11 +24,32 @@ import javax.inject.Inject
 @HiltViewModel
 internal class ProductListViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
+    getCategoryFlow: GetCategoryFlowUseCase,
 ) : ViewModel(), SideEffectSource<ProductListSideEffect> by SideEffectSourceImpl() {
+
+    // TODO: [Top] Inject dispatcher
+    private val viewModelScopeDefault = viewModelScope + Dispatchers.Default
 
     private val navigationThrottler = Throttler.getNavigationThrottler()
 
     private val navEntry = savedStateHandle.toRoute<ProductListNavEntry>()
+    private val categoryId = Category.Id(navEntry.categoryId)
+
+    private val categoryRequester = FlowRequester(CategoryRequest) {
+        val params = GetCategoryFlowUseCase.Params(
+            id = categoryId,
+            cachePolicy = CachePolicy.LocalFirstThenRemote(),
+        )
+        getCategoryFlow(params)
+    }
+
+    private val category = categoryRequester.flow
+        .map { it.getOrNull() }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = null,
+        )
 
     fun onBackClicked() {
         navigationThrottler.throttle {
@@ -25,4 +57,6 @@ internal class ProductListViewModel @Inject constructor(
             emitSideEffect(ProductListSideEffect.Navigate(action))
         }
     }
+
+    private data object CategoryRequest : FlowRequest
 }
