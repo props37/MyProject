e8a7afd5e98a90008cf7a730b74b6a4a6651ce9e
Artem Sidorov <sidorovaa@melonfashion.com>
2024-12-08 18:34:09 +0000

    Update Compose stability config file

--- core/domain/src/main/java/ru/livetyping/zarina/core/domain/model/auth/BearerTokens.kt
+++ core/domain/src/main/java/ru/livetyping/zarina/core/domain/model/auth/BearerTokens.kt
@@ -2,6 +2,7 @@ package ru.livetyping.zarina.core.domain.model.auth
 
 import ru.livetyping.zarina.core.domain.model.common.Token
 
+// Marked as stable on config/compose/stability_config.txt
 public data class BearerTokens(
     val accessToken: Token,
     val refreshToken: Token,
