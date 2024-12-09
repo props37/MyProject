54e8f7155b476c1a140367f94f071a7b93ad900f
Artem Sidorov <sidorovaa@melonfashion.com>
2024-12-09 18:57:12 +0000

    Add CartRepository::getCartProductIdsFlow and areCartProductIdsFetched methods

--- core/domain/src/main/java/ru/livetyping/zarina/core/domain/repository/CartRepository.kt
+++ core/domain/src/main/java/ru/livetyping/zarina/core/domain/repository/CartRepository.kt
@@ -1,3 +1,11 @@
 package ru.livetyping.zarina.core.domain.repository
 
-public interface CartRepository
+import kotlinx.coroutines.flow.Flow
+import ru.livetyping.zarina.core.domain.cache.CachePolicy
+import ru.livetyping.zarina.core.domain.model.product.Product
+
+public interface CartRepository {
+    public fun getCartProductIdsFlow(cachePolicy: CachePolicy): Flow<Set<Product.Id>>
+
+    public fun areCartProductIdsFetched(): Boolean
+}
