7cbdb8b26d4c3150e567cee86113cef630638005
Artem Sidorov <sidorovaa@melonfashion.com>
2024-12-10 10:47:48 +0000

    Add :core:deep-link module

---
+++ core/deep-link/src/main/java/ru/livetyping/zarina/core/deeplink/ZarinaWebLinkUri.kt
@@ -0,0 +1,8 @@
+package ru.livetyping.zarina.core.deeplink
+
+public val ZarinaWebLinkUris: List<String> = listOf(
+    "https://zarina.ru",
+    "https://www.zarina.ru",
+    "https://pwa.zarina.ru",
+    "https://www.pwa.zarina.ru",
+)
