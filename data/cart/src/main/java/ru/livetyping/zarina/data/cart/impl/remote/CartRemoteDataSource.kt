e6f45e1ef54421606a8da17f369d7ad34b547ba9
Artem Sidorov <sidorovaa@melonfashion.com>
2024-12-10 10:02:32 +0000

    Add CartRemoteDataSource::getCartProductIdsFlow method

--- data/cart/src/main/java/ru/livetyping/zarina/data/cart/impl/remote/CartRemoteDataSource.kt
+++ data/cart/src/main/java/ru/livetyping/zarina/data/cart/impl/remote/CartRemoteDataSource.kt
@@ -1,5 +1,8 @@
 package ru.livetyping.zarina.data.cart.impl.remote
 
-internal interface CartRemoteDataSource {
+import kotlinx.coroutines.flow.Flow
+import ru.livetyping.zarina.data.cart.impl.model.CartProductIds
 
+internal interface CartRemoteDataSource {
+    fun getCartProductIdsFlow(): Flow<CartProductIds>
 }
--- data/cart/src/main/java/ru/livetyping/zarina/data/cart/impl/remote/CartRemoteDataSourceImpl.kt
+++ data/cart/src/main/java/ru/livetyping/zarina/data/cart/impl/remote/CartRemoteDataSourceImpl.kt
@@ -1,10 +1,16 @@
 package ru.livetyping.zarina.data.cart.impl.remote
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import ru.livetyping.zarina.data.cart.impl.model.CartProductIds
 import ru.livetyping.zarina.data.cart.impl.remote.api.CartApi
 import javax.inject.Inject
 
 internal class CartRemoteDataSourceImpl @Inject constructor(
-    private val cartApi: CartApi,
+    private val api: CartApi,
 ) : CartRemoteDataSource {
-
+    override fun getCartProductIdsFlow(): Flow<CartProductIds> = flow {
+        val cartProductIds = api.getCartProductIds().toCartProductIds()
+        emit(cartProductIds)
+    }
 }
