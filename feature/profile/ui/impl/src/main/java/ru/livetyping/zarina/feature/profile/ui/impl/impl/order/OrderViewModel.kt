0f6a592766642ebcf65078edba065cc49db11888
Artem Sidorov <sidorovaa@melonfashion.com>
2024-12-16 17:21:00 +0000

    Update OrderViewModel

--- feature/profile/ui/impl/src/main/java/ru/livetyping/zarina/feature/profile/ui/impl/impl/order/OrderViewModel.kt
+++ feature/profile/ui/impl/src/main/java/ru/livetyping/zarina/feature/profile/ui/impl/impl/order/OrderViewModel.kt
@@ -2,7 +2,9 @@ package ru.livetyping.zarina.feature.profile.ui.impl.impl.order
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.navigation.toRoute
 import dagger.hilt.android.lifecycle.HiltViewModel
+import ru.livetyping.zarina.core.domain.model.order.Order
 import ru.livetyping.zarina.core.uicommon.Throttler
 import ru.livetyping.zarina.core.uicommon.sideeffect.SideEffectSource
 import ru.livetyping.zarina.core.uicommon.sideeffect.SideEffectSourceImpl
@@ -14,6 +16,9 @@ internal class OrderViewModel @Inject constructor(
 ) : ViewModel(), SideEffectSource<OrderSideEffect> by SideEffectSourceImpl() {
 
     private val navigationThrottler = Throttler.getNavigationThrottler()
+
+    private val navEntry = savedStateHandle.toRoute<OrderNavEntry>()
+    private val orderId = Order.Id(navEntry.orderId)
 
     fun onBackClicked() {
         navigationThrottler.throttle {
