c8fe2b999be391d9051cdf1b000eb5337b16a8e9
Artem Sidorov <sidorovaa@melonfashion.com>
2024-12-18 17:35:48 +0000

    Update sign in PhoneConfirmation screen

--- feature/signin/ui/impl/src/main/java/ru/livetyping/zarina/feature/signin/ui/impl/impl/phoneconfirmation/PhoneConfirmationScreen.kt
+++ feature/signin/ui/impl/src/main/java/ru/livetyping/zarina/feature/signin/ui/impl/impl/phoneconfirmation/PhoneConfirmationScreen.kt
@@ -2,23 +2,41 @@ package ru.livetyping.zarina.feature.signin.ui.impl.impl.phoneconfirmation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.union
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import ru.livetyping.zarina.core.domain.model.common.PhoneNumber
+import ru.livetyping.zarina.core.uicomponent.otp.SmsOtp
 import ru.livetyping.zarina.core.uicompose.otp.TextFieldOtpState
+import ru.livetyping.zarina.core.uicompose.tryRequestFocus
 import ru.livetyping.zarina.core.uikit.bottomnavbar.bottomNavBarPadding
+import ru.livetyping.zarina.core.uikit.scroll.ZarinaScrollableDefaults
 import ru.livetyping.zarina.core.uikit.theme.UiKitTheme
+import ru.livetyping.zarina.feature.signin.ui.impl.impl.phoneconfirmation.component.PhoneConfirmationTopBar
 
 @Composable
 internal fun PhoneConfirmationScreen(
@@ -60,7 +78,34 @@ internal fun ScreenContent(
             )
             .bottomNavBarPadding(WindowInsets.ime),
     ) {
-        TODO()
-        // TODO: [Top] Implement
+        PhoneConfirmationTopBar(onBackClicked = { onEvent(PhoneConfirmationEvent.BackClicked) })
+
+        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+            Spacer(modifier = Modifier.height(24.dp))
+
+            val focusRequester = remember { FocusRequester() }
+            LaunchedEffect(Unit) {
+                delay(FocusRequestDelay)
+                focusRequester.tryRequestFocus()
+            }
+
+            SmsOtp(
+                otpState = otpState,
+                phone = phone,
+                onOtpEntered = { onEvent(PhoneConfirmationEvent.OtpEntered) },
+                onRequestNewOtpClicked = {
+                    onEvent(PhoneConfirmationEvent.RequestNewOtpClicked)
+                },
+                contentPadding = PaddingValues(horizontal = 16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester),
+            )
+
+            Spacer(modifier = Modifier.height(ZarinaScrollableDefaults.ScrollableBottomPadding))
+            Spacer(modifier = Modifier.windowInsetsBottomHeight(WindowInsets.safeDrawing))
+        }
     }
 }
+
+private const val FocusRequestDelay = 100L
