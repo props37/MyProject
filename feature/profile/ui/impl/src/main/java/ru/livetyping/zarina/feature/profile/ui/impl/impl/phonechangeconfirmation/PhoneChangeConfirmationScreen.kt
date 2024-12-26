769f5a3ee69f882ac97e295b0f84b81e4b4e337c
Artem Sidorov <sidorovaa@melonfashion.com>
2024-12-26 19:17:55 +0000

    Update PhoneChangeConfirmation screen

--- feature/profile/ui/impl/src/main/java/ru/livetyping/zarina/feature/profile/ui/impl/impl/phonechangeconfirmation/PhoneChangeConfirmationScreen.kt
+++ feature/profile/ui/impl/src/main/java/ru/livetyping/zarina/feature/profile/ui/impl/impl/phonechangeconfirmation/PhoneChangeConfirmationScreen.kt
@@ -1,26 +1,56 @@
 package ru.livetyping.zarina.feature.profile.ui.impl.impl.phonechangeconfirmation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.union
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import ru.livetyping.zarina.core.uicommon.YandexCaptchaEvent
+import ru.livetyping.zarina.core.uicomponent.captcha.YandexCaptchaDialog
+import ru.livetyping.zarina.core.uicomponent.otp.SmsOtp
+import ru.livetyping.zarina.core.uicompose.tryRequestFocus
 import ru.livetyping.zarina.core.uikit.bottomnavbar.bottomNavBarPadding
+import ru.livetyping.zarina.core.uikit.scroll.ZarinaScrollableDefaults
 import ru.livetyping.zarina.core.uikit.theme.UiKitTheme
+import ru.livetyping.zarina.feature.profile.ui.impl.impl.phonechangeconfirmation.model.PhoneChangeConfirmationEvent
+import ru.livetyping.zarina.feature.profile.ui.impl.impl.phonechangeconfirmation.model.PhoneChangeConfirmationState
 
 @Composable
 internal fun PhoneChangeConfirmationScreen(
     navActions: PhoneChangeConfirmationNavActions,
     viewModel: PhoneChangeConfirmationViewModel = hiltViewModel(),
 ) {
+    val state by viewModel.phoneChangeConfirmationState.collectAsStateWithLifecycle()
+
     ScreenContent(
+        state = state,
+        onEvent = viewModel::onPhoneChangeConfirmationEvent,
+        onYandexCaptchaEvent = viewModel::onYandexCaptchaEvent,
         sideEffects = viewModel.sideEffects,
         navActions = navActions,
     )
@@ -28,6 +58,9 @@ internal fun PhoneChangeConfirmationScreen(
 
 @Composable
 internal fun ScreenContent(
+    state: PhoneChangeConfirmationState,
+    onEvent: (PhoneChangeConfirmationEvent) -> Unit,
+    onYandexCaptchaEvent: (YandexCaptchaEvent) -> Unit,
     sideEffects: Flow<PhoneChangeConfirmationSideEffect>,
     navActions: PhoneChangeConfirmationNavActions,
 ) {
@@ -36,16 +69,55 @@ internal fun ScreenContent(
         navActions = navActions,
     )
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(UiKitTheme.colors.background.general.regular.default)
-            .windowInsetsPadding(
-                WindowInsets.statusBars
-                    .union(WindowInsets.displayCutout),
-            )
-            .bottomNavBarPadding(),
-    ) {
+    Box {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(UiKitTheme.colors.background.general.regular.default)
+                .windowInsetsPadding(
+                    WindowInsets.statusBars
+                        .union(WindowInsets.displayCutout)
+                        .union(WindowInsets.ime),
+                )
+                .bottomNavBarPadding(WindowInsets.ime),
+        ) {
+            // TODO: [Top] Add top bar
 
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                Spacer(modifier = Modifier.height(24.dp))
+
+                val focusRequester = remember { FocusRequester() }
+                LaunchedEffect(Unit) {
+                    delay(FocusRequestDelay)
+                    focusRequester.tryRequestFocus()
+                }
+
+                SmsOtp(
+                    otpState = state.otpState,
+                    phone = state.phone,
+                    isRequestNewOtpButtonLoading = state.isRequestNewOtpButtonLoading,
+                    onOtpEntered = { onEvent(PhoneChangeConfirmationEvent.OtpEntered) },
+                    onRequestNewOtpClicked = {
+                        onEvent(PhoneChangeConfirmationEvent.RequestNewOtpClicked)
+                    },
+                    contentPadding = PaddingValues(horizontal = 16.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .focusRequester(focusRequester),
+                )
+
+                Spacer(modifier = Modifier.height(ZarinaScrollableDefaults.ScrollableBottomPadding))
+                Spacer(modifier = Modifier.windowInsetsBottomHeight(WindowInsets.safeDrawing))
+            }
+        }
+
+        if (state.visibleYandexCaptcha != null) {
+            YandexCaptchaDialog(
+                captcha = state.visibleYandexCaptcha,
+                onEvent = onYandexCaptchaEvent,
+            )
+        }
     }
 }
+
+private const val FocusRequestDelay = 100L
