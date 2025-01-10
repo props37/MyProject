1b647f9e3cf31d224edf05426ebba13a25b6fcc1
Artem Sidorov <sidorovaa@melonfashion.com>
2025-01-10 13:55:35 +0000

    Introduce WebViewFeature

---
+++ feature/webview/ui/api/src/main/java/ru/livetyping/zarina/feature/webview/ui/WebViewFeature.kt
@@ -0,0 +1,17 @@
+package ru.livetyping.zarina.feature.webview.ui
+
+import kotlinx.serialization.Serializable
+import ru.livetyping.zarina.core.feature.ComposableFeatureEntry
+import ru.livetyping.zarina.core.navigation.NavigationEntry
+import ru.livetyping.zarina.feature.webview.ui.WebViewFeature.NavActions
+import ru.livetyping.zarina.feature.webview.ui.WebViewFeature.NavEntry
+
+public interface WebViewFeature : ComposableFeatureEntry<NavEntry, NavActions, Unit> {
+
+    @Serializable
+    public data class NavEntry(val url: String) : NavigationEntry
+
+    public class NavActions(
+        public val onBackClicked: () -> Unit,
+    )
+}
