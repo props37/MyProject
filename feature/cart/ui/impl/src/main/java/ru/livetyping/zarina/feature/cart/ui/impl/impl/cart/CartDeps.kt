31b35a47d0f90baed1e8837309c87a562a2b8fd7
Artem Sidorov <sidorovaa@melonfashion.com>
2025-01-14 10:00:30 +0000

    Update CartDeps

--- feature/cart/ui/impl/src/main/java/ru/livetyping/zarina/feature/cart/ui/impl/impl/cart/CartDeps.kt
+++ feature/cart/ui/impl/src/main/java/ru/livetyping/zarina/feature/cart/ui/impl/impl/cart/CartDeps.kt
@@ -2,9 +2,23 @@ package ru.livetyping.zarina.feature.cart.ui.impl.impl.cart
 
 import ru.livetyping.zarina.core.domain.usecase.cart.GetCartProductCountFlowUseCase
 import ru.livetyping.zarina.core.domain.usecase.cart.GetCartProductIdsFlowUseCase
+import ru.livetyping.zarina.core.domain.usecase.user.GetUserCityFlowUseCase
+import ru.livetyping.zarina.core.domain.usecase.user.SetUserCityUseCase
+import ru.livetyping.zarina.core.domain.usecase.wishlist.ToggleProductInWishlistUseCase
 import javax.inject.Inject
 
 internal class CartDeps @Inject constructor(
     val getCartProductIdsFlow: GetCartProductIdsFlowUseCase,
     val getCartProductCountFlow: GetCartProductCountFlowUseCase,
+    val getUserCityFlow: GetUserCityFlowUseCase,
+    val setUserCity: SetUserCityUseCase,
+    // TODO: [Top] Add GetCartFlowUseCase
+    // TODO: [Top] Add ClearCartUseCase
+    val toggleProductInWishlist: ToggleProductInWishlistUseCase,
+    // TODO: [Top] Add RemoveProductFromCartUseCase
+    // TODO: [Top] Add ApplyMyCardToCartUseCase
+    // TODO: [Top] Add ApplyPromoCodeUseCase
+    // TODO: [Top] Add RemovePromoCodeUseCase
+    // TODO: [Top] Add ApplyBonusWriteOffUseCase
+    // TODO: [Top] Add RemoveBonusWriteOffUseCase
 )
