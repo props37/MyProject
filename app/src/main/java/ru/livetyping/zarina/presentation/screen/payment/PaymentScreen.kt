fa5ea99a384b52d491b86cdb01b008ecfbf41e59
Artem Sidorov <sidorovaa@melonfashion.com>
2025-01-17 13:15:17 +0000

    Update PaymentScreen internal WebView settings

--- app/src/main/java/ru/livetyping/zarina/presentation/screen/payment/PaymentScreen.kt
+++ app/src/main/java/ru/livetyping/zarina/presentation/screen/payment/PaymentScreen.kt
@@ -77,7 +77,6 @@ private fun ScreenContent(
                 factory = { context ->
                     WebView(context).apply {
                         isVisible = isWebViewVisible
-                        loadUrl(paymentUrl.value)
                         settings.apply {
                             javaScriptEnabled = true
                             builtInZoomControls = false
@@ -86,6 +85,8 @@ private fun ScreenContent(
                             loadsImagesAutomatically = true
                             domStorageEnabled = true
                         }
+                        scrollBarStyle = WebView.SCROLLBARS_OUTSIDE_OVERLAY
+                        isScrollbarFadingEnabled = true
                         webViewClient = object : WebViewClient() {
                             override fun onPageFinished(view: WebView?, url: String?) {
                                 if (!isWebViewVisible && url == paymentUrl.value) {
@@ -93,6 +94,7 @@ private fun ScreenContent(
                                 }
                             }
                         }
+                        loadUrl(paymentUrl.value)
                     }
                 },
                 update = { webView ->
