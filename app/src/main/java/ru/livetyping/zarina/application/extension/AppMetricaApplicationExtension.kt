fa7692a670b50910817e387432754ba8eb88c164
Artem Sidorov <sidorovaa@melonfashion.com>
2025-01-29 16:15:22 +0000

    Initialize AppMetrica on app startup

---
+++ app/src/main/java/ru/livetyping/zarina/application/extension/AppMetricaApplicationExtension.kt
@@ -0,0 +1,17 @@
+package ru.livetyping.zarina.application.extension
+
+import android.app.Application
+import io.appmetrica.analytics.AppMetrica
+import io.appmetrica.analytics.AppMetricaConfig
+import ru.livetyping.zarina.BuildConfig
+import ru.livetyping.zarina.application.extension.base.ApplicationExtension
+import javax.inject.Inject
+
+class AppMetricaApplicationExtension @Inject constructor() : ApplicationExtension {
+    override fun install(application: Application) {
+        val config = AppMetricaConfig.newConfigBuilder(BuildConfig.APP_METRICA_KEY).apply {
+            if (BuildConfig.IS_LOGGING_ENABLED) withLogs()
+        }.build()
+        AppMetrica.activate(application, config)
+    }
+}
