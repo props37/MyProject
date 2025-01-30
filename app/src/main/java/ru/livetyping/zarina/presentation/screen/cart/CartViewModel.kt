bb15de15119d1950410a2dca5ebf78c3b279b348
Artem Sidorov <sidorovaa@melonfashion.com>
2025-01-30 18:05:29 +0000

    Report StartCheckoutEvents

--- app/src/main/java/ru/livetyping/zarina/presentation/screen/cart/CartViewModel.kt
+++ app/src/main/java/ru/livetyping/zarina/presentation/screen/cart/CartViewModel.kt
@@ -485,6 +485,7 @@ class CartViewModel @AssistedInject constructor(
 
     fun onCheckoutClicked() {
         navigationThrottler.throttle {
+            AppMetricaHelper.reportStartCheckoutEvent()
             val action = CartScreenAction.CheckoutClicked(currentCartType.value)
             emitSideEffect(SideEffect.Navigate(action))
         }
