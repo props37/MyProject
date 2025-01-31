14f96c2f4941299b2d377a904e3c13399ec9df68
Artem Sidorov <sidorovaa@melonfashion.com>
2025-01-31 09:02:18 +0000

    Remove DeviceType from AppMetrica initialization

--- app/src/main/java/ru/livetyping/zarina/application/extension/AppMetricaApplicationExtension.kt
+++ app/src/main/java/ru/livetyping/zarina/application/extension/AppMetricaApplicationExtension.kt
@@ -3,7 +3,6 @@ package ru.livetyping.zarina.application.extension
 import android.app.Application
 import io.appmetrica.analytics.AppMetrica
 import io.appmetrica.analytics.AppMetricaConfig
-import io.appmetrica.analytics.PredefinedDeviceTypes
 import io.appmetrica.analytics.profile.Attribute
 import io.appmetrica.analytics.profile.GenderAttribute
 import io.appmetrica.analytics.profile.UserProfile
@@ -32,7 +31,6 @@ class AppMetricaApplicationExtension @Inject constructor(
 
     private fun initializeAppMetrica(application: Application) {
         val config = AppMetricaConfig.newConfigBuilder(BuildConfig.APP_METRICA_KEY).apply {
-            withDeviceType(getDeviceType())
             if (BuildConfig.IS_LOGGING_ENABLED) withLogs()
         }.build()
         AppMetrica.activate(application, config)
@@ -73,9 +71,5 @@ class AppMetricaApplicationExtension @Inject constructor(
             }
             apply(ageAttr)
         }.build()
-    }
-
-    private fun getDeviceType(): String {
-        return "${PredefinedDeviceTypes.PHONE}-android"
     }
 }
