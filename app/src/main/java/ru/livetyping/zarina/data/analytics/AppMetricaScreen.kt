22b1bd72153d12f51ad25ce386084f599ee81ab4
Artem Sidorov <sidorovaa@melonfashion.com>
2025-01-31 09:03:57 +0000

    Specify screenName for AppMetricaScreens

--- app/src/main/java/ru/livetyping/zarina/data/analytics/AppMetricaScreen.kt
+++ app/src/main/java/ru/livetyping/zarina/data/analytics/AppMetricaScreen.kt
@@ -3,15 +3,15 @@ package ru.livetyping.zarina.data.analytics
 import io.appmetrica.analytics.ecommerce.ECommerceScreen
 
 // NOTE: DO NOT CHANGE. Values correspond to AppMetrica screen names. The same are used on iOS app.
-enum class AppMetricaScreen {
-    PRODUCT_LIST,
-    PRODUCT,
-    WISHLIST,
-    PRODUCT_SEARCH;
+enum class AppMetricaScreen(val screenName: String) {
+    PRODUCT_LIST("product-list"),
+    PRODUCT("product"),
+    WISHLIST("wishlist"),
+    PRODUCT_SEARCH("product-search");
 
     fun toECommerceScreen(): ECommerceScreen {
         return ECommerceScreen().apply {
-            this.name = this@AppMetricaScreen.name
+            this.name = screenName
         }
     }
 }
