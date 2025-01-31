d36f63bd751517432a8f7308796a04feb62a23ae
Artem Sidorov <sidorovaa@melonfashion.com>
2025-01-31 17:31:49 +0000

    Clear :core:ui-component module

--- core/ui-component/build.gradle.kts
+++ core/ui-component/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.parcelize)
-    alias(libs.plugins.kotlin.compose.compiler)
 }
 
 android {
@@ -27,7 +25,6 @@ android {
     }
 
     compileOptions {
-        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -35,44 +32,14 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-
-    buildFeatures {
-        compose = true
-    }
-
-    lint {
-        disable += listOf("UsingMaterialAndMaterial3Libraries")
-    }
 }
 
 kotlin {
     explicitApi()
 }
 
-composeCompiler {
-    reportsDestination = layout.buildDirectory.dir("compose_compiler")
-    stabilityConfigurationFiles.add(rootProject.layout.projectDirectory.file("config/compose/stability_config.txt"))
-}
-
 dependencies {
-    api(projects.core.domain)
-    api(projects.core.uiKit)
-    implementation(projects.core.uiCompose)
-    implementation(projects.core.resource)
-
-    implementation(platform(libs.jetpack.compose.bom))
-    implementation(libs.jetpack.compose.ui)
-    implementation(libs.jetpack.compose.material)
-    api(libs.jetpack.compose.material3)
-    implementation(libs.jetpack.compose.toolingPreview)
-    debugImplementation(libs.jetpack.compose.tooling)
-    debugImplementation(libs.jetpack.compose.testManifest)
-
     implementation(libs.timber)
-
-    lintChecks(libs.lint.composeChecks)
-
-    coreLibraryDesugaring(libs.coreLibraryDesugaring)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.jetpack.test.junit)
