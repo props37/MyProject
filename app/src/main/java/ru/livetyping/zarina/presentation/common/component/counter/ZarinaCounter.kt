6ce14b9868593c792808cf0664037593b6a0b7d8
Artem Sidorov <sidorovaa@melonfashion.com>
2025-02-03 10:24:57 +0000

    Fix ZarinaCounter appearance

--- app/src/main/java/ru/livetyping/zarina/presentation/common/component/counter/ZarinaCounter.kt
+++ app/src/main/java/ru/livetyping/zarina/presentation/common/component/counter/ZarinaCounter.kt
@@ -33,7 +33,7 @@ fun ZarinaCounter(
             contentAlignment = Alignment.Center,
             modifier = modifier
                 .drawBehind {
-                    drawCircle(color = backgroundColor, radius = this.size.maxDimension / 2)
+                    drawCircle(color = backgroundColor, radius = this.size.minDimension / 2)
                 }
                 .padding(contentPadding),
             content = content,
