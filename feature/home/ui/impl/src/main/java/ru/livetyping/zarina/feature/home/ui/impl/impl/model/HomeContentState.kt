83a060f6c32f20c444bf3ad71ff3be5548640308
Artem Sidorov <sidorovaa@melonfashion.com>
2025-02-04 20:07:56 +0000

    Refactor HomeContentState

--- feature/home/ui/impl/src/main/java/ru/livetyping/zarina/feature/home/ui/impl/impl/model/HomeContentState.kt
+++ feature/home/ui/impl/src/main/java/ru/livetyping/zarina/feature/home/ui/impl/impl/model/HomeContentState.kt
@@ -6,17 +6,21 @@ import ru.livetyping.zarina.core.uikit.error.ZarinaErrorScreenState
 import ru.livetyping.zarina.feature.home.domain.model.HomeContent
 
 @Stable
-internal sealed class HomeContentState(
-    open val isRefreshing: Boolean,
-) {
+internal sealed class HomeContentState {
+    abstract val isRefreshing: Boolean
+
     @Immutable
     data class Success(
         val content: HomeContent,
         override val isRefreshing: Boolean,
-    ) : HomeContentState(isRefreshing = isRefreshing)
+    ) : HomeContentState()
 
-    data object Loading : HomeContentState(isRefreshing = false)
+    data object Loading : HomeContentState() {
+        override val isRefreshing = false
+    }
 
     @Immutable
-    data class Error(val state: ZarinaErrorScreenState) : HomeContentState(isRefreshing = false)
+    data class Error(val state: ZarinaErrorScreenState) : HomeContentState() {
+        override val isRefreshing = false
+    }
 }
