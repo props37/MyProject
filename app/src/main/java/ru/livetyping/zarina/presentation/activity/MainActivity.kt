54b789091e25f22ada44183a91813308973e626f
Artem Sidorov <sidorovaa@melonfashion.com>
2025-02-07 21:41:56 +0000

    Move startup fetchers from ZarinaApplication to MainActivity

--- app/src/main/java/ru/livetyping/zarina/presentation/activity/MainActivity.kt
+++ app/src/main/java/ru/livetyping/zarina/presentation/activity/MainActivity.kt
@@ -20,9 +20,20 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.flow.transformLatest
 import kotlinx.coroutines.launch
 import ru.livetyping.zarina.BuildConfig
 import ru.livetyping.zarina.base.behavior.DefaultBehaviorController
+import ru.livetyping.zarina.domain.authorization.AuthorizationTokens
 import ru.livetyping.zarina.presentation.activity.lifecycleobserver.ActivityLifecycleObserverManager
 import ru.livetyping.zarina.presentation.app.ZarinaApp
 import ru.livetyping.zarina.presentation.common.behavior.screenbrightness.LocalScreenBrightnessBehaviorController
@@ -35,12 +46,20 @@ import ru.livetyping.zarina.presentation.common.behavior.systembars.SystemBarsBe
 import ru.livetyping.zarina.presentation.common.behavior.systembars.SystemBarsBehaviorController
 import ru.livetyping.zarina.presentation.theme.UiKitTheme
 import ru.livetyping.zarina.presentation.theme.ZarinaTheme
+import ru.livetyping.zarina.usecase.authorization.GetAuthorizationTokensFlowUseCase
+import ru.livetyping.zarina.usecase.cart.FetchCartProductIdsUseCase
+import ru.livetyping.zarina.usecase.favorite.FetchFavoriteProductIdsUseCase
+import ru.livetyping.zarina.usecase.user.FetchUserCityUseCase
+import ru.livetyping.zarina.usecase.user.GetUserFlowUseCase
+import ru.livetyping.zarina.util.base.usecase.invoke
 import ru.livetyping.zarina.util.library.accompanist.rememberBottomSheetNavigator
 import ru.livetyping.zarina.util.library.activity.DefaultDarkScrim
 import ru.livetyping.zarina.util.library.activity.DefaultLightScrim
+import ru.livetyping.zarina.util.library.coroutines.WhileUiSubscribed
 import ru.livetyping.zarina.util.platform.getSizeInBytes
 import timber.log.Timber
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.seconds
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -48,12 +67,42 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var activityLifecycleObserverManager: ActivityLifecycleObserverManager
 
+    @Inject
+    lateinit var getAuthorizationTokensFlowUseCase: GetAuthorizationTokensFlowUseCase
+
+    @Inject
+    lateinit var fetchCartProductIdsUseCase: FetchCartProductIdsUseCase
+
+    @Inject
+    lateinit var fetchFavoriteProductIdsUseCase: FetchFavoriteProductIdsUseCase
+
+    @Inject
+    lateinit var fetchUserCityUseCase: FetchUserCityUseCase
+
+    @Inject
+    lateinit var getUserFlowUseCase: GetUserFlowUseCase
+
+    private val authTokensFlow = getAuthorizationTokensFlowUseCase()
+        .map { it.getOrNull() }
+        .conflate()
+        .shareIn(
+            scope = lifecycleScope,
+            started = SharingStarted.WhileUiSubscribed,
+            replay = 1,
+        )
+
     private var navController: NavHostController? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
+        fetchUser()
+        fetchUserCity()
+        setUpCartProductIdsFetching()
+        setUpFavoriteProductIdsFetching()
+
         addActivityLifecycleObservers()
 
         val defaultSystemBarsBehavior = SystemBarsBehavior(
@@ -144,5 +193,50 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun fetchUser() {
+        lifecycleScope.launch {
+            getUserFlowUseCase().firstOrNull()
+        }
+        // TODO: [High] Fetch updated user
+    }
+
+    private fun fetchUserCity() {
+        lifecycleScope.launch {
+            fetchUserCityUseCase()
+        }
+    }
+
+    private fun setUpCartProductIdsFetching() {
+        authTokensFlow
+            .filterNotNull()
+            .distinctUntilChanged()
+            .transformLatest<AuthorizationTokens, Unit> {
+                // TODO: [High] Find a better way
+                // Delay is used to prevent making requests with old authorization tokens
+                // as tokens stored on the disk get updated earlier than HttpClient tokens
+                delay(FETCHING_DELAY)
+                fetchCartProductIdsUseCase()
+            }
+            .launchIn(lifecycleScope)
+    }
+
+    private fun setUpFavoriteProductIdsFetching() {
+        authTokensFlow
+            .filterNotNull()
+            .distinctUntilChanged()
+            .transformLatest<AuthorizationTokens, Unit> {
+                // TODO: [High] Find a better way
+                // Delay is used to prevent making requests with old authorization tokens
+                // as tokens stored on the disk get updated earlier than HttpClient tokens
+                delay(FETCHING_DELAY)
+                fetchFavoriteProductIdsUseCase()
+            }
+            .launchIn(lifecycleScope)
+    }
+
+    companion object {
+        private val FETCHING_DELAY = 1.seconds
     }
 }
