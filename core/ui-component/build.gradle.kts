fd8f01cecdd3ec067265ab415ec49b57872bcc39
Artem Sidorov <sidorovaa@melonfashion.com>
2025-02-14 19:07:40 +0000

    Add :core:ui-kit dependency to :core:ui-component module

--- core/ui-component/build.gradle.kts
+++ core/ui-component/build.gradle.kts
@@ -42,6 +42,7 @@ kotlin {
 dependencies {
     implementation(projects.core.uiCompose)
     implementation(projects.core.uiModel)
+    implementation(projects.core.uiKit)
     implementation(projects.core.platform)
     implementation(projects.core.coroutinesUtil)
 
