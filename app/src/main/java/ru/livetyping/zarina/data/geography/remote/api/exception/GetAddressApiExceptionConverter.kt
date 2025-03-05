6e64bd3ec43feac28cdc70c5cd42ed6a5bc7d42b
Artem Sidorov <sidorovaa@melonfashion.com>
2025-03-05 12:13:01 +0000

    Handle 500 status code on ProductSuggestionsApiExceptionConverter
    Also update KtorApiExceptionConverter to handle more common ResponseException instead of ClientRequestException

--- app/src/main/java/ru/livetyping/zarina/data/geography/remote/api/exception/GetAddressApiExceptionConverter.kt
+++ app/src/main/java/ru/livetyping/zarina/data/geography/remote/api/exception/GetAddressApiExceptionConverter.kt
@@ -1,6 +1,6 @@
 package ru.livetyping.zarina.data.geography.remote.api.exception
 
-import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.plugins.ResponseException
 import io.ktor.http.HttpStatusCode
 import ru.livetyping.zarina.data.common.remote.api.exception.KtorApiExceptionConverter
 import ru.livetyping.zarina.domain.common.exception.EmptySearchQueryException
@@ -8,7 +8,7 @@ import ru.livetyping.zarina.domain.geography.exception.AddressNotFoundException
 import javax.inject.Inject
 
 class GetAddressApiExceptionConverter @Inject constructor() : KtorApiExceptionConverter() {
-    override suspend fun handle(e: ClientRequestException): Nothing {
+    override suspend fun handle(e: ResponseException): Nothing {
         when (e.response.status) {
             HttpStatusCode.UnprocessableEntity -> throw EmptySearchQueryException()
             HttpStatusCode.NotFound -> throw AddressNotFoundException()
