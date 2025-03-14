ba2c5c30f311394dab078fd6352755946eee9011
Artem Sidorov <sidorovaa@melonfashion.com>
2025-03-14 20:59:34 +0000

    Hide keyboard before navigation

--- feature/sign-in/ui/impl/src/main/java/ru/livetyping/zarina/feature/signin/ui/impl/impl/signinbyemail/SignInByEmailPhoneConfirmationScreenBehavior.kt
+++ feature/sign-in/ui/impl/src/main/java/ru/livetyping/zarina/feature/signin/ui/impl/impl/signinbyemail/SignInByEmailPhoneConfirmationScreenBehavior.kt
@@ -3,6 +3,7 @@ package ru.livetyping.zarina.feature.signin.ui.impl.impl.signinbyemail
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.lifecycle.compose.LifecycleStartEffect
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.flow.Flow
@@ -17,6 +18,7 @@ internal fun SignInByEmailPhoneConfirmationScreenBehavior(
 ) {
     val currentNavActions by rememberUpdatedState(navActions)
     val currentZarinaToastController by rememberUpdatedState(LocalZarinaToastController.current)
+    val currentKeyboardController by rememberUpdatedState(LocalSoftwareKeyboardController.current)
 
     BottomNavBarBehavior(isVisible = false)
 
@@ -25,6 +27,7 @@ internal fun SignInByEmailPhoneConfirmationScreenBehavior(
             sideEffects.collect { sideEffect ->
                 when (sideEffect) {
                     is SignInByEmailPhoneConfirmationSideEffect.Navigate -> {
+                        currentKeyboardController?.hide()
                         navigate(currentNavActions, sideEffect.action)
                     }
 
