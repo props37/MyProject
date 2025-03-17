b5c092739503dae45b7adb0e64cf664932d02f77
Artem Sidorov <sidorovaa@melonfashion.com>
2025-03-17 14:54:13 +0000

    Fix mapping from User to UserEntity

--- core/database/src/main/java/ru/livetyping/zarina/core/database/user/UserEntity.kt
+++ core/database/src/main/java/ru/livetyping/zarina/core/database/user/UserEntity.kt
@@ -80,7 +80,7 @@ public data class UserEntity(
                 phone = user.phone?.value,
                 firstName = user.firstName,
                 lastName = user.lastName,
-                birthDate = user.birthDate.toString(),
+                birthDate = user.birthDate?.toString(),
                 gender = user.gender?.let { GenderEntity.from(it) },
                 notificationSettings = NotificationSettings.from(user.notificationSettings),
             )
