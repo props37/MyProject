73199c261a1660f89c72bc694b3611c2e3062123
Artem Sidorov <sidorovaa@melonfashion.com>
2025-03-24 18:34:57 +0000

    Update PickupStoreSelectorDependencies

--- feature/cart/ui/impl/src/main/java/ru/livetyping/zarina/feature/cart/ui/impl/impl/pickupstoreselector/PickupStoreSelectorDependencies.kt
+++ feature/cart/ui/impl/src/main/java/ru/livetyping/zarina/feature/cart/ui/impl/impl/pickupstoreselector/PickupStoreSelectorDependencies.kt
@@ -1,8 +1,10 @@
 package ru.livetyping.zarina.feature.cart.ui.impl.impl.pickupstoreselector
 
 import ru.livetyping.zarina.core.domain.usecase.cart.GetCartFlowUseCase
+import ru.livetyping.zarina.core.domain.usecase.user.GetUserCityFlowUseCase
 import javax.inject.Inject
 
 internal class PickupStoreSelectorDependencies @Inject constructor(
+    val getUserCityFlow: GetUserCityFlowUseCase,
     val getCartFlow: GetCartFlowUseCase,
 )
