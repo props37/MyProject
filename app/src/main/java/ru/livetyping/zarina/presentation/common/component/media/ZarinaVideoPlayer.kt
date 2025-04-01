2e4df9915b1775594f2d9c3e63edbc44817241d7
Artem Sidorov <sidorovaa@melonfashion.com>
2025-04-01 13:11:45 +0100

    Remove code related to workaround for a bug that can't be reproduced now

--- app/src/main/java/ru/livetyping/zarina/presentation/common/component/media/ZarinaVideoPlayer.kt
+++ app/src/main/java/ru/livetyping/zarina/presentation/common/component/media/ZarinaVideoPlayer.kt
@@ -1,14 +1,10 @@
 package ru.livetyping.zarina.presentation.common.component.media
 
-import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.LifecycleResumeEffect
 import androidx.lifecycle.compose.LifecycleStartEffect
@@ -21,14 +17,10 @@ import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.source.ProgressiveMediaSource
 import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import ru.livetyping.zarina.domain.common.Url
 import ru.livetyping.zarina.presentation.common.media.exoplayer.LocalExoPlayerCacheHolder
 import ru.livetyping.zarina.presentation.common.media.exoplayer.rememberExoPlayer
 import timber.log.Timber
-import kotlin.time.Duration.Companion.milliseconds
 
 @androidx.annotation.OptIn(UnstableApi::class)
 @Composable
@@ -119,7 +111,6 @@ fun ZarinaVideoPlayer(
     modifier: Modifier = Modifier,
     resizeMode: Int = AspectRatioFrameLayout.RESIZE_MODE_ZOOM,
 ) {
-    val coroutineScope = rememberCoroutineScope()
     val playerViewState = remember { mutableStateOf<PlayerView?>(null) }
 
     LifecycleResumeEffect(Unit) {
@@ -131,28 +122,6 @@ fun ZarinaVideoPlayer(
             Timber.tag(TAG).v("onPauseOrDispose")
             playerView?.onPause()
         }
-    }
-
-    // Workaround for PlayerView bug due to which the video sometimes doesn't occupy the whole
-    // PlayerView size
-    val xOffsetJob = remember { mutableStateOf<Job?>(null) }
-    val xOffset = remember { mutableIntStateOf(0) }
-    DisposableEffect(exoPlayer) {
-        val listener = object : Player.Listener {
-            override fun onPlaybackStateChanged(playbackState: Int) {
-                if (playbackState == Player.STATE_READY) {
-                    xOffsetJob.value?.cancel()
-                    xOffsetJob.value = coroutineScope.launch {
-                        Timber.tag(TAG).v("Adjust offset to fix scaling")
-                        xOffset.intValue = 1
-                        delay(50.milliseconds)
-                        xOffset.intValue = 0
-                    }
-                }
-            }
-        }
-        exoPlayer.addListener(listener)
-        onDispose {}
     }
 
     AndroidView(
@@ -180,7 +149,7 @@ fun ZarinaVideoPlayer(
             Timber.tag(TAG).v("AndroidView onRelease")
             playerView.player = null
         },
-        modifier = modifier.offset { IntOffset(xOffset.intValue, 0) },
+        modifier = modifier,
     )
 }
 
