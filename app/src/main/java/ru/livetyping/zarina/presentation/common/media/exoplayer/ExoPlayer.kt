998201cfcbf4a1f13022f3853cadb69eaea16963
Artem Sidorov <sidorovaa@melonfashion.com>
2025-04-01 13:08:18 +0100

    Override Player.Listener onPlayerError method on rememberExoPlayer

--- app/src/main/java/ru/livetyping/zarina/presentation/common/media/exoplayer/ExoPlayer.kt
+++ app/src/main/java/ru/livetyping/zarina/presentation/common/media/exoplayer/ExoPlayer.kt
@@ -7,9 +7,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalContext
 import androidx.media3.common.C
+import androidx.media3.common.PlaybackException
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import timber.log.Timber
 
 @OptIn(UnstableApi::class)
 @Composable
@@ -32,6 +34,10 @@ fun rememberExoPlayer(
                             currentOnReadyToPlay?.invoke()
                         }
                     }
+
+                    override fun onPlayerError(error: PlaybackException) {
+                        Timber.tag(TAG).e(error)
+                    }
                 }
                 addListener(listener)
             }
@@ -41,3 +47,5 @@ fun rememberExoPlayer(
         this.videoScalingMode = videoScalingMode
     }
 }
+
+private const val TAG = "ExoPlayer"
