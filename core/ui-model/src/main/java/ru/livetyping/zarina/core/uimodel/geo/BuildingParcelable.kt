d416f655b6d6b9a808a79c19ca5d429f48991d7d
Artem Sidorov <sidorovaa@melonfashion.com>
2025-04-03 16:19:51 +0100

    Add BuildingParcelable and StreetParcelable UI models

---
+++ core/ui-model/src/main/java/ru/livetyping/zarina/core/uimodel/geo/BuildingParcelable.kt
@@ -0,0 +1,30 @@
+package ru.livetyping.zarina.core.uimodel.geo
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
+import ru.livetyping.zarina.core.domain.model.geo.Building
+import ru.livetyping.zarina.core.domain.model.geo.KladrId
+
+@Serializable
+@Parcelize
+public data class BuildingParcelable(
+    val id: String,
+    val name: String,
+) : Parcelable {
+    public fun toBuilding(): Building {
+        return Building(
+            id = KladrId(id),
+            name = name,
+        )
+    }
+
+    public companion object {
+        public fun from(building: Building): BuildingParcelable {
+            return BuildingParcelable(
+                id = building.id.value,
+                name = building.name,
+            )
+        }
+    }
+}
---
+++ core/ui-model/src/main/java/ru/livetyping/zarina/core/uimodel/geo/StreetParcelable.kt
@@ -0,0 +1,30 @@
+package ru.livetyping.zarina.core.uimodel.geo
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
+import ru.livetyping.zarina.core.domain.model.geo.KladrId
+import ru.livetyping.zarina.core.domain.model.geo.Street
+
+@Serializable
+@Parcelize
+public data class StreetParcelable(
+    val id: String,
+    val name: String,
+) : Parcelable {
+    public fun toStreet(): Street {
+        return Street(
+            id = KladrId(id),
+            name = name,
+        )
+    }
+
+    public companion object {
+        public fun from(street: Street): StreetParcelable {
+            return StreetParcelable(
+                id = street.id.value,
+                name = street.name,
+            )
+        }
+    }
+}
