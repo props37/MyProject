bbaa8dac54482e04fc67632902a94edace56532b
Artem Sidorov <sidorovaa@melonfashion.com>
2025-04-15 11:13:54 +0100

    ZRWEB-2212: Use loyalty program level names coming from backend rather than hardcoding them in the app

--- app/src/main/java/ru/livetyping/zarina/data/user/remote/api/dto/LoyaltyCardDto.kt
+++ app/src/main/java/ru/livetyping/zarina/data/user/remote/api/dto/LoyaltyCardDto.kt
@@ -10,10 +10,16 @@ data class LoyaltyCardDto(
     val number: String? = null,
 
     @SerialName("current_level")
-    val level: LoyaltyCardLevelDto? = null,
+    val currentLevel: LoyaltyCardLevelDto? = null,
+
+    @SerialName("current_level_description")
+    val currentLevelDescription: String? = null,
 
     @SerialName("next_level")
     val nextLevel: LoyaltyCardLevelDto? = null,
+
+    @SerialName("next_level_description")
+    val nextLevelDescription: String? = null,
 
     @SerialName("next_level_purchases_sum")
     val nextLevelRequiredPurchaseSum: Int? = null,
@@ -32,20 +38,24 @@ data class LoyaltyCardDto(
 ) {
     fun toLoyaltyCard(): LoyaltyCard {
         checkNotNull(number) { "number is null" }
-        checkNotNull(level) { "level is null" }
-        val nextLevelInfo = if (nextLevel != null && nextLevelRequiredPurchaseSum != null) {
-            LoyaltyCard.NextLevelInfo(
-                level = nextLevel.toLoyaltyCardLevel(),
-                requiredPurchaseSum = nextLevelRequiredPurchaseSum,
-            )
-        } else null
+        checkNotNull(currentLevel) { "currentLevel is null" }
+        checkNotNull(currentLevelDescription) { "currentLevelDescription is null" }
+        val nextLevelInfo =
+            if (nextLevel != null && nextLevelDescription != null && nextLevelRequiredPurchaseSum != null) {
+                LoyaltyCard.NextLevelInfo(
+                    level = nextLevel.toLoyaltyCardLevel(),
+                    levelName = nextLevelDescription,
+                    requiredPurchaseSum = nextLevelRequiredPurchaseSum,
+                )
+            } else null
         val bonuses = LoyaltyCard.Bonuses(
             bonusCount = bonusCount ?: 0,
             expectedBonusCount = expectedBonusCount ?: 0,
         )
         return LoyaltyCard(
             number = LoyaltyCard.Number(number),
-            level = level.toLoyaltyCardLevel(),
+            currentLevel = currentLevel.toLoyaltyCardLevel(),
+            currentLevelName = currentLevelDescription,
             nextLevelInfo = nextLevelInfo,
             bonuses = bonuses,
             totalPurchaseSum = totalPurchaseSum ?: 0,
