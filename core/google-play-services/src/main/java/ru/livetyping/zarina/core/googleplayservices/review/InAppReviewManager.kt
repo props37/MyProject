1595151934e9be8a102509a07918dfd1b74f5ada
Artem Sidorov <sidorovaa@melonfashion.com>
2025-04-23 18:24:15 +0100

    Implement quota for InAppReviewManager::launchReviewFlow method

--- core/google-play-services/src/main/java/ru/livetyping/zarina/core/googleplayservices/review/InAppReviewManager.kt
+++ core/google-play-services/src/main/java/ru/livetyping/zarina/core/googleplayservices/review/InAppReviewManager.kt
@@ -4,9 +4,13 @@ import android.app.Activity
 import android.content.Context
 import ru.livetyping.zarina.core.analytics.AppMetrica
 import ru.livetyping.zarina.core.googleplayservices.impl.review.InAppReviewManagerImpl
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.days
 
 public interface InAppReviewManager {
-    public suspend fun launchReviewFlow()
+    public suspend fun launchReviewFlow(
+        minTimeSinceLastLaunch: Duration = MIN_TIME_BETWEEN_REVIEW_FLOW_LAUNCHES_IN_DAYS.days,
+    )
 
     public fun setActivity(activity: Activity)
 
@@ -15,6 +19,8 @@ public interface InAppReviewManager {
     public fun release()
 
     public companion object {
+        public const val MIN_TIME_BETWEEN_REVIEW_FLOW_LAUNCHES_IN_DAYS: Int = 30
+
         public fun createInstance(
             context: Context,
             appMetrica: AppMetrica?,
