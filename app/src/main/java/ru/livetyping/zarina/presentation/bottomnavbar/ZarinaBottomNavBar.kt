96969c6648f8b635e8d6cc45f6493e723da74576
Artem Sidorov <sidorovaa@melonfashion.com>
2025-04-25 13:42:48 +0100

    ZRWEB-2252: Refactor ZarinaBottomBar
    Simplify the component and move animation logic to the place where the component is used

--- app/src/main/java/ru/livetyping/zarina/presentation/bottomnavbar/ZarinaBottomNavBar.kt
+++ app/src/main/java/ru/livetyping/zarina/presentation/bottomnavbar/ZarinaBottomNavBar.kt
@@ -1,11 +1,32 @@
 package ru.livetyping.zarina.presentation.bottomnavbar
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.SpringSpec
+import androidx.compose.animation.core.VisibilityThreshold
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavDestination.Companion.hasRoute
@@ -13,6 +34,9 @@ import androidx.navigation.NavHostController
 import ru.livetyping.zarina.core.uikit.bottombar.ZarinaBottomBar
 import ru.livetyping.zarina.core.uikit.bottombar.ZarinaBottomBarDefaults
 import ru.livetyping.zarina.core.uikit.bottombar.ZarinaBottomBarItem
+import ru.livetyping.zarina.core.uikit.bottombar.navigation.behavior.BottomNavBarBehavior
+import ru.livetyping.zarina.core.uikit.bottombar.navigation.behavior.LocalBottomNavBarBehaviorController
+import ru.livetyping.zarina.core.uikit.bottombar.navigation.sizetracker.LocalBottomNavBarSizeTracker
 
 @Composable
 fun ZarinaBottomNavBar(
@@ -22,7 +46,7 @@ fun ZarinaBottomNavBar(
     modifier: Modifier = Modifier,
     windowInsets: WindowInsets = ZarinaBottomBarDefaults.WindowInsets,
 ) {
-    ZarinaBottomBar(
+    AnimatedZarinaBottomBar(
         windowInsets = windowInsets,
         modifier = modifier,
     ) {
@@ -59,6 +83,72 @@ fun ZarinaBottomNavBar(
     }
 }
 
+@Composable
+private fun AnimatedZarinaBottomBar(
+    modifier: Modifier = Modifier,
+    windowInsets: WindowInsets = ZarinaBottomBarDefaults.WindowInsets,
+    content: @Composable RowScope.() -> Unit,
+) {
+    val behaviorController = LocalBottomNavBarBehaviorController.current
+    val behavior by behaviorController.currentBehavior.collectAsStateWithLifecycle()
+    val isBottomNavBarVisible = behavior is BottomNavBarBehavior.Visible
+    val isBottomNavBarAnimated = behavior.isAnimated
+
+    val sizeTracker = LocalBottomNavBarSizeTracker.current
+
+    val visibleState = remember { MutableTransitionState(isBottomNavBarVisible) }
+    DisposableEffect(isBottomNavBarVisible) {
+        visibleState.targetState = isBottomNavBarVisible
+        onDispose {}
+    }
+
+    LaunchedEffect(visibleState, sizeTracker) {
+        snapshotFlow { visibleState.currentState }.collect { isVisible ->
+            if (!isVisible) {
+                sizeTracker.onSizeChanged(IntSize.Zero)
+            }
+        }
+    }
+
+    val enterTransition = remember { expandVertically(BottomBarAnimationSpec) }
+    val exitTransition = remember { shrinkVertically(BottomBarAnimationSpec) }
+
+    val contentEnterTransition = remember {
+        slideInVertically(BottomBarContentAnimationSpec) { it }
+    }
+    val contentExitTransition = remember {
+        slideOutVertically(BottomBarContentAnimationSpec) { it }
+    }
+
+    AnimatedVisibility(
+        visibleState = visibleState,
+        enter = if (isBottomNavBarAnimated) enterTransition else EnterTransition.None,
+        exit = if (isBottomNavBarAnimated) exitTransition else ExitTransition.None,
+        modifier = modifier
+            .onSizeChanged { size ->
+                sizeTracker.onSizeChanged(size)
+            },
+    ) {
+        ZarinaBottomBar(
+            windowInsets = windowInsets,
+            content = content,
+            modifier = Modifier
+                .animateEnterExit(
+                    enter = if (isBottomNavBarAnimated) {
+                        contentEnterTransition
+                    } else {
+                        EnterTransition.None
+                    },
+                    exit = if (isBottomNavBarAnimated) {
+                        contentExitTransition
+                    } else {
+                        ExitTransition.None
+                    },
+                ),
+        )
+    }
+}
+
 private fun isItemSelected(
     bottomNavItem: BottomNavBarItem,
     backStack: List<NavBackStackEntry>,
@@ -73,3 +163,17 @@ private fun isItemSelected(
     return lastBottomNavBarItemBackStackEntry?.destination
         ?.hasRoute(bottomNavBarItemFeatureEntry::class) ?: false
 }
+
+private const val BottomBarAnimationSpringStiffness = Spring.StiffnessMedium
+
+@Stable
+internal val BottomBarAnimationSpec: SpringSpec<IntSize> = spring(
+    stiffness = BottomBarAnimationSpringStiffness,
+    visibilityThreshold = IntSize.VisibilityThreshold,
+)
+
+@Stable
+internal val BottomBarContentAnimationSpec: SpringSpec<IntOffset> = spring(
+    stiffness = BottomBarAnimationSpringStiffness,
+    visibilityThreshold = IntOffset.VisibilityThreshold,
+)
--- core/ui-kit/src/main/java/ru/livetyping/zarina/core/uikit/bottombar/ZarinaBottomBar.kt
+++ core/ui-kit/src/main/java/ru/livetyping/zarina/core/uikit/bottombar/ZarinaBottomBar.kt
@@ -1,17 +1,5 @@
 package ru.livetyping.zarina.core.uikit.bottombar
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
-import androidx.compose.animation.core.MutableTransitionState
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.SpringSpec
-import androidx.compose.animation.core.VisibilityThreshold
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
@@ -27,25 +15,12 @@ import androidx.compose.foundation.layout.union
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.Stable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import ru.livetyping.zarina.core.uikit.bottombar.navigation.behavior.BottomNavBarBehavior
-import ru.livetyping.zarina.core.uikit.bottombar.navigation.behavior.LocalBottomNavBarBehaviorController
-import ru.livetyping.zarina.core.uikit.bottombar.navigation.sizetracker.LocalBottomNavBarSizeTracker
 import ru.livetyping.zarina.core.uikit.divider.ZarinaDividerDefaults
 import ru.livetyping.zarina.core.uikit.theme.UiKitTheme
 
@@ -55,89 +30,31 @@ public fun ZarinaBottomBar(
     windowInsets: WindowInsets = ZarinaBottomBarDefaults.WindowInsets,
     content: @Composable RowScope.() -> Unit,
 ) {
-    val behaviorController = LocalBottomNavBarBehaviorController.current
-    val behavior by behaviorController.currentBehavior.collectAsStateWithLifecycle()
-    val isBottomNavBarVisible = behavior is BottomNavBarBehavior.Visible
-    val isBottomNavBarAnimated = behavior.isAnimated
+    val backgroundColor = UiKitTheme.colors.background.general.regular.default
+    val topBorderColor = UiKitTheme.colors.border.general.default
 
-    val sizeTracker = LocalBottomNavBarSizeTracker.current
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+            .drawBehind {
+                drawRect(backgroundColor)
 
-    val visibleState = remember { MutableTransitionState(isBottomNavBarVisible) }
-    DisposableEffect(isBottomNavBarVisible) {
-        visibleState.targetState = isBottomNavBarVisible
-        onDispose {}
-    }
-
-    LaunchedEffect(visibleState, sizeTracker) {
-        snapshotFlow { visibleState.currentState }.collect { isVisible ->
-            if (!isVisible) {
-                sizeTracker.onSizeChanged(IntSize.Zero)
-            }
-        }
-    }
-
-    AnimatedVisibility(
-        visibleState = visibleState,
-        enter = remember(isBottomNavBarAnimated) {
-            if (isBottomNavBarAnimated) {
-                expandVertically(ZarinaBottomBarDefaults.BottomNavBarAnimationSpec)
-            } else {
-                EnterTransition.None
-            }
-        },
-        exit = remember(isBottomNavBarAnimated) {
-            if (isBottomNavBarAnimated) {
-                shrinkVertically(ZarinaBottomBarDefaults.BottomNavBarAnimationSpec)
-            } else {
-                ExitTransition.None
-            }
-        },
-        modifier = modifier.onSizeChanged { size ->
-            sizeTracker.onSizeChanged(size)
-        },
-    ) {
-        val backgroundColor = UiKitTheme.colors.background.general.regular.default
-        val topBorderColor = UiKitTheme.colors.border.general.default
-
-        Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .fillMaxWidth()
-                .heightIn(min = 56.dp)
-                .animateEnterExit(
-                    enter = remember(isBottomNavBarAnimated) {
-                        if (isBottomNavBarAnimated) {
-                            slideInVertically(ZarinaBottomBarDefaults.BottomNavBarContentAnimationSpec) { it }
-                        } else {
-                            EnterTransition.None
-                        }
-                    },
-                    exit = remember(isBottomNavBarAnimated) {
-                        if (isBottomNavBarAnimated) {
-                            slideOutVertically(ZarinaBottomBarDefaults.BottomNavBarContentAnimationSpec) { it }
-                        } else {
-                            ExitTransition.None
-                        }
-                    },
+                drawLine(
+                    color = topBorderColor,
+                    start = Offset.Zero,
+                    end = Offset(size.width, 0f),
+                    strokeWidth = ZarinaDividerDefaults.Thickness.toPx(),
                 )
-                .drawBehind {
-                    drawRect(backgroundColor)
-
-                    drawLine(
-                        color = topBorderColor,
-                        start = Offset.Zero,
-                        end = Offset(size.width, 0f),
-                        strokeWidth = ZarinaDividerDefaults.Thickness.toPx(),
-                    )
-                }
-                .selectableGroup()
-                .windowInsetsPadding(windowInsets)
-                .clipToBounds()
-                .padding(top = 6.dp, bottom = 4.dp),
-            content = content,
-        )
-    }
+            }
+            .selectableGroup()
+            .windowInsetsPadding(windowInsets)
+            .clipToBounds()
+            .padding(top = 6.dp, bottom = 4.dp),
+        content = content,
+    )
 }
 
 public object ZarinaBottomBarDefaults {
@@ -146,18 +63,4 @@ public object ZarinaBottomBarDefaults {
         get() = androidx.compose.foundation.layout.WindowInsets.navigationBars
             .union(androidx.compose.foundation.layout.WindowInsets.displayCutout)
             .only(WindowInsetsSides.Horizontal + WindowInsetsSides.Bottom)
-
-    private const val BottomNavBarAnimationSpringStiffness = Spring.StiffnessMedium
-
-    @Stable
-    internal val BottomNavBarAnimationSpec: SpringSpec<IntSize> = spring(
-        stiffness = BottomNavBarAnimationSpringStiffness,
-        visibilityThreshold = IntSize.VisibilityThreshold,
-    )
-
-    @Stable
-    internal val BottomNavBarContentAnimationSpec: SpringSpec<IntOffset> = spring(
-        stiffness = BottomNavBarAnimationSpringStiffness,
-        visibilityThreshold = IntOffset.VisibilityThreshold,
-    )
 }
