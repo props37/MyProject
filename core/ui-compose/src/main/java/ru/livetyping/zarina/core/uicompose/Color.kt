f5b11f9258abce48acd035b700656ef579dfee48
Artem Sidorov <sidorovaa@melonfashion.com>
2025-04-29 18:37:15 +0100

    ZRWEB-2252: Refactor Color::toComposeColor extension

--- core/ui-compose/src/main/java/ru/livetyping/zarina/core/uicompose/Color.kt
+++ core/ui-compose/src/main/java/ru/livetyping/zarina/core/uicompose/Color.kt
@@ -2,13 +2,14 @@ package ru.livetyping.zarina.core.uicompose
 
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.Color
+import androidx.core.graphics.toColorInt
 import timber.log.Timber
 import ru.livetyping.zarina.core.domain.model.common.Color as ColorDomain
 
 @Stable
 public fun ColorDomain.toComposeColor(): Color? {
     return try {
-        val intCode = android.graphics.Color.parseColor(this.value)
+        val intCode = this.value.toColorInt()
         Color(intCode)
     } catch (e: Exception) {
         Timber.tag(Tag).e(e, "Failed to convert domain color $this to Compose color")
