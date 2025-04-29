0fb947b1e3f04f02d96718a555b1ecc7ee1260a2
Artem Sidorov <sidorovaa@melonfashion.com>
2025-04-29 12:45:09 +0100

    ZRWEB-2252: Add GetCatalogMenuUseCase

--- app/src/main/java/ru/livetyping/zarina/di/UseCaseModule.kt
+++ app/src/main/java/ru/livetyping/zarina/di/UseCaseModule.kt
@@ -37,6 +37,7 @@ import ru.livetyping.zarina.core.domain.usecase.cart.RedeemBonusesUseCase
 import ru.livetyping.zarina.core.domain.usecase.cart.RemoveProductFromCartUseCase
 import ru.livetyping.zarina.core.domain.usecase.cart.WithdrawMyCardUseCase
 import ru.livetyping.zarina.core.domain.usecase.cart.WithdrawPromoCodeUseCase
+import ru.livetyping.zarina.core.domain.usecase.catalog.GetCatalogMenuUseCase
 import ru.livetyping.zarina.core.domain.usecase.category.GetCategoriesFlowUseCase
 import ru.livetyping.zarina.core.domain.usecase.category.GetCategoryFlowUseCase
 import ru.livetyping.zarina.core.domain.usecase.category.GetCategoryPathUseCase
@@ -1228,6 +1229,17 @@ internal class UseCaseModule {
     ): GetInAppReviewRequestFlowUseCase {
         return GetInAppReviewRequestFlowUseCase.getInstance(
             userRepository = userRepository,
+            logger = logger,
+        )
+    }
+
+    @Provides
+    fun provideGetCatalogMenuUseCase(
+        contentRepository: ContentRepository,
+        logger: UseCaseLogger,
+    ): GetCatalogMenuUseCase {
+        return GetCatalogMenuUseCase.getInstance(
+            contentRepository = contentRepository,
             logger = logger,
         )
     }
