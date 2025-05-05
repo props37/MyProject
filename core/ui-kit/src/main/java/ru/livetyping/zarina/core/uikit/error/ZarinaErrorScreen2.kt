2367242ba176225524cc0aab82dbe558057a2e61
Artem Sidorov <sidorovaa@melonfashion.com>
2025-05-05 19:57:15 +0100

    ZRWEB-2252: Add ZarinaErrorScreen2

---
+++ core/ui-kit/src/main/java/ru/livetyping/zarina/core/uikit/error/ZarinaErrorScreen2.kt
@@ -0,0 +1,91 @@
+package ru.livetyping.zarina.core.uikit.error
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.livetyping.zarina.core.uicompose.textString
+import ru.livetyping.zarina.core.uikit.button.ZarinaButton
+import ru.livetyping.zarina.core.uikit.theme.UiKitTheme2
+import ru.livetyping.zarina.core.uikit.theme.ZarinaTheme2
+
+// TODO: [Top] Add video
+// TODO: [Top] Migrate to custom layout
+
+@Composable
+public fun ZarinaErrorScreen2(
+    state: ZarinaErrorScreenState2,
+    onButtonClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier,
+    ) {
+        Box(modifier = Modifier.weight(1f))
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(
+                text = textString(state.title).uppercase(),
+                style = UiKitTheme2.typography.h2Regular,
+                color = UiKitTheme2.colors.mainBlack,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(horizontal = 16.dp),
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = textString(state.body).uppercase(),
+                style = UiKitTheme2.typography.body,
+                color = UiKitTheme2.colors.mainBlack,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(horizontal = 16.dp),
+            )
+
+            if (state.isButtonVisible) {
+                Spacer(modifier = Modifier.height(32.dp))
+
+                ZarinaButton(
+                    onClick = onButtonClick,
+                    contentPadding = PaddingValues(horizontal = 60.dp, vertical = 8.dp),
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                ) {
+                    Text(text = textString(state.buttonText).uppercase())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun Preview() {
+    ZarinaTheme2 {
+        ZarinaErrorScreen2(
+            state = ZarinaErrorScreenState2.NETWORK,
+            onButtonClick = {},
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
+        )
+    }
+}
---
+++ core/ui-kit/src/main/java/ru/livetyping/zarina/core/uikit/error/ZarinaErrorScreenState2.kt
@@ -0,0 +1,39 @@
+package ru.livetyping.zarina.core.uikit.error
+
+import androidx.compose.runtime.Immutable
+import ru.livetyping.zarina.core.resource.R
+import ru.livetyping.zarina.core.text.Text
+import java.io.IOException
+
+@Immutable
+public data class ZarinaErrorScreenState2(
+    val title: Text,
+    val body: Text,
+    val isButtonVisible: Boolean,
+    val buttonText: Text,
+) {
+    public companion object {
+        public val NETWORK: ZarinaErrorScreenState2
+            get() = ZarinaErrorScreenState2(
+                title = Text.Resource(R.string.res_something_went_wrong),
+                body = Text.Resource(R.string.res_internet_connection_error_guide),
+                isButtonVisible = true,
+                buttonText = Text.Resource(R.string.res_refresh),
+            )
+
+        public val GENERIC: ZarinaErrorScreenState2
+            get() = ZarinaErrorScreenState2(
+                title = Text.Resource(R.string.res_something_went_wrong),
+                body = Text.Resource(R.string.res_generic_error_guide),
+                isButtonVisible = true,
+                buttonText = Text.Resource(R.string.res_refresh),
+            )
+
+        public fun from(throwable: Throwable): ZarinaErrorScreenState2 {
+            return when (throwable) {
+                is IOException -> NETWORK
+                else -> GENERIC
+            }
+        }
+    }
+}
