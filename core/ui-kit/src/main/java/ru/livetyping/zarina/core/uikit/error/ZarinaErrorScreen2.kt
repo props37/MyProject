9d1b5191093859b7ec195060f7ede0a6d5fe64e2
Artem Sidorov <sidorovaa@melonfashion.com>
2025-05-06 10:37:35 +0100

    ZRWEB-2252: Add video to ZarinaErrorScreen2

--- core/ui-kit/src/main/java/ru/livetyping/zarina/core/uikit/error/ZarinaErrorScreen2.kt
+++ core/ui-kit/src/main/java/ru/livetyping/zarina/core/uikit/error/ZarinaErrorScreen2.kt
@@ -1,13 +1,11 @@
 package ru.livetyping.zarina.core.uikit.error
 
-import androidx.compose.foundation.background
+import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -16,18 +14,20 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.ui.compose.SURFACE_TYPE_TEXTURE_VIEW
+import ru.livetyping.zarina.core.mediacompose.SimpleVideoPlayer
 import ru.livetyping.zarina.core.uicompose.textString
 import ru.livetyping.zarina.core.uikit.button.ZarinaButton
 import ru.livetyping.zarina.core.uikit.theme.UiKitTheme2
-import ru.livetyping.zarina.core.uikit.theme.ZarinaTheme2
+import ru.livetyping.zarina.core.resource.R as RCommon
 
-// TODO: [Top] Add video
 // TODO: [Top] Rename after full migration
 
+@OptIn(UnstableApi::class)
 @Composable
 public fun ZarinaErrorScreen2(
     state: ZarinaErrorScreenState2,
@@ -45,12 +45,13 @@ public fun ZarinaErrorScreen2(
             Spacer(modifier = Modifier.height(topPadding))
         }
 
-        // TODO: [Top] Add video player
-        Box(
+        SimpleVideoPlayer(
+            resId = RCommon.raw.zarina_error_screen_video,
+            contentScale = ContentScale.Fit,
+            surfaceType = SURFACE_TYPE_TEXTURE_VIEW,
             modifier = Modifier
                 .weight(1f)
-                .fillMaxWidth()
-                .background(Color.Red),
+                .fillMaxWidth(),
         )
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -96,19 +97,5 @@ public fun ZarinaErrorScreen2(
                     .fillMaxHeight(0.2f)
             )
         }
-    }
-}
-
-@Composable
-@Preview
-private fun Preview() {
-    ZarinaTheme2 {
-        ZarinaErrorScreen2(
-            state = ZarinaErrorScreenState2.NETWORK,
-            onButtonClick = {},
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White),
-        )
     }
 }
