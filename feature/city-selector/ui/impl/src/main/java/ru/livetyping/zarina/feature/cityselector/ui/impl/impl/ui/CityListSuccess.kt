39158cd506f990b85abfebfc715f588ee63cd5d6
Artem Sidorov <sidorovaa@melonfashion.com>
2025-05-13 17:46:38 +0100

    ZRWEB-2260: Scroll CitySelector list to the top when the list changes

--- feature/city-selector/ui/impl/src/main/java/ru/livetyping/zarina/feature/cityselector/ui/impl/impl/ui/CityListSuccess.kt
+++ feature/city-selector/ui/impl/src/main/java/ru/livetyping/zarina/feature/cityselector/ui/impl/impl/ui/CityListSuccess.kt
@@ -8,8 +8,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -33,6 +35,8 @@ internal fun CityListSuccess(
     modifier: Modifier = Modifier,
 ) {
     Box(modifier = modifier.fillMaxSize()) {
+        val lazyListState = rememberLazyListState()
+
         val selectCityButtonHeight = if (state.isSelectCityButtonVisible) {
             ZarinaButtonDefaults.SizeMedium + SelectCityBottomPadding
         } else {
@@ -41,7 +45,13 @@ internal fun CityListSuccess(
         val listBottomPadding =
             bottomPadding + ZarinaScrollableDefaults.ScrollableBottomPadding + selectCityButtonHeight
 
+        DisposableEffect(state.cities) {
+            lazyListState.requestScrollToItem(0)
+            onDispose {}
+        }
+
         LazyColumn(
+            state = lazyListState,
             contentPadding = PaddingValues(
                 top = topPadding,
                 bottom = listBottomPadding,
