493921d8865660bbf1b9bfeaa8dce860f351eebc
Artem Sidorov <sidorovaa@melonfashion.com>
2025-05-14 14:17:04 +0100

    ZRWEB-2307: Optimize imports

--- feature/home/ui/impl/src/main/java/ru/livetyping/zarina/feature/home/ui/impl/impl/ui/Banner.kt
+++ feature/home/ui/impl/src/main/java/ru/livetyping/zarina/feature/home/ui/impl/impl/ui/Banner.kt
@@ -7,14 +7,12 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import ru.livetyping.zarina.core.uikit.bottombar.navigation.bottomNavBarHeightAsState
 import ru.livetyping.zarina.core.uikit.screen.ZarinaLogoLoadingScreen
 import ru.livetyping.zarina.core.uikit.theme.UiKitTheme
 import ru.livetyping.zarina.feature.home.domain.model.Banner
