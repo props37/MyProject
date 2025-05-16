1ac8b1608a4d789260a57448b3712f1be0d21cf9
Artem Sidorov <sidorovaa@melonfashion.com>
2025-05-16 16:44:22 +0100

    ZRWEB-2289: Update ProductList screen top bar

--- feature/product-list/ui/impl/src/main/java/ru/livetyping/zarina/feature/productlist/ui/impl/impl/productlist/ui/TopBar.kt
+++ feature/product-list/ui/impl/src/main/java/ru/livetyping/zarina/feature/productlist/ui/impl/impl/productlist/ui/TopBar.kt
@@ -1,35 +1,22 @@
 package ru.livetyping.zarina.feature.productlist.ui.impl.impl.productlist.ui
 
-import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.material.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import ru.livetyping.zarina.core.uicompose.AnimatedContentCrossfadeTransitionSpec
 import ru.livetyping.zarina.core.uikit.button.ZarinaBackIconButton
-import ru.livetyping.zarina.core.uikit.button.ZarinaFilterIconButton
-import ru.livetyping.zarina.core.uikit.button.ZarinaIconButton
 import ru.livetyping.zarina.core.uikit.skeleton.ZarinaTextSkeleton
-import ru.livetyping.zarina.core.uikit.theme.UiKitTheme
+import ru.livetyping.zarina.core.uikit.theme.UiKitTheme2
 import ru.livetyping.zarina.core.uikit.topbar.ZarinaTopBar
 import ru.livetyping.zarina.feature.productlist.ui.impl.impl.productlist.model.TopBarEvent
 import ru.livetyping.zarina.feature.productlist.ui.impl.impl.productlist.model.TopBarState
-import ru.livetyping.zarina.core.resource.R as RCommon
 
 @Composable
 internal fun TopBar(
@@ -39,63 +26,41 @@ internal fun TopBar(
 ) {
     ZarinaTopBar(
         contentPadding = PaddingValues(vertical = 4.dp),
-        modifier = modifier,
-    ) {
-        ZarinaBackIconButton(
-            onClick = { onEvent(TopBarEvent.BackClicked) },
-            iconSize = IconSize,
-            modifier = Modifier.padding(start = 2.dp),
-        )
-
-        Spacer(modifier = Modifier.width(4.dp))
-
-        AnimatedContent(
-            targetState = state.categoryName,
-            transitionSpec = { AnimatedContentCrossfadeTransitionSpec },
-            contentAlignment = Alignment.CenterStart,
-            label = "TopBar category name",
-            modifier = Modifier.weight(1f),
-        ) { categoryName ->
-            val textStyle = UiKitTheme.typography.primary.regular
-            if (categoryName != null) {
-                Text(
-                    text = categoryName,
-                    style = textStyle,
-                    color = UiKitTheme.colors.text.general.regular.default,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                )
-            } else {
-                ZarinaTextSkeleton(
-                    textStyle = textStyle,
-                    modifier = Modifier
-                        .wrapContentWidth(align = Alignment.Start)
-                        .fillMaxWidth(fraction = 0.5f),
-                )
-            }
-        }
-
-        Spacer(modifier = Modifier.width(4.dp))
-
-        ZarinaIconButton(
-            onClick = { onEvent(TopBarEvent.SearchClicked) },
-            indication = ripple(bounded = false, radius = IconSize),
-        ) {
-            Icon(
-                imageVector = ImageVector.vectorResource(RCommon.drawable.ic_magnifying_glass_24),
-                contentDescription = stringResource(RCommon.string.res_search_noun),
-                tint = UiKitTheme.colors.icon.regular.default,
-                modifier = Modifier.size(IconSize),
+        startContent = {
+            ZarinaBackIconButton(
+                onClick = { onEvent(TopBarEvent.BackClicked) },
+                modifier = Modifier.padding(start = 2.dp),
             )
-        }
+        },
+        centerContent = {
+            Crossfade(
+                targetState = state.categoryName,
+                label = "TopBar category name",
+                modifier = Modifier.fillMaxWidth(),
+            ) { categoryName ->
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxWidth(),
+                ) {
+                    val textStyle = UiKitTheme2.typography.h3
 
-        ZarinaFilterIconButton(
-            onClick = { onEvent(TopBarEvent.FiltersClicked) },
-            appliedFilterCount = state.appliedFilterCount,
-            iconSize = 20.dp,
-            modifier = Modifier.padding(end = 2.dp),
-        )
-    }
+                    if (categoryName != null) {
+                        Text(
+                            text = categoryName.uppercase(),
+                            style = textStyle,
+                            color = UiKitTheme2.colors.mainBlack,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                        )
+                    } else {
+                        ZarinaTextSkeleton(
+                            textStyle = textStyle,
+                            modifier = Modifier.fillMaxWidth(0.6f),
+                        )
+                    }
+                }
+            }
+        },
+        modifier = modifier,
+    )
 }
-
-private val IconSize: Dp get() = 20.dp
