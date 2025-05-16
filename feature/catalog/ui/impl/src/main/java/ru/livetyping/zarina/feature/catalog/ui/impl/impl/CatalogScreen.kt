6495d4f923333d3b0bdaf17693a04a5e30600369
Artem Sidorov <sidorovaa@melonfashion.com>
2025-05-16 17:08:47 +0100

    Track when Catalog screen opened to AppMetrica

--- feature/catalog/ui/impl/src/main/java/ru/livetyping/zarina/feature/catalog/ui/impl/impl/CatalogScreen.kt
+++ feature/catalog/ui/impl/src/main/java/ru/livetyping/zarina/feature/catalog/ui/impl/impl/CatalogScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.Flow
+import ru.livetyping.zarina.core.uicompose.LifecycleEventEffect
 import ru.livetyping.zarina.core.uicompose.collapsingtopbar.CollapsingTopBarDefaults
 import ru.livetyping.zarina.core.uicompose.collapsingtopbar.CollapsingTopBarLayout
 import ru.livetyping.zarina.core.uikit.bottombar.navigation.bottomNavBarHeightAsState
@@ -37,6 +38,8 @@ internal fun CatalogScreen(
     val catalogState by viewModel.catalogState.collectAsStateWithLifecycle()
 
     BackHandler(onBack = { viewModel.onCatalogEvent(CatalogEvent.BackClicked) })
+
+    LifecycleEventEffect(onLifecycleEvent = viewModel::onLifecycleEvent)
 
     ScreenContent(
         catalogState = catalogState,
