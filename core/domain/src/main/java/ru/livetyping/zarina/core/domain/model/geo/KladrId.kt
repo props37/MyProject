2b94380af2e1416be887106e9e761657cc97c2bf
Artem Sidorov <sidorovaa@melonfashion.com>
2025-05-29 21:57:46 +0100

    Update KladrId.MOSCOW and SAINT_PETERSBURG values

--- core/domain/src/main/java/ru/livetyping/zarina/core/domain/model/geo/KladrId.kt
+++ core/domain/src/main/java/ru/livetyping/zarina/core/domain/model/geo/KladrId.kt
@@ -1,5 +1,6 @@
 package ru.livetyping.zarina.core.domain.model.geo
 
+// TODO: [Top] Rename to FiasId
 // Marked as stable on config/compose/stability_config.txt
 /**
  * According to "КЛАДР РФ"
@@ -8,9 +9,9 @@ package ru.livetyping.zarina.core.domain.model.geo
 public value class KladrId(public val value: String) {
     public companion object {
         public val MOSCOW: KladrId
-            get() = KladrId("7700000000000")
+            get() = KladrId("0c5b2444-70a0-4932-980c-b4dc0d3f02b5")
 
         public val SAINT_PETERSBURG: KladrId
-            get() = KladrId("7800000000000")
+            get() = KladrId("c2deb16a-0330-4f05-821f-1d09c93331e6")
     }
 }
