8f77d77d121f7caeb4b91ada85cdae873c0b06ff
Artem Sidorov <sidorovaa@melonfashion.com>
2025-06-03 13:17:19 +0100

    ZRWEB-2289: Clean Product screen

--- feature/product/ui/impl/src/main/java/ru/livetyping/zarina/feature/product/ui/impl/impl/product/model/ProductSuggestionsEvent.kt
+++
@@ -1,9 +0,0 @@
-package ru.livetyping.zarina.feature.product.ui.impl.impl.product.model
-
-import ru.livetyping.zarina.core.domain.model.product.Product
-
-internal sealed interface ProductSuggestionsEvent {
-    data class ProductClicked(val product: Product) : ProductSuggestionsEvent
-
-    data object ErrorRefreshClicked : ProductSuggestionsEvent
-}
--- feature/product/ui/impl/src/main/java/ru/livetyping/zarina/feature/product/ui/impl/impl/product/model/TopBarEvent.kt
+++
@@ -1,7 +0,0 @@
-package ru.livetyping.zarina.feature.product.ui.impl.impl.product.model
-
-internal sealed interface TopBarEvent {
-    data object BackClicked : TopBarEvent
-
-    data object ShareClicked : TopBarEvent
-}
