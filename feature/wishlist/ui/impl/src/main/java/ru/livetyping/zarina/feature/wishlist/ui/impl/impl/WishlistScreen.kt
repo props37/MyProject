1e0cd98b8c98f4dc109ddc0a5844352330a1871c
Artem Sidorov <sidorovaa@melonfashion.com>
2025-06-10 17:55:32 +0100

    Fix Wishlist top bar counter

--- feature/wishlist/ui/impl/src/main/java/ru/livetyping/zarina/feature/wishlist/ui/impl/impl/WishlistScreen.kt
+++ feature/wishlist/ui/impl/src/main/java/ru/livetyping/zarina/feature/wishlist/ui/impl/impl/WishlistScreen.kt
@@ -96,7 +96,7 @@ private fun ScreenContent(
                 val alpha by animateFloatAsState(if (productPagingItems.itemCount > 0) 1f else 0f)
 
                 TopBar(
-                    productCount = wishlistState.productCount,
+                    productCount = productPagingItems.itemCount,
                     alphaProvider = { alpha },
                 )
             },
