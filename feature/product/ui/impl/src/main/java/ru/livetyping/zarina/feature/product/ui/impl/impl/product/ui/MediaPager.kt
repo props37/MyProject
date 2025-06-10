126ecf4d5d34ac46877921e4e04ded2555c84768
Artem Sidorov <sidorovaa@melonfashion.com>
2025-06-10 16:05:09 +0100

    ZRWEB-2289: Simplify MediaPager function

--- feature/product/ui/impl/src/main/java/ru/livetyping/zarina/feature/product/ui/impl/impl/product/ui/MediaPager.kt
+++ feature/product/ui/impl/src/main/java/ru/livetyping/zarina/feature/product/ui/impl/impl/product/ui/MediaPager.kt
@@ -1,32 +1,24 @@
 package ru.livetyping.zarina.feature.product.ui.impl.impl.product.ui
 
-import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import ru.livetyping.zarina.core.domain.model.media.Media
-import ru.livetyping.zarina.core.uicompose.pager.EndlessPagerStateUtils
 import ru.livetyping.zarina.core.uicompose.pager.rememberEndlessPagerState
-import ru.livetyping.zarina.core.uikit.skeleton.rememberZarinaSkeletonShimmer
+import ru.livetyping.zarina.core.uikit.media.ZarinaMediaHorizontalPager
+import ru.livetyping.zarina.core.uikit.product.ProductDefaults
 
 @Composable
 internal fun MediaPager(
     mediaList: List<Media>,
     modifier: Modifier = Modifier,
 ) {
-    val pagerState = rememberEndlessPagerState(itemCount = mediaList.size)
-    val shimmer = rememberZarinaSkeletonShimmer()
-
-    HorizontalPager(
-        state = pagerState,
-        modifier = modifier,
-    ) { page ->
-        val media = EndlessPagerStateUtils.getLooping(mediaList, page)
-
-        if (media != null) {
-            Media(
-                media = media,
-                shimmer = shimmer,
-            )
-        }
+    Box(modifier = modifier.aspectRatio(ProductDefaults.MediaAspectRatio)) {
+        ZarinaMediaHorizontalPager(
+            pagerState = rememberEndlessPagerState(itemCount = mediaList.size),
+            media = mediaList,
+            modifier = Modifier.matchParentSize(),
+        )
     }
 }
