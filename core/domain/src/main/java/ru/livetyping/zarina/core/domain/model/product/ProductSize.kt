207fac3143589ac94664d8710df3b0dc992f841d
Artem Sidorov <sidorovaa@melonfashion.com>
2025-06-14 12:15:34 +0100

    ZRWEB-2289: Introduce ProductSize value classes and ProductHeight value class

---
+++ core/domain/src/main/java/ru/livetyping/zarina/core/domain/model/product/ProductSize.kt
@@ -0,0 +1,10 @@
+package ru.livetyping.zarina.core.domain.model.product
+
+@JvmInline
+public value class ProductSize(public val value: String)
+
+@JvmInline
+public value class ProductSizeEn(public val value: String)
+
+@JvmInline
+public value class ProductSizeRu(public val value: String)
