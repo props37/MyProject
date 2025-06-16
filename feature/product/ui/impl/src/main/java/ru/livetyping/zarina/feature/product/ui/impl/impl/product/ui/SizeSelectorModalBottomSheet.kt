4ee87c2d7858253ddd937f2201f24ee0e6d24faa
Artem Sidorov <sidorovaa@melonfashion.com>
2025-06-16 10:25:11 +0100

    ZRWEB-2289: Add Not Available label to SizeSelectorModalBottomSheet items

--- feature/product/ui/impl/src/main/java/ru/livetyping/zarina/feature/product/ui/impl/impl/product/ui/SizeSelectorModalBottomSheet.kt
+++ feature/product/ui/impl/src/main/java/ru/livetyping/zarina/feature/product/ui/impl/impl/product/ui/SizeSelectorModalBottomSheet.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -15,7 +18,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import ru.livetyping.zarina.core.domain.model.product.ProductOffer
 import ru.livetyping.zarina.core.uicompose.none
@@ -118,8 +123,6 @@ private fun Item(
         ),
         modifier = modifier,
     ) {
-        val textStyle = UiKitTheme2.typography.body
-
         val text = when (type) {
             SizeSelectorType.SIZE -> item.offer.size.size.uppercase()
             SizeSelectorType.HEIGHT -> item.offer.height?.height.orEmpty()
@@ -129,8 +132,28 @@ private fun Item(
 
         Text(
             text = text,
-            style = textStyle,
+            style = UiKitTheme2.typography.body,
             color = color,
+            modifier = Modifier.weight(1f),
         )
+
+        if (!item.offer.isAvailable) {
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Text(
+                text = stringResource(RCommon.string.res_not_available).uppercase(),
+                style = UiKitTheme2.typography.body2,
+                color = UiKitTheme2.colors.mainBlack,
+            )
+
+            Spacer(modifier = Modifier.width(4.dp))
+
+            Icon(
+                imageVector = ImageVector.vectorResource(RCommon.drawable.ic_bell_24),
+                contentDescription = null,
+                tint = UiKitTheme2.colors.mainBlack,
+                modifier = Modifier.size(12.dp),
+            )
+        }
     }
 }
