cbd09d3f46a9e58666dae9628c003b1596e1a564
Artem Sidorov <sidorovaa@melonfashion.com>
2025-06-17 13:23:47 +0100

    ZRWEB-2568: Send app version in headers

--- core/network/src/main/java/ru/livetyping/zarina/core/network/impl/ZarinaApiHeaderProvider.kt
+++ core/network/src/main/java/ru/livetyping/zarina/core/network/impl/ZarinaApiHeaderProvider.kt
@@ -1,14 +1,16 @@
 package ru.livetyping.zarina.core.network.impl
 
-import javax.inject.Inject
-
-internal class ZarinaApiHeaderProvider @Inject constructor() {
+internal class ZarinaApiHeaderProvider(private val appVersionName: String) {
     fun provide(): Map<String, String> {
-        return mapOf(KEY_CLIENT_SOURCE to VALUE_CLIENT_SOURCE)
+        return mapOf(
+            KEY_CLIENT_SOURCE to VALUE_CLIENT_SOURCE,
+            KEY_CLIENT_VERSION to appVersionName,
+        )
     }
 
     companion object {
         private const val KEY_CLIENT_SOURCE = "x-client-source"
+        private const val KEY_CLIENT_VERSION = "x-client-version"
 
         private const val VALUE_CLIENT_SOURCE = "android-app"
     }
