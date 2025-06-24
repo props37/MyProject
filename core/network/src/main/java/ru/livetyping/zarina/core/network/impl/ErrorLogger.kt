0b42209375a2c49a1eee6961cc519efbd11fda74
Artem Sidorov <sidorovaa@melonfashion.com>
2025-06-24 17:23:57 +0100

    ZRWEB-2618: Add HTTP-methods to error logs

--- core/network/src/main/java/ru/livetyping/zarina/core/network/impl/ErrorLogger.kt
+++ core/network/src/main/java/ru/livetyping/zarina/core/network/impl/ErrorLogger.kt
@@ -6,7 +6,6 @@ import io.ktor.client.plugins.ResponseException
 import io.ktor.client.statement.request
 import ru.livetyping.zarina.core.analytics.HttpError
 import ru.livetyping.zarina.core.analytics.HttpErrorLogger
-import java.net.ConnectException
 import kotlin.coroutines.cancellation.CancellationException
 
 internal fun HttpClientConfig<*>.logErrors(logger: HttpErrorLogger) {
@@ -16,12 +15,15 @@ internal fun HttpClientConfig<*>.logErrors(logger: HttpErrorLogger) {
                 is ResponseException -> {
                     HttpError {
                         val response = cause.response
-                        request = response.request.url.toString()
+                        request = buildString {
+                            append(response.request.method.value)
+                            append(SPACE)
+                            append(response.request.url.toString())
+                        }
                         statusCode = response.status.value
                     }
                 }
 
-                is ConnectException -> HttpError { description = cause.message }
                 is CancellationException -> null
                 else -> HttpError { description = cause.message }
             }
@@ -30,3 +32,5 @@ internal fun HttpClientConfig<*>.logErrors(logger: HttpErrorLogger) {
         }
     }
 }
+
+private const val SPACE = ' '
