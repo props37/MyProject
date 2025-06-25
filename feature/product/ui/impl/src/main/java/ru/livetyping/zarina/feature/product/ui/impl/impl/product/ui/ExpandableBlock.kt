9c28771b44a2e14d92c0a147791818a97a07b9db
Artem Sidorov <sidorovaa@melonfashion.com>
2025-06-25 15:36:05 +0100

    ZRWEB-2317: Update models that contains information about models and product sizes they wear

--- feature/product/ui/impl/src/main/java/ru/livetyping/zarina/feature/product/ui/impl/impl/product/ui/ExpandableBlock.kt
+++ feature/product/ui/impl/src/main/java/ru/livetyping/zarina/feature/product/ui/impl/impl/product/ui/ExpandableBlock.kt
@@ -134,13 +134,14 @@ private fun rememberProductDetailsText(product: ProductDetailed): AnnotatedStrin
                 appendTwoLines()
             }
 
-            product.modelInfo?.modelParams?.let {
-                appendKeyValue(
-                    key = context.getString(R.string.product_model_parameters),
-                    value = it,
-                    keyStyle = keyStyle,
-                )
-            }
+            // TODO: [Top] Implement
+//            product.modelInfo?.modelParams?.let {
+//                appendKeyValue(
+//                    key = context.getString(R.string.product_model_parameters),
+//                    value = it,
+//                    keyStyle = keyStyle,
+//                )
+//            }
         }
     }
 }
