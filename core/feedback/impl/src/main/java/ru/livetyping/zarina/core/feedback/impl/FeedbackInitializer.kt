d3371e011752c2686d47c565bf4ad7f963adc51b
Artem Sidorov <sidorovaa@melonfashion.com>
2025-07-14 16:38:26 +0100

    ZRWEB-2131, ZRWEB-2679: Set up custom theme

--- core/feedback/impl/src/main/java/ru/livetyping/zarina/core/feedback/impl/FeedbackInitializer.kt
+++ core/feedback/impl/src/main/java/ru/livetyping/zarina/core/feedback/impl/FeedbackInitializer.kt
@@ -2,7 +2,9 @@ package ru.livetyping.zarina.core.feedback.impl
 
 import android.app.Application
 import android.util.Log
+import ru.uxfeedback.pub.sdk.UxFbColor
 import ru.uxfeedback.pub.sdk.UxFbSettings
+import ru.uxfeedback.pub.sdk.UxFbTheme
 import ru.uxfeedback.pub.sdk.UxFeedback
 
 public object FeedbackInitializer {
@@ -17,6 +19,7 @@ public object FeedbackInitializer {
                 appId = appId,
                 settings = getSettings(isDebugEnabled),
             )
+            UxFeedback.sdk?.theme = UxFbTheme.fromStyle(R.style.UxFeedbackTheme)
             Log.v(TAG, "UxFeedback initialized")
         } catch (e: Exception) {
             Log.e(TAG, "Failed to initialize UxFeedback", e)
@@ -33,14 +36,25 @@ public object FeedbackInitializer {
 
     private fun getSettings(isDebugEnabled: Boolean): UxFbSettings {
         return UxFbSettings.getDefault().apply {
-            slideInUiBlocked = true
-
             debugEnabled = isDebugEnabled
             if (isDebugEnabled) {
                 startGlobalDelayTimer = 1
             }
+
+            slideInUiBlocked = true
+            slideInUiBlackoutColor = UxFbColor.fromHex(BLACKOUT_COLOR_HEX)
+            slideInUiBlackoutOpacity = BLACKOUT_OPACITY
+            slideInUiBlackoutBlur = BLACKOUT_BLUR
+
+            popupUiBlackoutColor = UxFbColor.fromHex(BLACKOUT_COLOR_HEX)
+            popupUiBlackoutOpacity = BLACKOUT_OPACITY
+            popupUiBlackoutBlur = BLACKOUT_BLUR
         }
     }
+
+    private const val BLACKOUT_COLOR_HEX = "#000000"
+    private const val BLACKOUT_OPACITY = 50
+    private const val BLACKOUT_BLUR = 40
 
     private const val USER_ID_KEY = "user_id"
 
