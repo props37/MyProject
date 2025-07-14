d7923ac7f2611b26873c266fc859bef343e69377
Artem Sidorov <sidorovaa@melonfashion.com>
2025-07-14 16:07:27 +0100

    ZRWEB-2131, ZRWEB-2679: Integrate Feedback to Catalog screen

--- feature/catalog/ui/impl/src/main/java/ru/livetyping/zarina/feature/catalog/ui/impl/impl/CatalogDependencies.kt
+++ feature/catalog/ui/impl/src/main/java/ru/livetyping/zarina/feature/catalog/ui/impl/impl/CatalogDependencies.kt
@@ -3,10 +3,12 @@ package ru.livetyping.zarina.feature.catalog.ui.impl.impl
 import ru.livetyping.zarina.core.analytics.AppMetrica
 import ru.livetyping.zarina.core.domain.usecase.catalog.GetCatalogMenuUseCase
 import ru.livetyping.zarina.core.domain.usecase.user.GetUserCityFlowUseCase
+import ru.livetyping.zarina.core.feedback.Feedback
 import javax.inject.Inject
 
 internal class CatalogDependencies @Inject constructor(
     val getCatalogMenu: GetCatalogMenuUseCase,
     val getUserCityFlow: GetUserCityFlowUseCase,
     val appMetrica: AppMetrica,
+    val feedback: Feedback,
 )
