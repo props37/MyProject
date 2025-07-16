64ce313302ed315d8229867d1cf694dfe9b41cad
Artem Sidorov <sidorovaa@melonfashion.com>
2025-07-16 10:38:23 +0100

    Fix imports

--- core/ui-kit/src/main/java/ru/livetyping/zarina/core/uikit/theme/LocalUiKitColors.kt
+++ core/ui-kit/src/main/java/ru/livetyping/zarina/core/uikit/theme/LocalUiKitColors.kt
@@ -2,7 +2,6 @@ package ru.livetyping.zarina.core.uikit.theme
 
 import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.staticCompositionLocalOf
-import ru.livetyping.zarina.core.uikit.impl.theme.LightUiKitColors
 
 // TODO: [Top] Remove after full migration
 
