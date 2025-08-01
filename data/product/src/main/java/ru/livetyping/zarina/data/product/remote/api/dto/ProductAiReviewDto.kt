1815ce338a2097f92da2eee39e4161db93e95b78
Bogdan Kostyukov <bnkostyukov@gmail.com>
2025-08-01 15:20:50 +0300

    ZRWEB-2787: Add ProductAiReviewDto for AI reviews

--- data/product/src/main/java/ru/livetyping/zarina/data/product/remote/api/dto/ProductAiReviewDto.kt
+++ data/product/src/main/java/ru/livetyping/zarina/data/product/remote/api/dto/ProductAiReviewDto.kt
@@ -1,6 +1,35 @@
 package ru.livetyping.zarina.data.product.remote.api.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal data class ProductAiReviewDto(val x: Int)
+internal data class ProductAiReviewDto(
+
+    @SerialName("externalId")
+    val externalId: String? = null,
+
+    @SerialName("description")
+    val description: String? = null,
+
+    @SerialName("reviewsCount")
+    val reviewsCount: Int? = null,
+
+    @SerialName("longDescription")
+    val longDescription: String? = null,
+
+    @SerialName("tableTagsData")
+    val tableTagsData: List<tagDto>? = null,
+) {
+    @Serializable
+    data class tagDto(
+        @SerialName("tagName")
+        val tagName: String? = null,
+
+        @SerialName("tagFocus")
+        val tagFocus: String? = null,
+
+        @SerialName("tagCount")
+        val tagCount: String? = null,
+    )
+}
